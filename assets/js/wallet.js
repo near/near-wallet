7f334627ac9982b2f68fdfc0282788380e98bc08
azban <me@azban.net>
2019-02-28 21:06:03 -0800

    jekyll: use baseurl for wallet.js urls

--- assets/js/wallet.js
+++ assets/js/wallet.js
@@ -1,7 +1,7 @@
 ---
 ---
-const WALLET_URL = "/login/";
-const WALLET_CREATE_NEW_ACCOUNT_URL = '/create/';
+const WALLET_URL = "{{ site.baseurl }}/login/";
+const WALLET_CREATE_NEW_ACCOUNT_URL = '{{ site.baseurl }}/create/';
 
 {% if jekyll.environment == "production" %}
 const CONTRACT_CREATE_ACCOUNT_URL = 'https://studio.nearprotocol.com/contract-api/account';
@@ -10,8 +10,8 @@ const NODE_URL = "https://studio.nearprotocol.com/devnet";
 const CONTRACT_CREATE_ACCOUNT_URL = 'http://localhost:3000/account';
 const NODE_URL = 'http://localhost:3030'
 {% else %}
-const CONTRACT_CREATE_ACCOUNT_URL = '/contract-api/account';
-const NODE_URL = '/devnet'
+const CONTRACT_CREATE_ACCOUNT_URL = '{{ site.baseurl }}/contract-api/account';
+const NODE_URL = '{{ site.baseurl }}/devnet'
 {% endif %}
 
 const KEY_UNIQUE_PREFIX = "_2:";
