6932150771822920dc6a89ea3799e6c25386cf0b
Vladimir Grichina <vgrichina@gmail.com>
2019-05-10 22:14:45 -0700

    Use ‘connected-react-router’

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
+import { ConnectedRouter } from 'connected-react-router'
 
 import '../index.css'
 
@@ -26,7 +27,7 @@ class Routing extends Component {
          <div className='App'>
             <GlobalStyle />
 
-            <Router basename={PATH_PREFIX}>
+            <ConnectedRouter basename={PATH_PREFIX}  history={this.props.history}>
                <ThemeProvider theme={theme}>
                   <ResponsiveContainer>
                      <Switch>
@@ -71,7 +72,7 @@ class Routing extends Component {
                      <Footer />
                   </ResponsiveContainer>
                </ThemeProvider>
-            </Router>
+            </ConnectedRouter>
          </div>
       )
    }
--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,6 +1,10 @@
 import thunk from 'redux-thunk'
 import { applyMiddleware, compose } from 'redux'
+import { routerMiddleware } from 'connected-react-router'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-export default composeEnhancers(applyMiddleware(thunk))
+export default (history) => composeEnhancers(
+    applyMiddleware(
+        routerMiddleware(history),
+        thunk))
