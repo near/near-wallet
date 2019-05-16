4c2e4ceba06265a1950f2e41b0e32bd44dba405a
Vladimir Grichina <vgrichina@gmail.com>
2019-05-15 17:48:56 -0700

    Fix lint error

--- src/components/common/Disclaimer.js
+++ src/components/common/Disclaimer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, Responsive } from 'semantic-ui-react'
+import { Grid } from 'semantic-ui-react'
 import styled from 'styled-components'
 
 const DisclaimerGrid = styled(Grid)`
