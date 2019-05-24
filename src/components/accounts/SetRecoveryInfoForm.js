2628529e03de9a3a2cfadc9dbd3afb88e9535533
marcinbodnardesktop <me@marcinbodnar.com>
2019-05-24 21:18:15 +0200

    Set Recovery page button padding fix

--- src/components/accounts/SetRecoveryInfoForm.js
+++ src/components/accounts/SetRecoveryInfoForm.js
@@ -14,22 +14,17 @@ const RecoveryInfoForm = styled(Form)`
       height: 64px;
       border: 4px solid #f8f8f8;
       padding: 0 0 0 20px;
-
       font-size: 18px;
       color: #4a4f54;
       font-weight: 400;
       background: 0;
-
       position: relative;
-
       :focus {
          border-color: #6ad1e3;
       }
    }
-
    &&&&& .spinner {
       margin-right: 20px;
-
       :before,
       :after {
          top: 28px;
@@ -37,7 +32,6 @@ const RecoveryInfoForm = styled(Form)`
          height: 24px;
       }
    }
-
    .problem > .input > input {
       background: url(${ProblemsImage}) right 22px center no-repeat;
       background-size: 24px 24px;
@@ -46,7 +40,6 @@ const RecoveryInfoForm = styled(Form)`
       background: url(${ProblemsImage}) right 22px center no-repeat;
       background-size: 24px 24px;
    }
-
    .success > .input > input {
       background: url(${CheckBlueImage}) right 22px center no-repeat;
       background-size: 24px 24px;
@@ -55,20 +48,16 @@ const RecoveryInfoForm = styled(Form)`
       background: url(${CheckBlueImage}) right 22px center no-repeat;
       background-size: 24px 24px;
    }
-
    &&& button {
       width: 288px;
       height: 60px;
       border-radius: 30px;
       border: 4px solid #0072ce;
-
       background: #0072ce;
-      margin: 10px 0 0 0;
-
+      margin: 10px 1em 0 0;
       font-size: 18px;
       color: #fff;
       letter-spacing: 2px;
-
       :hover {
          background: #fff;
          color: #0072ce;
@@ -84,30 +73,24 @@ const RecoveryInfoForm = styled(Form)`
          color: #0072ce;
       }
    }
-
    .ui.button {
       height: 60px;
-      margin: 10px 0 0 1em;
-
+      margin: 10px 0 0 0;
       background-color: #fff;
       border-radius: 30px;
       border: 4px solid #e6e6e6;
       color: #999999;
-
       font-size: 18px;
       line-height: 24px;
       letter-spacing: 2px;
-
       :hover {
          background-color: #e6e6e6;
          color: #fff;
       }
    }
-
    & h3 {
       margin-bottom: 1rem;
    }
-
    select.react-phone-number-input__country-select {
       height: 100%;
    }
@@ -127,10 +110,18 @@ const SetRecoveryInfoForm = ({
          <Form.Field>
             <h3>Phone Number</h3>
             <PhoneInput
-               className={`create ${requestStatus ? (requestStatus.success ? 'success' : 'problem') : ''} ${formLoader ? 'loading' : '' }`}
+               className={`create ${
+                  requestStatus
+                     ? requestStatus.success
+                        ? 'success'
+                        : 'problem'
+                     : ''
+               } ${formLoader ? 'loading' : ''}`}
                name='phoneNumber'
                value={phoneNumber}
-               onChange={ value => handleChange(null, { name: 'phoneNumber', value })}
+               onChange={value =>
+                  handleChange(null, { name: 'phoneNumber', value })
+               }
                placeholder='example: +1 555 123 4567'
             />
          </Form.Field>
@@ -148,8 +139,12 @@ const SetRecoveryInfoForm = ({
       )}
 
       <Form.Field>
-         <Button type='submit' disabled={!isLegit}>PROTECT ACCOUNT</Button>
-         <Link to="/dashboard" className="ui button">NOT NOW</Link>
+         <Button type='submit' disabled={!isLegit}>
+            PROTECT ACCOUNT
+         </Button>
+         <Link to='/dashboard' className='ui button'>
+            NOT NOW
+         </Link>
       </Form.Field>
    </RecoveryInfoForm>
 )
