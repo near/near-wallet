2865c66059d1b59befafa207816a0c2e1da88644
marcinbodnardesktop <me@marcinbodnar.com>
2019-06-12 20:37:10 +0200

    Send Money change account field validation text and position

--- src/components/send-money/SendMoneyFirstStep.js
+++ src/components/send-money/SendMoneyFirstStep.js
@@ -32,20 +32,16 @@ const CustomList = styled(List)`
          .alert-info {
             font-weight: 600;
             margin: 0 0 -8px 0;
-            padding: 0 0 0 24px !important;
+            padding: 0 0 0 0 !important;
             line-height: 34px;
             font-size: 14px;
             margin-top: -6px;
-            text-align: left;
+            text-align: center;
             &.problem {
                color: #ff585d;
             }
             &.success {
                color: #6ad1e3;
-            }
-            &.amount {
-               text-align: center;
-               padding: 0 !important;
             }
          }
          .main-image {
@@ -218,12 +214,12 @@ const SendMoneyFirstStep = ({
 
                   {successMessage && (
                      <Segment basic className='alert-info success'>
-                        Username exists.
+                        User found.
                      </Segment>
                   )}
                   {errorMessage && (
                      <Segment basic className='alert-info problem'>
-                        Username does not exist.
+                        User not found.
                      </Segment>
                   )}
                </List.Content>
@@ -243,7 +239,7 @@ const SendMoneyFirstStep = ({
             
          </List.Item>
          {amountStatus && (
-            <Segment basic textAlign='center' className='alert-info problem amount'>
+            <Segment basic textAlign='center' className='alert-info problem'>
                {amountStatus}
             </Segment>
          )}
