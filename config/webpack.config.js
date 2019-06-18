5719b107d196aae45c47c6417f49b02858827900
Vladimir Grichina <vgrichina@gmail.com>
2019-06-18 13:39:06 -0700

    Treat nearlib same as app code in webpack config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -328,7 +328,7 @@ module.exports = function(webpackEnv) {
             // The preset includes JSX, Flow, TypeScript, and some ESnext features.
             {
               test: /\.(js|mjs|jsx|ts|tsx)$/,
-              include: paths.appSrc,
+              include: [paths.appSrc, /nearlib/],
               loader: require.resolve('babel-loader'),
               options: {
                 customize: require.resolve(
