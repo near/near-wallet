e41e71a127e70a938bbdcccb161ab7b971a1282d
Yifang Ma <yifang@nearprotocol.com>
2019-07-29 17:42:15 -0700

    not nan

--- src/components/send-money/SendMoneyAmountInput.js
+++ src/components/send-money/SendMoneyAmountInput.js
@@ -110,7 +110,7 @@ class SendMoneyAmountInput extends Component {
                   {amountStatus}
                </Segment>
             )}
-            {amount ? <Balance milli={milli} amount={this.state.amount} /> : "NaN"}
+            {amount ? <Balance milli={milli} amount={this.state.amount} /> : ''}
          </CustomDiv>
       )
    }
