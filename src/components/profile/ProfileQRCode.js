8911234e9fd16f3c612f49b6363520c08f0bfac4
Vladimir Grichina <vgrichina@gmail.com>
2019-08-13 22:54:38 -0700

    Some code and style cleanup

---
+++ src/components/profile/ProfileQRCode.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import styled from 'styled-components'
+import { QRCode } from "react-qr-svg";
+
+const CustomDiv = styled.div`
+    & {
+        text-align: center;
+    }
+
+    h2 {
+        margin-bottom: 1em;
+    }
+    
+    p {
+        margin-top: 1em;    
+    }
+`
+
+const ProfileQRCode = ({ account }) => (
+   <CustomDiv>
+        <h2>Scan to pay</h2>
+        <QRCode
+            bgColor="#FFFFFF"
+            fgColor="#24272a"
+            level="Q"
+            style={{ width: "100%" }}
+            value={`${window.location.protocol}//${window.location.host}/send-money/${account.accountId}`}
+        />
+        <p>Scan using phone camera to send payment to this address.</p>
+   </CustomDiv>
+)
+
+export default ProfileQRCode
--- src/components/profile/ProfileSection.js
+++ src/components/profile/ProfileSection.js
@@ -6,8 +6,7 @@ const ProfileSection = ({ children }) => (
    <Grid columns={2} stackable>
       <Grid.Column computer={10}>{children[0]}</Grid.Column>
       <Grid.Column computer={6}>
-         {children[1]}
-         {children[2]}
+         {children.slice(1)}
       </Grid.Column>
    </Grid>
 )
