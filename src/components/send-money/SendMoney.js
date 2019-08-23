acc9256c3626359b65153765ee4d902c7bab82eb
Vladimir Grichina <vgrichina@gmail.com>
2019-08-23 15:36:09 -0700

    Remove leftover debug console.log

--- src/components/send-money/SendMoney.js
+++ src/components/send-money/SendMoney.js
@@ -125,10 +125,9 @@ class SendMoney extends Component {
    isLegitForm = () => {
       const { paramAccountId, amount, amountStatus } = this.state
       const { requestStatus } = this.props
-      console.log("[sendmoney.js], ", amount)
       return paramAccountId
-      ? ((amount) > 0 && amountStatus === '')
-      : (requestStatus && requestStatus.success && (amount) > 0 && amountStatus === '')
+         ? ((amount) > 0 && amountStatus === '')
+         : (requestStatus && requestStatus.success && (amount) > 0 && amountStatus === '')
    }
 
    render() {
