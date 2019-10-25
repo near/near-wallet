1cee40233174672f46274c951afec08d5d3b1a31
Vladimir Grichina <vgrichina@gmail.com>
2019-10-24 19:08:47 -0700

    Revert "Fixed loading SVG as component"
    This reverts commit 5f9b0d86a9dc2e27585d9117bf7a7d34438e1e83.

--- src/components/sign/SignAnimatedArrow.js
+++ src/components/sign/SignAnimatedArrow.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import SendImage from '../../images/icon-send.svg'
+import { ReactComponent as SendImage } from '../../images/icon-send.svg'
 
 import styled from 'styled-components'
 
