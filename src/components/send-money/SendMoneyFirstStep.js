740d03c40dd53b25ff6815673300496c926611af
marcinbodnardesktop <me@marcinbodnar.com>
2020-01-08 23:15:38 +0100

    Change indentation for SendMoneyFirstStep component

--- src/components/send-money/SendMoneyFirstStep.js
+++ src/components/send-money/SendMoneyFirstStep.js
@@ -1,10 +1,10 @@
 import React, { Fragment } from 'react'
 
 import {
-   Header,
-   TextArea,
-   List,
-   Form
+    Header,
+    TextArea,
+    List,
+    Form
 } from 'semantic-ui-react'
 
 import PageContainer from '../common/PageContainer'
@@ -25,69 +25,67 @@ const SendMoneyFirstStep = ({
    amount,
    checkAvailability
 }) => {
-   return (
-      <Form autoComplete='off'>
-         <MobileContainer>
-            <Fragment>
-               <PageContainer
-                  title={`Send NEAR`}
-                  type='center'
-               />
-               <List className='list-top border'>
-                     {paramAccountId ? (
-                        <List.Item>
-                           <List.Content as='h2'>{accountId}</List.Content>
-                           <List.Content>@{accountId}</List.Content>
+    return (
+        <Form autoComplete='off'>
+            <MobileContainer>
+                <Fragment>
+                    <PageContainer
+                        title={`Send NEAR`}
+                        type='center'
+                    />
+                    <List className='list-top border'>
+                        {paramAccountId ? (
+                            <List.Item>
+                                <List.Content as='h2'>{accountId}</List.Content>
+                                <List.Content>@{accountId}</List.Content>
+                            </List.Item>
+                        ) : (
+                            <List.Item>
+                                <List.Content>
+                                    <Header as='h4' textAlign='left'>Username to send to:</Header>
+                                    <AccountFormAccountId
+                                        formLoader={formLoader}
+                                        handleChange={handleChange}
+                                        defaultAccountId={accountId}
+                                        autoFocus={true}
+                                    />
+                                </List.Content>
+                            </List.Item>
+                        )}
+                        <List.Item className='amount border-top'>
+                            <SendMoneyAmountInput 
+                                handleChange={handleChange} 
+                                defaultAmount={amount}
+                            />
                         </List.Item>
-                     ) : (
-                        <List.Item>
-                           <List.Content>
-                              <Header as='h4' textAlign='left'>Username to send to:</Header>
-                              <AccountFormAccountId
-                                 formLoader={formLoader}
-                                 handleChange={handleChange}
-                                 defaultAccountId={accountId}
-                                 checkAvailability={checkAvailability}
-                                 requestStatus={requestStatus}
-                                 autoFocus={true}
-                              />
-                           </List.Content>
+                        {false ? (
+                            <List.Item className='add-note border-bottom border-top'>
+                                <TextArea
+                                    name='note'
+                                    value={note}
+                                    onChange={handleChange}
+                                    placeholder='Add a note...'
+                                />
+                            </List.Item>
+                        ) : null}
+                    </List>
+                </Fragment>
+                <Fragment>
+                    <List className='list-bottom border'>
+                        <List.Item className='send-money'>
+                            <FormButton
+                                onClick={handleNextStep}
+                                color='green'
+                                disabled={!isLegitForm()}
+                            >
+                                SEND
+                            </FormButton>
                         </List.Item>
-                     )}
-                     <List.Item className='amount border-top'>
-                        <SendMoneyAmountInput 
-                           handleChange={handleChange} 
-                           defaultAmount={amount}
-                        />
-                     </List.Item>
-                     {false ? (
-                        <List.Item className='add-note border-bottom border-top'>
-                           <TextArea
-                              name='note'
-                              value={note}
-                              onChange={handleChange}
-                              placeholder='Add a note...'
-                           />
-                        </List.Item>
-                     ) : null}
-               </List>
-            </Fragment>
-            <Fragment>
-               <List className='list-bottom border'>
-                  <List.Item className='send-money'>
-                     <FormButton
-                        onClick={handleNextStep}
-                        color='green'
-                        disabled={!isLegitForm()}
-                     >
-                        SEND
-                     </FormButton>
-                  </List.Item>
-               </List>
-            </Fragment>
-         </MobileContainer>
-      </Form>
-   )
+                    </List>
+                </Fragment>
+            </MobileContainer>
+        </Form>
+    )
 }
 
 export default SendMoneyFirstStep
