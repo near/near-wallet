632187466b3aae6b678a79c71a76af6a7dfbed4f
Patrick Tajima <patrick.tajima@gmail.com>
2020-01-16 16:48:37 -0800

    Sign up: show nav links if user has existing account

--- src/components/responsive/ResponsiveContainer.js
+++ src/components/responsive/ResponsiveContainer.js
@@ -13,27 +13,41 @@ class ResponsiveContainer extends Component {
         return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth;
     }
 
-    userInSignupFlow = () => {
+    showNavLinks = () => {
+        const { account, location } = this.props;
+        const accounts = account.accounts;
         const signUpRoutes = ['create', 'set-recovery', 'setup-seed-phrase'];
-        const currentBaseRoute = this.props.location.pathname.replace(/^\/([^\/]*).*$/, '$1');
-        return signUpRoutes.includes(currentBaseRoute);
+        const currentBaseRoute = location.pathname.replace(/^\/([^\/]*).*$/, '$1');
+
+        if (signUpRoutes.includes(currentBaseRoute)) {
+            if (currentBaseRoute === signUpRoutes[0]) {
+                if (accounts)
+                    return true;
+                else
+                    return false;
+            } else {
+                if (accounts && Object.keys(accounts).length > 1)
+                    return true;
+                else
+                    return false;
+            }
+        } else {
+            return true;
+        }
     }
 
     render() {
-        const { account } = this.props;
-
-        const showNavbarLinks = !this.userInSignupFlow() && account.accountId;
 
         return (
             <>
                 <DesktopView
                     {...this.props}
-                    showNavbarLinks={showNavbarLinks}
+                    showNavbarLinks={this.showNavLinks()}
                     getWidth={this.getWidth}
                 />
                 <MobileView
                     {...this.props}
-                    showNavbarLinks={showNavbarLinks}
+                    showNavbarLinks={this.showNavLinks()}
                     getWidth={this.getWidth}
                 />
                 <div className='main'>
