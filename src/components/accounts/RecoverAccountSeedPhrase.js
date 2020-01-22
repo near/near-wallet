e0cc2ba8288e2aa75db134f1e40a1121d60dea80
Vladimir Grichina <vgrichina@gmail.com>
2020-01-21 20:04:02 -0800

    Reformat RecoverAccountSeedPhrase.js and wallet.js

--- src/components/accounts/RecoverAccountSeedPhrase.js
+++ src/components/accounts/RecoverAccountSeedPhrase.js
@@ -9,85 +9,85 @@ import AccountFormSection from './AccountFormSection'
 import AccountFormContainer from './AccountFormContainer'
 
 class RecoverAccountSeedPhrase extends Component {
-   state = {
-      accountId: this.props.accountId,
-      seedPhrase: this.props.seedPhrase
-   }
+    state = {
+        accountId: this.props.accountId,
+        seedPhrase: this.props.seedPhrase
+    }
 
-   // TODO: Use some validation framework?
-   validators = {
-      seedPhrase: value => true // TODO validate seed phrase
-   }
+    // TODO: Use some validation framework?
+    validators = {
+        seedPhrase: value => true // TODO validate seed phrase
+    }
 
-   get isLegit() {
-      return Object.keys(this.validators).every(field => this.validators[field](this.state[field]))
-   }
+    get isLegit() {
+        return Object.keys(this.validators).every(field => this.validators[field](this.state[field]))
+    }
 
-   componentDidMount = () => {}
+    componentDidMount = () => {}
 
-   componentWillUnmount = () => {
-      this.props.clear()
-   }
+    componentWillUnmount = () => {
+        this.props.clear()
+    }
 
-   handleChange = (e, { name, value }) => {
-      this.setState(() => ({
-         [name]: value
-      }))
-   }
+    handleChange = (e, { name, value }) => {
+        this.setState(() => ({
+            [name]: value
+        }))
+    }
 
-   handleSubmit = e => {
-      e.preventDefault()
+    handleSubmit = e => {
+        e.preventDefault()
 
-      if (!this.isLegit) {
-         return false
-      }
+        if (!this.isLegit) {
+            return false
+        }
 
-      const accountId = this.state.accountId
-      this.props.recoverAccountSeedPhrase(this.state.seedPhrase, accountId)
-         .then(({ error }) => {
-            if (error) return
-            this.props.redirectToApp()
-         })
-   }
+        const accountId = this.state.accountId
+        this.props.recoverAccountSeedPhrase(this.state.seedPhrase, accountId)
+            .then(({ error }) => {
+                if (error) return
+                this.props.redirectToApp()
+            })
+    }
 
-   render() {
-      const combinedState = {
-         ...this.props,
-         ...this.state,
-         isLegit: this.isLegit && !this.props.formLoader
-      }
-      
-      return (
-         <AccountFormContainer 
-            wide={true} 
-            title="Recover using Seed Phrase"
-            text={"Enter the backup seed phrase associated with the account"}
-         >
-            <AccountFormSection requestStatus={this.props.requestStatus} handleSubmit={this.handleSubmit.bind(this)}>
-               <RecoverAccountSeedPhraseForm
-                  {...combinedState}
-                  handleChange={this.handleChange}
-               />
-            </AccountFormSection>
-         </AccountFormContainer>
-      )
-   }
+    render() {
+        const combinedState = {
+            ...this.props,
+            ...this.state,
+            isLegit: this.isLegit && !this.props.formLoader
+        }
+        
+        return (
+            <AccountFormContainer 
+                wide={true} 
+                title="Recover using Seed Phrase"
+                text={"Enter the backup seed phrase associated with the account"}
+            >
+                <AccountFormSection requestStatus={this.props.requestStatus} handleSubmit={this.handleSubmit.bind(this)}>
+                    <RecoverAccountSeedPhraseForm
+                        {...combinedState}
+                        handleChange={this.handleChange}
+                    />
+                </AccountFormSection>
+            </AccountFormContainer>
+        )
+    }
 }
 
 const mapDispatchToProps = {
-   recoverAccountSeedPhrase, 
-   redirectToApp,
-   checkAccountAvailable,
-   clear
+    recoverAccountSeedPhrase, 
+    redirectToApp,
+    checkAccountAvailable,
+    clear
 }
 
 const mapStateToProps = ({ account }, { match }) => ({
-   ...account,
-   accountId: match.params.accountId || '',
-   seedPhrase: match.params.seedPhrase || '',
+    ...account,
+    accountId: match.params.accountId || '',
+    seedPhrase: match.params.seedPhrase || '',
 })
 
 export const RecoverAccountSeedPhraseWithRouter = connect(
-   mapStateToProps, 
-   mapDispatchToProps
+    mapStateToProps, 
+    mapDispatchToProps
 )(withRouter(RecoverAccountSeedPhrase))
