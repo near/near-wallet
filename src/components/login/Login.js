1d79612f60b67af9ab4a0f6b24c5ac88df4f927c
marcinbodnardesktop <me@marcinbodnar.com>
2020-01-23 18:47:58 +0100

    Change indentation for Login.js

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -10,108 +10,108 @@ import LoginDetails from './LoginDetails'
 import { handleRefreshAccount, handleRefreshUrl, switchAccount, clearAlert, allowLogin, handleLoginUrl } from '../../actions/account'
 
 class Login extends Component {
-   state = {
-      buttonLoader: false,
-      dropdown: false,
-   }
+    state = {
+        buttonLoader: false,
+        dropdown: false,
+    }
 
-   componentDidMount = () => {
-      this.props.handleLoginUrl()
-   }   
+    componentDidMount = () => {
+        this.props.handleLoginUrl()
+    }    
 
-   handleOnClick = () => {
-      this.setState({
-         dropdown: !this.state.dropdown
-      })
-   }
+    handleOnClick = () => {
+        this.setState({
+            dropdown: !this.state.dropdown
+        })
+    }
 
-   handleDeny = e => {
-      e.preventDefault();
-      if (this.props.account.url.failure_url) {
-         window.location.href = this.props.account.url.failure_url
-      }
-   }
+    handleDeny = e => {
+        e.preventDefault();
+        if (this.props.account.url.failure_url) {
+            window.location.href = this.props.account.url.failure_url
+        }
+    }
 
-   handleAllow = () => {
-      this.setState(() => ({
-         buttonLoader: true
-      }))
+    handleAllow = () => {
+        this.setState(() => ({
+            buttonLoader: true
+        }))
 
-      this.props.allowLogin()
-         .finally(() => {
-            this.setState(() => ({
-               buttonLoader: false
-            }))
-         })
-   }
+        this.props.allowLogin()
+            .finally(() => {
+                this.setState(() => ({
+                    buttonLoader: false
+                }))
+            })
+    }
 
-   handleSelectAccount = accountId => {
-      this.props.switchAccount(accountId)
-      this.props.handleRefreshAccount(this.props.history)
-   }
+    handleSelectAccount = accountId => {
+        this.props.switchAccount(accountId)
+        this.props.handleRefreshAccount(this.props.history)
+    }
 
-   redirectCreateAccount = () => {
-      this.props.history.push('/create')
-   }
+    redirectCreateAccount = () => {
+        this.props.history.push('/create')
+    }
 
-   render() {
-      const { account, match } = this.props
+    render() {
+        const { account, match } = this.props
 
-      return (
-         <LoginContainer>
-            <Route
-               exact
-               path={`${match.url}`}
-               render={(props) => (
-                  <LoginForm
-                     {...this.state}
-                     {...props}
-                     appTitle={account.url && account.url.title}
-                     contractId={account.url && account.url.contract_id}
-                     handleOnClick={this.handleOnClick}
-                     handleDeny={this.handleDeny}
-                     handleAllow={this.handleAllow}
-                     handleSelectAccount={this.handleSelectAccount}
-                     redirectCreateAccount={this.redirectCreateAccount}
-                     handleDetails={this.handleDetails}
-                  />
-               )}
-            />
-            <Route 
-               exact
-               path={`${match.url}/details`}
-               render={(props) => (
-                  <LoginDetails
-                     {...props}
-                     contractId={account.url && account.url.contract_id}
-                     appTitle={account.url && account.url.title}
-                  />
-               )}
-            />
-            <Route 
-               exact
-               path={`${match.url}/confirm`}
-               render={(props) => (
-                  <LoginConfirm
-                     {...props}
-                     buttonLoader={this.state.buttonLoader}
-                     appTitle={account.url && account.url.title}
-                     handleAllow={this.handleAllow}
-                  />
-               )}
-            />
-         </LoginContainer>
-      )
-   }
+        return (
+            <LoginContainer>
+                <Route
+                    exact
+                    path={`${match.url}`}
+                    render={(props) => (
+                        <LoginForm
+                            {...this.state}
+                            {...props}
+                            appTitle={account.url && account.url.title}
+                            contractId={account.url && account.url.contract_id}
+                            handleOnClick={this.handleOnClick}
+                            handleDeny={this.handleDeny}
+                            handleAllow={this.handleAllow}
+                            handleSelectAccount={this.handleSelectAccount}
+                            redirectCreateAccount={this.redirectCreateAccount}
+                            handleDetails={this.handleDetails}
+                        />
+                    )}
+                />
+                <Route 
+                    exact
+                    path={`${match.url}/details`}
+                    render={(props) => (
+                        <LoginDetails
+                            {...props}
+                            contractId={account.url && account.url.contract_id}
+                            appTitle={account.url && account.url.title}
+                        />
+                    )}
+                />
+                <Route 
+                    exact
+                    path={`${match.url}/confirm`}
+                    render={(props) => (
+                        <LoginConfirm
+                            {...props}
+                            buttonLoader={this.state.buttonLoader}
+                            appTitle={account.url && account.url.title}
+                            handleAllow={this.handleAllow}
+                        />
+                    )}
+                />
+            </LoginContainer>
+        )
+    }
 }
 
 const mapDispatchToProps = {
-   handleRefreshAccount,
-   handleRefreshUrl,
-   switchAccount,
-   allowLogin,
-   clearAlert,
-   handleLoginUrl
+    handleRefreshAccount,
+    handleRefreshUrl,
+    switchAccount,
+    allowLogin,
+    clearAlert,
+    handleLoginUrl
 }
 
 const mapStateToProps = ({ account }) => ({
