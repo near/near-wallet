07e43518304e644ad5fa16a72661cdc087f0b3bc
Patrick Tajima <patrick.tajima@gmail.com>
2020-01-24 17:24:39 -0800

    Recover w/link: Add confirm account page

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -20,6 +20,7 @@ import { CreateAccountWithRouter } from './accounts/CreateAccount'
 import { SetRecoveryMethodContainerWithRouter } from './accounts/SetRecoveryMethodContainer'
 import { RecoverAccountWithRouter } from './accounts/RecoverAccount'
 import { RecoverAccountSeedPhraseWithRouter } from './accounts/RecoverAccountSeedPhrase'
+import { RecoverWithLinkWithRouter } from './accounts/RecoverWithLink'
 import { LoginWithRouter } from './login/Login'
 import { ContactsWithRouter } from './contacts/Contacts'
 import { AuthorizedAppsWithRouter } from './access-keys/AccessKeys'
@@ -118,6 +119,11 @@ class Routing extends Component {
                               exact
                               path='/recover-seed-phrase/:accountId?/:seedPhrase?'
                               component={RecoverAccountSeedPhraseWithRouter}
+                           />
+                           <Route
+                              exact
+                              path='/recover-with-link/:accountId?/:seedPhrase?'
+                              component={RecoverWithLinkWithRouter}
                            />
                            <PrivateRoute
                               path='/login'
---
+++ src/components/accounts/RecoverWithLink.js
@@ -0,0 +1,165 @@
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
+import styled from 'styled-components'
+import Button from '../common/Button'
+import { 
+    recoverAccountSeedPhrase,
+    handleRefreshAccount 
+} from '../../actions/account'
+import { Snackbar, snackbarDuration } from '../common/Snackbar'
+import { Translate } from 'react-localize-redux'
+
+const Container = styled.div`
+
+`
+
+const Title = styled.h1`
+    margin-bottom: 10px;
+    
+    @media (min-width: 768px) {
+        margin-bottom: 0;
+    }
+`
+
+const Desc = styled.div`
+    color: #4a4f54;
+    font-size: 18px;
+    line-height: 150%;
+    font-family: BwSeidoRound;
+    margin-top: ${props => props.last ? "20px" : "0"};
+
+    @media (min-width: 768px) {
+        font-size: 28px;
+    }
+`
+
+const UserName = styled.span`
+    color: #24272a;
+    background-color: #f8f8f8;
+    padding: 5px;
+`
+
+const ButtonWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    @media (min-width: 768px) {
+        flex-direction: row;
+    }
+
+    button {
+
+        @media (min-width: 768px) {
+            max-width: 300px;
+        }
+
+        &:last-of-type {
+            margin-top: 25px;
+            position: relative;
+            overflow: hidden;
+
+            @media (min-width: 768px) {
+                margin-left: 25px;
+                margin-top: 0;
+            }
+
+            color: #6AD1E3;
+            border: 2px solid #6AD1E3;
+            background-color: white;
+
+            &:hover {
+                color: white;
+                background-color: #6AD1E3;
+            }
+        }
+    }
+`
+
+const RecoverUrl = styled.div`
+    position: absolute;
+    z-index: -1;
+    text-transform: initial;
+`
+
+class RecoverWithLink extends Component {
+    constructor(props) {
+        super(props);
+
+        this.recoverUrl = React.createRef();
+
+        this.state = {
+            accountId: this.props.accountId,
+            seedPhrase: this.props.seedPhrase,
+            successSnackbar: false
+        };
+    }
+
+    handleCopyUrl = () => {
+        const selection = window.getSelection();
+        selection.selectAllChildren(this.recoverUrl.current);
+        document.execCommand('copy');
+        this.setState({ successSnackbar: true }, () => {
+            setTimeout(() => {
+                this.setState({successSnackbar: false});
+            }, snackbarDuration)
+        });
+    }
+
+    handleContinue = () => {
+        this.props.recoverAccountSeedPhrase(this.state.seedPhrase, this.state.accountId)
+            .then(({ error }) => {
+                if (error) return;
+                this.props.handleRefreshAccount();
+            }).then(() => {
+                this.props.history.push('/profile');
+            })
+    }
+
+    render() {
+        
+        return (
+            <Translate>
+                {({ translate }) => (
+                    <Container className='ui container'>
+                        <Title>{translate('recoverWithLink.title')}</Title>
+                        <Desc>{translate('recoverWithLink.pOne')} <UserName>@{this.state.accountId}</UserName></Desc>
+                        <Desc last>{translate('recoverWithLink.pTwo')}</Desc>
+                        <ButtonWrapper>
+                            <Button onClick={this.handleContinue}>
+                                {translate('button.continue')}
+                            </Button>
+                            <Button onClick={this.handleCopyUrl}>
+                                {translate('button.copyUrl')}
+                                <RecoverUrl ref={this.recoverUrl}>{window.location.href}</RecoverUrl>
+                            </Button>
+                        </ButtonWrapper>
+                        <Snackbar
+                            theme='success'
+                            message={translate('recoverWithLink.snackbarCopySuccess')}
+                            show={this.state.successSnackbar}
+                            onHide={() => this.setState({ successSnackbar: false })}
+                        />
+                    </Container>
+                )}
+            </Translate>
+        )
+    }
+}
+
+const mapDispatchToProps = {
+    recoverAccountSeedPhrase, 
+    handleRefreshAccount
+}
+
+const mapStateToProps = ({ account }, { match }) => ({
+    ...account,
+    accountId: match.params.accountId || '',
+    seedPhrase: match.params.seedPhrase || '',
+})
+
+export const RecoverWithLinkWithRouter = connect(
+    mapStateToProps, 
+    mapDispatchToProps
+)(withRouter(RecoverWithLink))
