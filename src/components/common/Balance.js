da2ed7cc92975ddef08231e7e1a685cb3ae6ba37
marcinbodnardesktop <me@marcinbodnar.com>
2020-01-31 03:15:32 +0100

    Display yoctoN in title tag if amount is <=0.0001 (#290)

--- src/components/common/Balance.js
+++ src/components/common/Balance.js
@@ -8,24 +8,35 @@ const CustomDiv = styled(List)`
     display: inline;
 `
 
+const FRAC_DIGITS = 5
+
 const Balance = ({ amount }) => {
     if (!amount) {
         throw new Error("amount property should not be null")
     }
     let amountShow = formatNEAR(amount)
+
     return (
-        <CustomDiv>
+        <CustomDiv title={['0','0.0001'].includes(utils.format.formatNearAmount(amount, (FRAC_DIGITS || 1) - 1)) ? `${formatWithCommas(amount)} yoctoⓃ` : ``}>
             {amountShow} Ⓝ
         </CustomDiv>
     )
 }
 
 export const formatNEAR = (amount) => {
-    let ret =  utils.format.formatNearAmount(amount, 5);
+    let ret =  utils.format.formatNearAmount(amount, FRAC_DIGITS);
     if (ret === '0') {
-        return "<0.00001";
+        return `<${!FRAC_DIGITS ? `0` : `0.${'0'.repeat((FRAC_DIGITS || 1) - 1)}1`}`
     }
     return ret;
+}
+
+const formatWithCommas = (value) => {
+    const pattern = /(-?\d+)(\d{3})/
+    while (pattern.test(value)) {
+        value = value.replace(pattern, '$1,$2')
+    }
+    return value
 }
 
 export default Balance
