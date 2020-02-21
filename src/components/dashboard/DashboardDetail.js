427b9093707d97d307c3ea0e7e75356c9bd0fccf
marcinbodnardesktop <me@marcinbodnar.com>
2020-02-21 01:21:51 +0100

    Change indentation for DashboardDetails component

--- src/components/dashboard/DashboardDetail.js
+++ src/components/dashboard/DashboardDetail.js
@@ -19,121 +19,121 @@ import AccessKeysIcon from '../../images/icon-keys-grey.svg'
 import DashboardKeys from './DashboardKeys'
 
 class DashboardDetail extends Component {
-   state = {
-      loader: false,
-      notice: false
-   }
+    state = {
+        loader: false,
+        notice: false
+    }
 
-   componentDidMount() {
-      this.refreshAccessKeys()
-      this.refreshTransactions()
+    componentDidMount() {
+        this.refreshAccessKeys()
+        this.refreshTransactions()
 
-      this.setState(() => ({
-         loader: true
-      }))
-   }
+        this.setState(() => ({
+            loader: true
+        }))
+    }
 
-   refreshTransactions() {
-      this.props.getTransactions()
-   }
+    refreshTransactions() {
+        this.props.getTransactions()
+    }
 
-   refreshAccessKeys = () => {
-      this.setState(() => ({
-         loader: true
-      }))
+    refreshAccessKeys = () => {
+        this.setState(() => ({
+            loader: true
+        }))
 
-      this.props.getAccessKeys().then(() => {
-         this.setState(() => ({
-            loader: false
-         }))
-      })
-   }
+        this.props.getAccessKeys().then(() => {
+            this.setState(() => ({
+                loader: false
+            }))
+        })
+    }
 
-   handleNotice = () => {
-      this.setState(state => ({
-         notice: !state.notice
-      }))
-   }
+    handleNotice = () => {
+        this.setState(state => ({
+            notice: !state.notice
+        }))
+    }
 
-   render() {
-      const { loader, notice } = this.state
-      const { authorizedApps, fullAccessKeys, transactions, amount, accountId } = this.props
-      return (
-         <PageContainer
-            title={(
-               amount
-                  ? <Fragment>
-                     <span className='balance'>Balance: </span>
-                     <Balance amount={amount} />
-                  </Fragment>
-                  : "Balance loading"
-            )}
-            additional={(
-               <Link to='/send-money'>
-                  <FormButton color='green-white-arrow' >
-                     SEND
-                  </FormButton>
-               </Link>
-            )}
-         >
-            <DashboardSection
-               notice={notice}
-               handleNotice={this.handleNotice}
+    render() {
+        const { loader, notice } = this.state
+        const { authorizedApps, fullAccessKeys, transactions, amount, accountId } = this.props
+        return (
+            <PageContainer
+                title={(
+                    amount
+                        ? <Fragment>
+                            <span className='balance'>Balance: </span>
+                            <Balance amount={amount} />
+                        </Fragment>
+                        : "Balance loading"
+                )}
+                additional={(
+                    <Link to='/send-money'>
+                        <FormButton color='green-white-arrow' >
+                            SEND
+                        </FormButton>
+                    </Link>
+                )}
             >
-               <DashboardActivity
-                  loader={loader}
-                  image={activityGreyImage}
-                  title='Activity'
-                  to={`${process.env.EXPLORER_URL || 'https://explorer.nearprotocol.com'}/accounts/${accountId}`}
-                  transactions={transactions}
-                  maxItems={5}
-                  accountId={accountId}
-               />
-               <DashboardKeys
-                  image={AuthorizedGreyImage}
-                  title='Authorized Apps'
-                  to='/authorized-apps'
-                  empty='You have not authorized any apps'
-                  accessKeys={authorizedApps}
-               />
-               <DashboardKeys
-                  image={AccessKeysIcon}
-                  title='Full Access Keys'
-                  to='/full-access-keys'
-                  empty='You have no full access keys'
-                  accessKeys={fullAccessKeys}
-               />
-            </DashboardSection>
-         </PageContainer>
-      )
-   }
+                <DashboardSection
+                    notice={notice}
+                    handleNotice={this.handleNotice}
+                >
+                    <DashboardActivity
+                        loader={loader}
+                        image={activityGreyImage}
+                        title='Activity'
+                        to={`${process.env.EXPLORER_URL || 'https://explorer.nearprotocol.com'}/accounts/${accountId}`}
+                        transactions={transactions}
+                        maxItems={5}
+                        accountId={accountId}
+                    />
+                    <DashboardKeys
+                        image={AuthorizedGreyImage}
+                        title='Authorized Apps'
+                        to='/authorized-apps'
+                        empty='You have not authorized any apps'
+                        accessKeys={authorizedApps}
+                    />
+                    <DashboardKeys
+                        image={AccessKeysIcon}
+                        title='Full Access Keys'
+                        to='/full-access-keys'
+                        empty='You have no full access keys'
+                        accessKeys={fullAccessKeys}
+                    />
+                </DashboardSection>
+            </PageContainer>
+        )
+    }
 }
 
 const mapDispatchToProps = {
-   getAccessKeys,
-   getTransactions
+    getAccessKeys,
+    getTransactions
 }
 
 // make sure that an action is an object, for UI purpose
 const postprocessSerdeStruct = (action) => typeof action == 'object' ? [action] : [{[action]: {}}]
 
 const mapStateToProps = ({ account }) => {
-   const transactions = account.transactions 
-      ? account.transactions.flatMap(t => t.actions.map((a) => ({
-         ...t,
-         action: postprocessSerdeStruct(a)
-      })))
-      : []
+    const transactions = account.transactions 
+        ? account.transactions.flatMap(t => t.actions.map((a) => ({
+            ...t,
+            action: postprocessSerdeStruct(a)
+        })))
+        : []
 
-   return {
-      ...account,
-      authorizedApps: account.authorizedApps,
-      fullAccessKeys: account.fullAccessKeys,
-      transactions
-   }
+    return {
+        ...account,
+        authorizedApps: account.authorizedApps,
+        fullAccessKeys: account.fullAccessKeys,
+        transactions
+    }
 }
 
 export default connect(
-   mapStateToProps,
-   mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(withRouter(DashboardDetail))
