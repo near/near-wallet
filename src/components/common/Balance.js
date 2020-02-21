2fd7ceae415efd03a397a5c19866222bb6ff22a5
Patrick Tajima <patrick.tajima@gmail.com>
2020-02-21 11:50:46 -0800

    Sign TX: convert tx value to string before formatting

--- src/components/common/Balance.js
+++ src/components/common/Balance.js
@@ -47,7 +47,7 @@ const showInYocto = (amountStr) => {
 const formatWithCommas = (value) => {
     const pattern = /(-?\d+)(\d{3})/
     while (pattern.test(value)) {
-        value = value.replace(pattern, '$1,$2')
+        value = value.toString().replace(pattern, '$1,$2')
     }
     return value
 }
