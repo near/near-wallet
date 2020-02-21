2fd17eba16b97df11780f3332c597270fb40a80d
marcinbodnardesktop <me@marcinbodnar.com>
2020-02-21 21:15:49 +0100

    Change indentation for SendMoneySecondStep component

--- src/components/send-money/SendMoneySecondStep.js
+++ src/components/send-money/SendMoneySecondStep.js
@@ -8,83 +8,83 @@ import FormButton from '../common/FormButton'
 import Balance from '../common/Balance'
 
 const SendMoneySecondStep = ({
-   handleNextStep,
-   handleExpandNote,
-   handleGoBack,
-   handleCancelTransfer,
-   expandNote,
-   note,
-   amount,
-   accountId,
-   loader
+    handleNextStep,
+    handleExpandNote,
+    handleGoBack,
+    handleCancelTransfer,
+    expandNote,
+    note,
+    amount,
+    accountId,
+    loader
 }) => (
-   <MobileContainer>
-      <Fragment>
-         <PageContainer
-            title={`Send`}
-            type='center'
-         />
-         <List className='list-top border'>
-            <List.Item as='h2' className='sending'>You are sending</List.Item>
-            <List.Item className='amount-sending border-bottom'>
-               {amount 
-               ? <Balance amount={amount} /> 
-               : "NaN"}
-            </List.Item>
-            <List.Item className='to'>
-               <Header as='h2'>to</Header>
-            </List.Item>
-            <List.Item as='h2'>{accountId}</List.Item>
-            <List.Item>@{accountId}</List.Item>
-            {note && (
-               <List.Item className='with-note '>
-                  with note:
-                  <br />
-                  {expandNote ? (
-                     <span className='color-black'>{note}</span>
-                  ) : (
-                     <span className='expand-note' onClick={handleExpandNote}>
-                        Expand note
-                     </span>
-                  )}
-               </List.Item>
-            )}
-         </List>
-      </Fragment>
-      <Fragment>
-         <List className='list-bottom border'>
-            <List.Item className='send-money border-top'>
-               <FormButton
-                  onClick={handleNextStep}
-                  color='green'
-                  disabled={loader}
-                  sending={loader}
-               >
-                  <Translate id='button.confirmAndSend' />
-               </FormButton>
-            </List.Item>
-            <List.Item className='confirmed'>Once confirmed, this cannot be undone.</List.Item>
-            <List.Item className='goback border-top'>
-               <FormButton
-                  onClick={handleGoBack}
-                  color='link bold'
-                  disabled={loader}
-               >
-                  <Translate id='button.needToEditGoBack' />
-               </FormButton>
-            </List.Item>
-         </List>
-         <List className='cancel'>
-            <FormButton
-                  onClick={handleCancelTransfer}
-                  color='link gray bold'
-                  disabled={loader}
-               >
-               <Translate id='button.cancelTransfer' />
-            </FormButton>
-         </List>
-      </Fragment>
-   </MobileContainer>
+    <MobileContainer>
+        <Fragment>
+            <PageContainer
+                title={`Send`}
+                type='center'
+            />
+            <List className='list-top border'>
+                <List.Item as='h2' className='sending'>You are sending</List.Item>
+                <List.Item className='amount-sending border-bottom'>
+                    {amount 
+                    ? <Balance amount={amount} /> 
+                    : "NaN"}
+                </List.Item>
+                <List.Item className='to'>
+                    <Header as='h2'>to</Header>
+                </List.Item>
+                <List.Item as='h2'>{accountId}</List.Item>
+                <List.Item>@{accountId}</List.Item>
+                {note && (
+                    <List.Item className='with-note '>
+                        with note:
+                        <br />
+                        {expandNote ? (
+                            <span className='color-black'>{note}</span>
+                        ) : (
+                            <span className='expand-note' onClick={handleExpandNote}>
+                                Expand note
+                            </span>
+                        )}
+                    </List.Item>
+                )}
+            </List>
+        </Fragment>
+        <Fragment>
+            <List className='list-bottom border'>
+                <List.Item className='send-money border-top'>
+                    <FormButton
+                        onClick={handleNextStep}
+                        color='green'
+                        disabled={loader}
+                        sending={loader}
+                    >
+                        <Translate id='button.confirmAndSend' />
+                    </FormButton>
+                </List.Item>
+                <List.Item className='confirmed'>Once confirmed, this cannot be undone.</List.Item>
+                <List.Item className='goback border-top'>
+                    <FormButton
+                        onClick={handleGoBack}
+                        color='link bold'
+                        disabled={loader}
+                    >
+                        <Translate id='button.needToEditGoBack' />
+                    </FormButton>
+                </List.Item>
+            </List>
+            <List className='cancel'>
+                <FormButton
+                        onClick={handleCancelTransfer}
+                        color='link gray bold'
+                        disabled={loader}
+                    >
+                    <Translate id='button.cancelTransfer' />
+                </FormButton>
+            </List>
+        </Fragment>
+    </MobileContainer>
 )
 
 export default SendMoneySecondStep
