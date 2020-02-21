6504811f4e2a42a9dabdda2994ab1e212a9ab372
marcinbodnardesktop <me@marcinbodnar.com>
2020-02-21 21:15:33 +0100

    Change indentation for SendMoneyThirdStep component

--- src/components/send-money/SendMoneyThirdStep.js
+++ src/components/send-money/SendMoneyThirdStep.js
@@ -9,44 +9,44 @@ import SignAnimatedArrow from '../sign/SignAnimatedArrow'
 import FormButton from '../common/FormButton'
 
 const SendMoneyThirdStep = ({ handleRedirectDashboard, note, amount, accountId }) => (
-   <MobileContainer>
-      <Fragment>
-         <PageContainer
-            title={`Success!`}
-            type='center'
-         />
-         <List className='list-top'>
-            <List.Item className='send-money'>
-               <SignAnimatedArrow />
-            </List.Item>
-            <List.Item as='h2' className='amount-sent'>
-               {amount 
-               ? <Balance amount={amount} /> 
-               : "NaN"}
-                  <span> was sent to:</span>
-            </List.Item>
-            <List.Item as='h2'>{accountId}</List.Item>
-            <List.Item>@{accountId}</List.Item>
-            {note && (
-               <List.Item className='with-note'>
-                  <span className='font-bold'>with note:</span>
-                  <br />
-                  {note}
-               </List.Item>
-            )}
-         </List>
-      </Fragment>
-      <Fragment>
-         <List className='list-bottom go-to-dashboard'>
-            <FormButton
-               onClick={handleRedirectDashboard}
-               color='seafoam-blue'
-            >
-               <Translate id='button.goToDashboard' />
-            </FormButton>
-         </List>
-      </Fragment>
-   </MobileContainer>
+    <MobileContainer>
+        <Fragment>
+            <PageContainer
+                title={`Success!`}
+                type='center'
+            />
+            <List className='list-top'>
+                <List.Item className='send-money'>
+                    <SignAnimatedArrow />
+                </List.Item>
+                <List.Item as='h2' className='amount-sent'>
+                    {amount 
+                    ? <Balance amount={amount} /> 
+                    : "NaN"}
+                        <span> was sent to:</span>
+                </List.Item>
+                <List.Item as='h2'>{accountId}</List.Item>
+                <List.Item>@{accountId}</List.Item>
+                {note && (
+                    <List.Item className='with-note'>
+                        <span className='font-bold'>with note:</span>
+                        <br />
+                        {note}
+                    </List.Item>
+                )}
+            </List>
+        </Fragment>
+        <Fragment>
+            <List className='list-bottom go-to-dashboard'>
+                <FormButton
+                    onClick={handleRedirectDashboard}
+                    color='seafoam-blue'
+                >
+                    <Translate id='button.goToDashboard' />
+                </FormButton>
+            </List>
+        </Fragment>
+    </MobileContainer>
 )
 
 export default SendMoneyThirdStep
