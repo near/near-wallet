1dbfc47afe2fa63dc0468b92b36d2742ede6a5de
Patrick Tajima <patrick.tajima@gmail.com>
2020-02-28 12:58:40 -0800

    Hide recaptcha badge and show alt disclaimer string

--- src/components/accounts/CreateAccountForm.js
+++ src/components/accounts/CreateAccountForm.js
@@ -3,11 +3,23 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import { Translate } from 'react-localize-redux'
 import { Header } from 'semantic-ui-react'
-
+import styled from 'styled-components'
 import ReCAPTCHA from 'react-google-recaptcha'
 
 import FormButton from '../common/FormButton'
 import AccountFormAccountId from './AccountFormAccountId'
+
+const RecaptchaString = styled.div`
+    margin-bottom: -10px;
+    font-size: 12px;
+    margin-top: 15px;
+    font-weight: 400;
+
+    a {
+        color: inherit;
+        text-decoration: underline;
+    }
+`
 
 const CreateAccountForm = ({
     loader,
@@ -29,6 +41,9 @@ const CreateAccountForm = ({
             checkAvailability={checkAvailability}
             requestStatus={requestStatus}
         />
+        <RecaptchaString>
+            This site is protected by reCAPTCHA and the Google <a href='https://policies.google.com/privacy' target='_blank' rel='noopener noreferrer'>Privacy Policy</a> and <a href='https://policies.google.com/terms' target='_blank' rel='noopener noreferrer'>Terms of Service</a> apply.
+        </RecaptchaString>
         {recaptchaTwo &&
             <ReCAPTCHA
                 sitekey='6LcZJtsUAAAAAN0hXzam-vEAPiFKMVsFY75Mn8AT'
