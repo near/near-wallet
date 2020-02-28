326be564900572bea40680186831d3ae62179e79
marcinbodnardesktop <me@marcinbodnar.com>
2020-02-28 21:31:48 +0100

    Change indentation for AccountNote component

--- src/components/common/AccountNote.js
+++ src/components/common/AccountNote.js
@@ -5,52 +5,52 @@ import { Translate } from 'react-localize-redux'
 import styled from 'styled-components'
 
 const CustomList = styled(List)`
-   &&& {
-      margin: 26px 0 0 1rem;
-      padding-left: 30px;
-      border-left: 4px solid #f8f8f8;
+    &&& {
+        margin: 26px 0 0 1rem;
+        padding-left: 30px;
+        border-left: 4px solid #f8f8f8;
 
-      .title {
-         letter-spacing: 2px;
-         font-weight: 600;
-         line-height: 20px;
-         color: #4a4f54;
-      }
-
-      @media screen and (max-width: 991px) {
-         margin: 26px 0 0 -1rem;
-         padding-left: 30px;
-         border-left: 4px solid #f8f8f8;
-
-         .title {
+        .title {
             letter-spacing: 2px;
             font-weight: 600;
             line-height: 20px;
             color: #4a4f54;
-         }
-      }
+        }
 
-      @media screen and (max-width: 767px) {
-         font-size: 12px;
-         padding-left: 1rem;
-      }
-   }
+        @media screen and (max-width: 991px) {
+            margin: 26px 0 0 -1rem;
+            padding-left: 30px;
+            border-left: 4px solid #f8f8f8;
+
+            .title {
+                letter-spacing: 2px;
+                font-weight: 600;
+                line-height: 20px;
+                color: #4a4f54;
+            }
+        }
+
+        @media screen and (max-width: 767px) {
+            font-size: 12px;
+            padding-left: 1rem;
+        }
+    }
 `
 
 const AccountNote = () => (
-   <CustomList>
-      <List.Item className='title'><Translate id='createAccount.note.title' /></List.Item>
-      <List.Item>
-         <Translate id='createAccount.note.one' />
-      </List.Item>
-      <List.Item>• <Translate id='createAccount.note.two' /></List.Item>
-      <List.Item>• <Translate id='createAccount.note.three' /></List.Item>
-      <List.Item>• <Translate id='createAccount.note.four' /></List.Item><br></br>
-      <List.Item>
-         <Translate id='createAccount.note.five' />
-      </List.Item>
-      <List.Item>• <Translate id='createAccount.note.six' /></List.Item>
-   </CustomList>
+    <CustomList>
+        <List.Item className='title'><Translate id='createAccount.note.title' /></List.Item>
+        <List.Item>
+            <Translate id='createAccount.note.one' />
+        </List.Item>
+        <List.Item>• <Translate id='createAccount.note.two' /></List.Item>
+        <List.Item>• <Translate id='createAccount.note.three' /></List.Item>
+        <List.Item>• <Translate id='createAccount.note.four' /></List.Item><br></br>
+        <List.Item>
+            <Translate id='createAccount.note.five' />
+        </List.Item>
+        <List.Item>• <Translate id='createAccount.note.six' /></List.Item>
+    </CustomList>
 )
 
 export default AccountNote
