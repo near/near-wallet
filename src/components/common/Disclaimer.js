347717207675201fa3bb93e852f4540a44da2af4
marcinbodnardesktop <me@marcinbodnar.com>
2020-02-28 21:31:59 +0100

    Change indentation for Disclaimer component

--- src/components/common/Disclaimer.js
+++ src/components/common/Disclaimer.js
@@ -5,39 +5,39 @@ import { Translate } from 'react-localize-redux'
 import styled from 'styled-components'
 
 const DisclaimerGrid = styled(Grid)`
-   && .disclaimer {
-      margin-top: 100px;
-      border-top: 2px solid #f8f8f8;
+    && .disclaimer {
+        margin-top: 100px;
+        border-top: 2px solid #f8f8f8;
 
-      > .column {
-         padding-left: 0px;
-         padding-right: 0px;
-      }
-   }
-   .disclaimer-info {
-      font-weight: 600;
-      letter-spacing: 2px;
-   }
+        > .column {
+            padding-left: 0px;
+            padding-right: 0px;
+        }
+    }
+    .disclaimer-info {
+        font-weight: 600;
+        letter-spacing: 2px;
+    }
 
-   @media screen and (max-width: 767px) {
-      && .disclaimer {
-         margin-top: 50px;
-         font-size: 12px;
-         margin-left: 1rem;
-         margin-right: 1rem;
-      }
-   }
+    @media screen and (max-width: 767px) {
+        && .disclaimer {
+            margin-top: 50px;
+            font-size: 12px;
+            margin-left: 1rem;
+            margin-right: 1rem;
+        }
+    }
 `
 
 const Disclaimer = () => (
-   <DisclaimerGrid>
-      <Grid.Row className='disclaimer'>
-         <Grid.Column computer={16} tablet={16} mobile={16}>
-            <span className='disclaimer-info'><Translate id='disclaimer.title' />: </span>
-            <Translate id='disclaimer.text' /> <a href='http://nearprotocol.com'>nearprotocol.com</a>
-         </Grid.Column>
-      </Grid.Row>
-   </DisclaimerGrid>
+    <DisclaimerGrid>
+        <Grid.Row className='disclaimer'>
+            <Grid.Column computer={16} tablet={16} mobile={16}>
+                <span className='disclaimer-info'><Translate id='disclaimer.title' />: </span>
+                <Translate id='disclaimer.text' /> <a href='http://nearprotocol.com'>nearprotocol.com</a>
+            </Grid.Column>
+        </Grid.Row>
+    </DisclaimerGrid>
 )
 
 export default Disclaimer
