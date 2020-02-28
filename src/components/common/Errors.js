055b650d393b07c8ba372546884549f2d79d2347
marcinbodnardesktop <me@marcinbodnar.com>
2020-02-28 21:32:12 +0100

    Change indentation for Errors component

--- src/components/common/Errors.js
+++ src/components/common/Errors.js
@@ -2,35 +2,35 @@ import React from 'react'
 import { Button, Divider } from 'semantic-ui-react'
 
 export class Error extends React.Component {
-   getErrorText() {
-      return 'An unexpected error occurred'
-   }
+    getErrorText() {
+        return 'An unexpected error occurred'
+    }
 
-   goBack = () => {
-      this.props.history.go(-2)
-   }
+    goBack = () => {
+        this.props.history.go(-2)
+    }
 
-   render() {
-      return (
-         <section className='App-section'>
-            <div className='App-error-message'>
-               <h2>
-                  {this.getErrorText()}
-                  <Divider />
-               </h2>
-               <Button onClick={this.goBack} content='Go back' />
-            </div>
-         </section>
-      )
-   }
+    render() {
+        return (
+            <section className='App-section'>
+                <div className='App-error-message'>
+                    <h2>
+                        {this.getErrorText()}
+                        <Divider />
+                    </h2>
+                    <Button onClick={this.goBack} content='Go back' />
+                </div>
+            </section>
+        )
+    }
 }
 
 export class NotFound extends Error {
-   goBack = () => {
-      this.props.history.go(-2)
-   }
+    goBack = () => {
+        this.props.history.go(-2)
+    }
 
-   getErrorText() {
-      return 'Not Found'
-   }
+    getErrorText() {
+        return 'Not Found'
+    }
 }
