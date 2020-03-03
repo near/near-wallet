3a2549fc43a6a81fe43fe136f532b07538f3eac8
marcinbodnardesktop <me@marcinbodnar.com>
2020-03-04 00:08:37 +0100

    Change indentation for StakingFormAmount component

--- src/components/node-staking/StakingFormAmount.js
+++ src/components/node-staking/StakingFormAmount.js
@@ -8,67 +8,67 @@ import styled from 'styled-components'
 const CustomFormInput = styled(Form.Input)``
 
 const CustomSegment = styled(Segment)`
-   &&& {
-      margin: 0px;
-      padding: 12px 0 0 0;
-   }
+    &&& {
+        margin: 0px;
+        padding: 12px 0 0 0;
+    }
 `
 
 class StakingFormAmount extends Component {
-   state = {
-      amountStaking: '',
-      amountPercent: 0
-   }
+    state = {
+        amountStaking: '',
+        amountPercent: 0
+    }
 
-   handleChangeAccountId = (e, { name, value }) => {
-      if (value.match(/[^0-9.]/)) {
-         return false
-      }
+    handleChangeAccountId = (e, { name, value }) => {
+        if (value.match(/[^0-9.]/)) {
+            return false
+        }
 
-      this.setState(() => ({
-         [name]: value,
-         amountPercent: Math.round((value / this.props.amount) * 100) / 100
-      }))
+        this.setState(() => ({
+            [name]: value,
+            amountPercent: Math.round((value / this.props.amount) * 100) / 100
+        }))
 
-      this.props.handleChange(e, { name, value })
-   }
+        this.props.handleChange(e, { name, value })
+    }
 
-   render () {
-      const { amount } = this.props
-      const { amountStaking, amountPercent } = this.state
+    render () {
+        const { amount } = this.props
+        const { amountStaking, amountPercent } = this.state
 
-      return (
-         <Fragment>
-            <CustomFormInput
-               className={`create ${
-                  amountStaking ? (amountPercent <= 100 ? 'success' : 'problem') : ''
-               }`}
-               name='amountStaking'
-               onChange={this.handleChangeAccountId}
-               placeholder='example: 15'
-               required
-               value={amountStaking}
-               autoComplete='off'
-               autoCorrect='off'
-               spellCheck='false'
-               tabIndex='1'
-            />
-            <CustomSegment basic textAlign='right' className={`color-black ${amountPercent <= 100 ? `` : `color-red`}`}>
-               <b>{amountPercent}% of Total: {amount}</b>
-            </CustomSegment>
-         </Fragment>
-      )
-   }
+        return (
+            <Fragment>
+                <CustomFormInput
+                    className={`create ${
+                        amountStaking ? (amountPercent <= 100 ? 'success' : 'problem') : ''
+                    }`}
+                    name='amountStaking'
+                    onChange={this.handleChangeAccountId}
+                    placeholder='example: 15'
+                    required
+                    value={amountStaking}
+                    autoComplete='off'
+                    autoCorrect='off'
+                    spellCheck='false'
+                    tabIndex='1'
+                />
+                <CustomSegment basic textAlign='right' className={`color-black ${amountPercent <= 100 ? `` : `color-red`}`}>
+                    <b>{amountPercent}% of Total: {amount}</b>
+                </CustomSegment>
+            </Fragment>
+        )
+    }
 }
 
 StakingFormAmount.propTypes = {
-   handleChange: PropTypes.func.isRequired
+    handleChange: PropTypes.func.isRequired
 }
 
 const mapStateToProps = ({ account }) => ({
-   ...account,
+    ...account,
 })
 
 export default connect(
-   mapStateToProps
+    mapStateToProps
 )(StakingFormAmount)
