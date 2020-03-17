47bec7354bc2fb80c63cb87d570fece06bca6076
marcinbodnardesktop <me@marcinbodnar.com>
2020-03-17 01:18:01 +0100

    Change indentation for NodeStaking component

--- src/components/node-staking/NodeStaking.js
+++ src/components/node-staking/NodeStaking.js
@@ -9,33 +9,33 @@ import NodeStakingNodes from './NodeStakingNodes'
 import NodeStakingStaking from './NodeStakingStaking'
 
 class NodeStaking extends Component {
-   state = {
-      nodes: [{online: true}, {online: false}],
-      // nodes: [],
+    state = {
+        nodes: [{online: true}, {online: false}],
+        // nodes: [],
 
-      staking: [{}, {}],
-      // staking: []
-   }
+        staking: [{}, {}],
+        // staking: []
+    }
 
-   render() {
-      const { nodes, staking } = this.state
+    render() {
+        const { nodes, staking } = this.state
 
-      return (
-         <NodeStakingContainer>
-            <PageContainer
-               title={<Translate id='nodeStaking.pageTitle' />}
-               additional={<h1><Translate id='nodeStaking.pageText' /></h1>}
-            />
-            {!nodes.length && <NodeStakingSteps />}
-            <NodeStakingNodes
-               nodes={nodes}
-            />
-            <NodeStakingStaking
-               staking={staking}
-            />
-         </NodeStakingContainer>
-      )
-   }
+        return (
+            <NodeStakingContainer>
+                <PageContainer
+                    title={<Translate id='nodeStaking.pageTitle' />}
+                    additional={<h1><Translate id='nodeStaking.pageText' /></h1>}
+                />
+                {!nodes.length && <NodeStakingSteps />}
+                <NodeStakingNodes
+                    nodes={nodes}
+                />
+                <NodeStakingStaking
+                    staking={staking}
+                />
+            </NodeStakingContainer>
+        )
+    }
 }
 
 export const NodeStakingWithRouter = withRouter(NodeStaking)
