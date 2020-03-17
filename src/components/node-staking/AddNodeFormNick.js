9277d1842fdab24b2b4981ada43019b99e74fe9f
marcinbodnardesktop <me@marcinbodnar.com>
2020-03-17 01:17:16 +0100

    Change indentation for AddNodeFormNick component

--- src/components/node-staking/AddNodeFormNick.js
+++ src/components/node-staking/AddNodeFormNick.js
@@ -8,44 +8,44 @@ import styled from 'styled-components'
 const CustomFormInput = styled(Form.Input)``
 
 class AddNodeFormNick extends Component {
-   state = {
-      nickname: '',
-   }
+    state = {
+        nickname: '',
+    }
 
-   handleChangeNick = (e, { name, value }) => {
-      this.setState(() => ({
-         [name]: value
-      }))
+    handleChangeNick = (e, { name, value }) => {
+        this.setState(() => ({
+            [name]: value
+        }))
 
-      this.props.handleChange(e, { name, value })
-   }
+        this.props.handleChange(e, { name, value })
+    }
 
-   render () {
-      const { nickname } = this.state
+    render () {
+        const { nickname } = this.state
 
-      return (
-         <Translate>
-            {({ translate }) => (
-               <CustomFormInput
-                  className='create'
-                  name='nickname'
-                  onChange={this.handleChangeNick}
-                  placeholder={translate('addNode.nicknameInput.placeholder')}
-                  required
-                  value={nickname}
-                  autoComplete='off'
-                  autoCorrect='off'
-                  spellCheck='false'
-                  tabIndex='2'
-               />
-            )}
-         </Translate>
-      )
-   }
+        return (
+            <Translate>
+                {({ translate }) => (
+                    <CustomFormInput
+                        className='create'
+                        name='nickname'
+                        onChange={this.handleChangeNick}
+                        placeholder={translate('addNode.nicknameInput.placeholder')}
+                        required
+                        value={nickname}
+                        autoComplete='off'
+                        autoCorrect='off'
+                        spellCheck='false'
+                        tabIndex='2'
+                    />
+                )}
+            </Translate>
+        )
+    }
 }
 
 AddNodeFormNick.propTypes = {
-   handleChange: PropTypes.func.isRequired
+    handleChange: PropTypes.func.isRequired
 }
 
 export default AddNodeFormNick
