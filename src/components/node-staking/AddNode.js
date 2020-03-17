400a1002fcc8304d00dd7ad83f4c2ceeba23229b
marcinbodnardesktop <me@marcinbodnar.com>
2020-03-17 01:16:49 +0100

    Change indentation for AddNode component

--- src/components/node-staking/AddNode.js
+++ src/components/node-staking/AddNode.js
@@ -8,63 +8,63 @@ import AccountFormSection from '../accounts/AccountFormSection'
 import AddNodeForm from './AddNodeForm'
 
 class AddNode extends Component {
-   state = {
-      loader: false,
-      ipAddress: '',
-      nickname: ''
-   }
+    state = {
+        loader: false,
+        ipAddress: '',
+        nickname: ''
+    }
 
-   handleChange = (e, { name, value }) => {
-      this.setState(() => ({
-         [name]: value
-      }))
-   }
+    handleChange = (e, { name, value }) => {
+        this.setState(() => ({
+            [name]: value
+        }))
+    }
 
-   handleSubmit = e => {
-      e.preventDefault()
+    handleSubmit = e => {
+        e.preventDefault()
 
-      if (!this.isLegitForm()) {
-         return false
-      }
+        if (!this.isLegitForm()) {
+            return false
+        }
 
-      this.setState(() => ({
-         loader: true
-      }))
+        this.setState(() => ({
+            loader: true
+        }))
 
-      setTimeout(() => {
-         this.setState(() => ({
-            loader: false
-         }))
-      }, 1500);
+        setTimeout(() => {
+            this.setState(() => ({
+                loader: false
+            }))
+        }, 1500);
 
-   }
+    }
 
-   isLegitForm = () => {
-      return this.state.ipAddress.match(/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/)
-   }
+    isLegitForm = () => {
+        return this.state.ipAddress.match(/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/)
+    }
 
-   render() {
-      const { loader } = this.state
+    render() {
+        const { loader } = this.state
 
-      return (
-         <AccountFormContainer 
-            location={this.props.location}
-            title={<Translate id='addNode.pageTitle' />}
-            text={<Translate id='addNode.pageText' />}
-            disclaimer={false}
-         >
-            <AccountFormSection 
-               handleSubmit={this.handleSubmit}
+        return (
+            <AccountFormContainer 
+                location={this.props.location}
+                title={<Translate id='addNode.pageTitle' />}
+                text={<Translate id='addNode.pageText' />}
+                disclaimer={false}
             >
-               <AddNodeForm
-                  loader={loader} 
-                  handleChange={this.handleChange}
-                  isLegitForm={this.isLegitForm}
-               />
-            </AccountFormSection>
-         </AccountFormContainer>
-      )
-   }
+                <AccountFormSection 
+                    handleSubmit={this.handleSubmit}
+                >
+                    <AddNodeForm
+                        loader={loader} 
+                        handleChange={this.handleChange}
+                        isLegitForm={this.isLegitForm}
+                    />
+                </AccountFormSection>
+            </AccountFormContainer>
+        )
+    }
 }
 
 export const AddNodeWithRouter = withRouter(AddNode)
