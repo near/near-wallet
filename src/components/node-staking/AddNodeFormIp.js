60dc162dac98b55825837ec698d59e7b72535182
marcinbodnardesktop <me@marcinbodnar.com>
2020-03-17 01:17:00 +0100

    Change indentation for AddNodeFormIp component

--- src/components/node-staking/AddNodeFormIp.js
+++ src/components/node-staking/AddNodeFormIp.js
@@ -9,48 +9,48 @@ const CustomFormInput = styled(Form.Input)`
 `
 
 class AddNodeFormIp extends Component {
-   state = {
-      ipAddress: ''
-   }
+    state = {
+        ipAddress: ''
+    }
 
-   handleChangeIp = (e, { name, value }) => {
-      if (value.match(/[^0-9.]/)) {
-         return false
-      }
-      
-      this.setState(() => ({
-         [name]: value
-      }))
+    handleChangeIp = (e, { name, value }) => {
+        if (value.match(/[^0-9.]/)) {
+            return false
+        }
+        
+        this.setState(() => ({
+            [name]: value
+        }))
 
-      this.props.handleChange(e, { name, value })
-   }
+        this.props.handleChange(e, { name, value })
+    }
 
-   render () {
-      const { ipAddress } = this.state
+    render () {
+        const { ipAddress } = this.state
 
-      return (
-         <Translate>
-            {({ translate }) => (
-               <CustomFormInput
-                  className='create'
-                  name='ipAddress'
-                  onChange={this.handleChangeIp}
-                  placeholder={translate('addNode.ipAddressInput.placeholder')}
-                  required
-                  value={ipAddress}
-                  autoComplete='off'
-                  autoCorrect='off'
-                  spellCheck='false'
-                  tabIndex='1'
-               />
-            )}
-         </Translate>
-      )
-   }
+        return (
+            <Translate>
+                {({ translate }) => (
+                    <CustomFormInput
+                        className='create'
+                        name='ipAddress'
+                        onChange={this.handleChangeIp}
+                        placeholder={translate('addNode.ipAddressInput.placeholder')}
+                        required
+                        value={ipAddress}
+                        autoComplete='off'
+                        autoCorrect='off'
+                        spellCheck='false'
+                        tabIndex='1'
+                    />
+                )}
+            </Translate>
+        )
+    }
 }
 
 AddNodeFormIp.propTypes = {
-   handleChange: PropTypes.func.isRequired
+    handleChange: PropTypes.func.isRequired
 }
 
 export default AddNodeFormIp
