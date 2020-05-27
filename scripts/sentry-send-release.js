70c00ce99b9fd953a664f17a94075858b653cb42
Vladimir Grichina <vgrichina@gmail.com>
2020-05-27 16:42:58 -0700

    Update script to upload Sentry release info

--- scripts/sentry-send-release.js
+++ scripts/sentry-send-release.js
@@ -1,5 +1,16 @@
 if (process.env.RENDER) {
     const { SENTRY_RELEASE } = require('../src/utils/sentry');
     const { execSync } = require('child_process');
-    console.log(execSync(`node_modules/.bin/sentry-cli releases set-commits --auto ${SENTRY_RELEASE}`));
+
+    const system = (command) => {
+        let result = execSync(command);
+        if (Buffer.isBuffer(result)) {
+            result = result.toString('utf-8');
+        }
+        console.log(result);
+    };
+
+    system(`node_modules/.bin/sentry-cli releases files ${SENTRY_RELEASE} upload-sourcemaps dist/`);
+    system(`node_modules/.bin/sentry-cli releases set-commits --auto ${SENTRY_RELEASE}`);
+    system(`node_modules/.bin/sentry-cli releases finalize ${SENTRY_RELEASE}`);
 }
