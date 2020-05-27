d5593fdfa9b3832767a790f3a347d4eefd0287dd
Vladimir Grichina <vgrichina@gmail.com>
2020-05-27 15:49:43 -0700

    Add release info for Sentry

--- src/index.js
+++ src/index.js
@@ -15,7 +15,15 @@ import createMiddleware from './middleware'
 
 import Routing from './components/Routing'
 
-Sentry.init({dsn: "https://75d1dabd0ab646329fad8a3e7d6c761d@o398573.ingest.sentry.io/5254526"});
+const SENTRY_RELEASE = process.env.SENTRY_RELEASE ||
+    (process.env.RENDER &&
+        `render:${process.env.RENDER_SERVICE_NAME}:${process.env.RENDER_GIT_BRANCH}:${process.env.RENDER_GIT_COMMIT}`)
+    || 'development';
+
+Sentry.init({
+    dsn: "https://75d1dabd0ab646329fad8a3e7d6c761d@o398573.ingest.sentry.io/5254526",
+    release: SENTRY_RELEASE
+});
 
 const history = createBrowserHistory()
 
