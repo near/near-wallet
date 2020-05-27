b5f1bdda4e340247c61aed614d926186e6c488b8
Vladimir Grichina <vgrichina@gmail.com>
2020-05-26 17:00:59 -0700

    Integrate Sentry

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import "regenerator-runtime/runtime";
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import * as Sentry from '@sentry/browser';
+
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import { LocalizeProvider } from 'react-localize-redux';
@@ -12,6 +14,8 @@ import createRootReducer from './reducers'
 import createMiddleware from './middleware'
 
 import Routing from './components/Routing'
+
+Sentry.init({dsn: "https://75d1dabd0ab646329fad8a3e7d6c761d@o398573.ingest.sentry.io/5254526"});
 
 const history = createBrowserHistory()
 
