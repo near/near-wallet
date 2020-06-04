f7fa0342debfd6c8c3be08b42fe0c610e9a9c585
Kendall Cole <kcole16@gmail.com>
2020-06-04 16:26:17 -0700

    Remove all urls with seed phrase from sentry

--- src/utils/sentry.js
+++ src/utils/sentry.js
@@ -7,7 +7,21 @@ const SENTRY_RELEASE = process.env.SENTRY_RELEASE ||
 
 const initSentry = () => Sentry.init({
     dsn: "https://75d1dabd0ab646329fad8a3e7d6c761d@o398573.ingest.sentry.io/5254526",
-    release: SENTRY_RELEASE
+    release: SENTRY_RELEASE,
+    beforeSend(event) {
+        if (event.request.url.includes('recover-with-link')) {
+            delete event.request.url;
+        }
+        return event;
+    },
+    beforeBreadcrumb(breadcrumb) {
+        if (breadcrumb.category === 'navigation' && 
+                (breadcrumb.data.from.includes('recover-with-link') || 
+                    breadcrumb.data.to.includes('recover-with-link'))) {
+            delete breadcrumb.data;
+        }
+        return breadcrumb;
+    }
 });
 
 // NOTE: This module needs to be Node.js compatible as it's used in scripts
