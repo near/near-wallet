0ba120f4594bff0307970b871e75b39c4fbc8d6a
Patrick Tajima <patrick.tajima@gmail.com>
2020-07-14 18:09:01 -0700

    Enable two factor: fix layout

--- src/components/accounts/two_factor/EnableTwoFactor.js
+++ src/components/accounts/two_factor/EnableTwoFactor.js
@@ -14,19 +14,9 @@ import {
 } from '../../../actions/account';
 import { useRecoveryMethods } from '../../../hooks/recoveryMethods';
 import EnterVerificationCode from '../EnterVerificationCode'
+import Container from '../../common/styled/Container.css'
 
-const Container = styled.form`
-    h2 {
-        max-width: 800px;
-        color: #4a4f54 !important;
-
-        @media (max-width: 767px) {
-            font-size: 14px !important;
-            line-height: 18px !important;
-            color: #999 !important;
-            margin-bottom: -15px;
-        }
-    }
+const StyledContainer = styled(Container)`
 
     h4 {
         margin-top: 40px;
@@ -38,6 +28,7 @@ const Container = styled.form`
 
     button {
         margin-top: 50px !important;
+        width: 100% !important;
     }
 `
 
@@ -128,52 +119,54 @@ export function EnableTwoFactor(props) {
 
     if (!initiated) {
         return (
-            <Container className='ui container' onSubmit={e => {handleNext(); e.preventDefault();}}>
-                <h1><Translate id='twoFactor.enable' /></h1>
-                <h2><Translate id='twoFactor.subHeader' /></h2>
-                <h4><Translate id='twoFactor.select' /><span>*</span></h4>
-                <TwoFactorOption
-                    onClick={() => setOption('email')}
-                    option='email'
-                    active={option}
-                >
-                    <Translate>
-                        {({ translate }) => (
-                            <input
-                                type='email'
-                                placeholder={translate('setupRecovery.emailPlaceholder')}
-                                value={email}
-                                onChange={e => setEmail(e.target.value)}
-                                tabIndex='1'
-                            />
-                        )}
-                    </Translate>
-                </TwoFactorOption>
-                <TwoFactorOption
-                    onClick={() => setOption('phone')}
-                    option='phone'
-                    active={option}
-                >
-                    <Translate>
-                        {({ translate }) => (
-                            <PhoneInput
-                                placeholder={translate('setupRecovery.phonePlaceholder')}
-                                value={phoneNumber}
-                                onChange={value => setPhoneNumber(value)}
-                                tabIndex='1'
-                            />
-                        )}
-                    </Translate>
-                </TwoFactorOption>
-                <FormButton
-                    color='blue'
-                    type='submit'
-                    disabled={!isValidInput()}
-                    sending={account.actionsPending.includes('INIT_TWO_FACTOR')}
-                >
-                    <Translate id={`button.continue`}/>
-                </FormButton>
-            </Container>
+            <StyledContainer className='small-centered'>
+                <form onSubmit={e => {handleNext(); e.preventDefault();}}>
+                    <h1><Translate id='twoFactor.enable' /></h1>
+                    <h2><Translate id='twoFactor.subHeader' /></h2>
+                    <h4><Translate id='twoFactor.select' /><span>*</span></h4>
+                    <TwoFactorOption
+                        onClick={() => setOption('email')}
+                        option='email'
+                        active={option}
+                    >
+                        <Translate>
+                            {({ translate }) => (
+                                <input
+                                    type='email'
+                                    placeholder={translate('setupRecovery.emailPlaceholder')}
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
+                                    tabIndex='1'
+                                />
+                            )}
+                        </Translate>
+                    </TwoFactorOption>
+                    <TwoFactorOption
+                        onClick={() => setOption('phone')}
+                        option='phone'
+                        active={option}
+                    >
+                        <Translate>
+                            {({ translate }) => (
+                                <PhoneInput
+                                    placeholder={translate('setupRecovery.phonePlaceholder')}
+                                    value={phoneNumber}
+                                    onChange={value => setPhoneNumber(value)}
+                                    tabIndex='1'
+                                />
+                            )}
+                        </Translate>
+                    </TwoFactorOption>
+                    <FormButton
+                        color='blue'
+                        type='submit'
+                        disabled={!isValidInput()}
+                        sending={account.actionsPending.includes('INIT_TWO_FACTOR')}
+                    >
+                        <Translate id={`button.continue`}/>
+                    </FormButton>
+                </form>
+            </StyledContainer>
         )
     } else {
         return (
