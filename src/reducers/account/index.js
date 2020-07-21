5e23eb1e2aa64a992ae1610ea9bea2687d4ac6c7
marcinbodnardesktop <me@marcinbodnar.com>
2020-07-21 21:16:45 +0200

    Remove ledger actions from account reducer

--- src/reducers/account/index.js
+++ src/reducers/account/index.js
@@ -140,51 +140,6 @@ const account = handleActions({
     })
 }, initialState)
 
-const ledger = handleActions({
-    [getLedgerAccountIds]: (state, { error, payload }) => {
-        if (error) {
-            return {
-                ...state,
-                signInWithLedger: undefined
-            }
-        }
-
-        return {
-            ...state,
-            signInWithLedger: payload && payload.reduce((r, accountId) => ({
-                ...r,
-                [accountId]: ''
-            }), {})
-        }
-    },
-    [addLedgerAccountId]: (state, { error, payload, ready, meta }) => {
-        if (error) {
-            return {
-                ...state,
-                signInWithLedger: undefined
-            }
-        }
-
-        return {
-            ...state,
-            signInWithLedger: {
-                ...state.signInWithLedger,
-                [meta.accountId]: ready ? payload : 'waiting'
-            }
-        }
-    },
-    [saveAndSelectLedgerAccounts]: (state, { error }) => {
-        if (error) {
-            return {
-                ...state,
-                signInWithLedger: undefined
-            }
-        }
-
-        return state
-    },
-}, initialState)
-
 export default reduceReducers(
     initialState,
     loaderReducer,
@@ -194,6 +149,5 @@ export default reduceReducers(
     recoverCodeReducer,
     accessKeys,
     account,
-    url,
-    ledger
+    url
 )
