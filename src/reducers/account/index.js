60f55ffef73658a9aece970149d8574976d71dae
marcinbodnardesktop <me@marcinbodnar.com>
2020-07-29 18:27:32 +0200

    Show Global Alert for SendMoney action (#705)

--- src/reducers/account/index.js
+++ src/reducers/account/index.js
@@ -17,7 +17,8 @@ import {
     deleteRecoveryMethod,
     sendNewRecoveryLink,
     recoverAccountSeedPhrase,
-    saveAndSelectLedgerAccounts
+    saveAndSelectLedgerAccounts,
+    sendMoney
 } from '../../actions/account'
 
 const initialState = {
@@ -42,7 +43,7 @@ const loaderReducer = (state, { type, ready }) => {
 const globalAlertReducer = handleActions({
     // TODO: Reset state before action somehow. On navigate / start of other action?
     // TODO: Make this generic to avoid listing actions
-    [combineActions(addAccessKey, addAccessKeySeedPhrase, setupRecoveryMessage, saveAndSelectLedgerAccounts, deleteRecoveryMethod, sendNewRecoveryLink, recoverAccountSeedPhrase)]: (state, { error, ready, payload, meta }) => ({
+    [combineActions(addAccessKey, addAccessKeySeedPhrase, setupRecoveryMessage, saveAndSelectLedgerAccounts, deleteRecoveryMethod, sendNewRecoveryLink, recoverAccountSeedPhrase, sendMoney)]: (state, { error, ready, payload, meta }) => ({
         ...state,
         globalAlert: ready ? {
             success: !error,
