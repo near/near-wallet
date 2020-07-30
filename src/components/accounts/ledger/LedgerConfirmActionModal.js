c09565ca4a649e39daa9b666a7efd01f12f242ff
marcinbodnardesktop <me@marcinbodnar.com>
2020-07-31 01:23:24 +0200

    Implement new design for Sign in with Ledger modal

--- src/components/accounts/ledger/LedgerConfirmActionModal.js
+++ src/components/accounts/ledger/LedgerConfirmActionModal.js
@@ -1,50 +1,171 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Translate } from 'react-localize-redux';
+
 import Modal from "../../common/modal/Modal";
 import ModalTheme from './ModalTheme';
 import MobileActionSheet from '../../common/modal/MobileActionSheet';
-// import FormButton from '../../common/FormButton';
-import { Translate } from 'react-localize-redux';
 import LedgerImage from '../../svg/LedgerImage';
+import UserIconGrey from '../../../images/UserIconGrey';
 
-const LedgerConfirmActionModal = ({ open, onClose, ledgerAccounts, gettingAccounts, addingAccounts }) => {
+const UserIcon = styled.div`
+    background-size: 21px;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: #f8f8f8;
+    text-align: center;
+    margin: 0 12px 0 0;
+    
+    svg {
+        width: 26px;
+        height: 26px;
+        margin: 7px;
+    }
+
+    @media (min-width: 940px) {
+        display: inline-block;
+    }
+`
+
+const H4 = styled.div`
+    color: #999;
+    text-align: center;
+    padding: 30px 0 60px 0;
+    font-size: 16px;
+`
+
+const AnimateList = styled.div`
+    margin-top: 10px;
+    height: 180px;
+    overflow: hidden;
+
+    &.animate {
+        & > div:first-of-type {
+            margin-top: ${props => `-${props.animate * 60}px`};
+            transition: 1s;
+        }
+    }
+
+    .row {
+        border-top: 2px solid #f5f5f5;
+        display: flex;
+        height: 60px;
+        align-items: center;
+
+        &.success .status {
+            background: #8fd6bd;
+            color: #1f825f;
+        }
+        &.pending .status {
+            background: #f4c898;
+            color: #ae6816;
+            text-align: left;
+            padding: 0 0 0 10px;
+
+            :after {
+                content: '.';
+                animation: dots 1s steps(5, end) infinite;
+            
+                @keyframes dots {
+                    0%, 20% {
+                        color: rgba(0,0,0,0);
+                        text-shadow:
+                            .3em 0 0 rgba(0,0,0,0),
+                            .6em 0 0 rgba(0,0,0,0);
+                    }
+                    40% {
+                        color: #ae6816;
+                        text-shadow:
+                            .3em 0 0 rgba(0,0,0,0),
+                            .6em 0 0 rgba(0,0,0,0);
+                    }
+                    60% {
+                        text-shadow:
+                            .3em 0 0 #ae6816,
+                            .6em 0 0 rgba(0,0,0,0);
+                    }
+                    80%, 100% {
+                        text-shadow:
+                            .3em 0 0 #ae6816,
+                            .6em 0 0 #ae6816;
+                    }
+                }
+            }
+        }
+        &.waiting {
+            .status {
+                background: #f8f8f8;
+                color: #aaaaaa;
+            }
+            > div:first-of-type {
+                opacity: 0.4;
+            }
+        }
+
+        .status {
+            width: 72px;
+            margin-left: auto;
+            height: 24px;
+            border-radius: 12px;
+            text-align: center;
+            line-height: 24px;
+            font-size: 12px;
+        }
+    }
+`
+
+const LedgerConfirmActionModal = ({ open, onClose, ledgerAccounts, gettingAccounts, addingAccounts, accountsApproved, totalAccounts }) => {
     return (
         <Modal
             id='ledger-confirm-action-modal'
             isOpen={open}
             onClose={onClose}
             closeButton='desktop'
+            modalSize='lg'
         >
             <ModalTheme/>
             <MobileActionSheet/>
             <h2><Translate id='confirmLedgerModal.header'/></h2>
             <LedgerImage animate={true}/>
-            {/* <p><Translate id='confirmLedgerModal.one'/></p> */}
-            
-            {gettingAccounts && (
-                <h3>
-                    Confirm public key
-                </h3>
-            )}
-            {addingAccounts && (
-                <div>
-                    <h3>
-                        Confirm the add key operation for each account:
-                        {ledgerAccounts.map((account) => (
-                            <div style={{ display: 'flex', paddingTop: '1rem', color: account.status === 'pending' ? '#999' : account.status === 'success' ? '#5ace84' : '' }}>
-                                <div style={{ flex: '70%' }}>
-                                    {account.accountId}
+
+            <div>
+                <H4>
+                    {gettingAccounts
+                        ? 'Confirm public key'
+                        : 'Ledger must add an access key for each account protected by your device:'}
+                </H4>
+                {addingAccounts && (
+                    <>
+                        <div>
+                            {accountsApproved}/{totalAccounts} account(s) approved
+                        </div>
+                        <AnimateList className={accountsApproved ? 'animate' : ''} animate={accountsApproved}>
+                            {ledgerAccounts.map((account) => (
+                                <div key={account.accountId} className={`row ${account.status}`}>
+                                    <UserIcon>
+                                        <UserIconGrey color='#9a9a9a' />
+                                    </UserIcon>
+                                    <h3>
+                                        @{account.accountId}
+                                    </h3>
+                                    <div className='status'>
+                                        {account.status === 'success' && (
+                                            'Approved'
+                                        )}
+                                        {account.status === 'pending' && (
+                                            'Pending'
+                                        )}
+                                        {account.status === 'waiting' && (
+                                            'Up next'
+                                        )}
+                                    </div>
                                 </div>
-                                <div style={{ flex: '30%', textAlign: 'right' }}>
-                                    {account.status}
-                                </div>
-                            </div>
-                        ))}
-                    </h3>
-                </div>
-            )}
-            {/* <FormButton color='gray-red' id='close-button'>
-                <Translate id='button.cancelOperation'/>
-            </FormButton> */}
+                            ))}
+                        </AnimateList>
+                    </>
+                )}
+            </div>
         </Modal>
     );
 }
