75d6552dce8e9f40a7d44abfe273a588a11e4016
marcinbodnardesktop <me@marcinbodnar.com>
2020-08-04 23:07:00 +0200

    Show modal based on redux state (#705)

--- src/components/accounts/ledger/LedgerConfirmActionModal.js
+++ src/components/accounts/ledger/LedgerConfirmActionModal.js
@@ -1,29 +1,37 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Translate } from 'react-localize-redux';
+
 import Modal from "../../common/modal/Modal";
 import ModalTheme from './ModalTheme';
 import MobileActionSheet from '../../common/modal/MobileActionSheet';
 import FormButton from '../../common/FormButton';
-import { Translate } from 'react-localize-redux';
+
 import LedgerImage from '../../svg/LedgerImage';
 
-const LedgerConfirmActionModal = ({ open, onClose, textId }) => {
-    return (
-        <Modal
-            id='ledger-confirm-action-modal'
-            isOpen={open}
-            onClose={onClose}
-            closeButton='desktop'
-        >
-            <ModalTheme/>
-            <MobileActionSheet/>
-            <h2><Translate id='confirmLedgerModal.header'/></h2>
-            <LedgerImage animate={true}/>
-            <p><Translate id={textId}/></p>
-            <FormButton color='gray-red' id='close-button'>
-                <Translate id='button.cancelOperation'/>
-            </FormButton>
-        </Modal>
-    );
+const LedgerConfirmActionModal = () => {
+
+    const ledger = useSelector(({ account }) => account.ledger)
+
+    return (ledger && ledger.hasLedger && ledger.modal && ledger.modal.show)
+        ? (
+            <Modal
+                id='ledger-confirm-action-modal'
+                isOpen={open}
+                onClose={onClose}
+                closeButton='desktop'
+            >
+                <ModalTheme/>
+                <MobileActionSheet/>
+                <h2><Translate id='confirmLedgerModal.header'/></h2>
+                <LedgerImage animate={true}/>
+                <p><Translate id={}/></p>
+                <FormButton color='gray-red' id='close-button'>
+                    <Translate id='button.cancelOperation'/>
+                </FormButton>
+            </Modal>
+        )
+        : null
 }
 
 export default LedgerConfirmActionModal;
