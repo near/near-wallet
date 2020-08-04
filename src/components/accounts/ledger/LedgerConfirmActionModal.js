ca03f7425f66b1c0689f459d7efaf2652f0456ef
marcinbodnardesktop <me@marcinbodnar.com>
2020-08-04 23:10:14 +0200

    Cleanup for ledger modal and remove cancel button (#705)

--- src/components/accounts/ledger/LedgerConfirmActionModal.js
+++ src/components/accounts/ledger/LedgerConfirmActionModal.js
@@ -5,7 +5,6 @@ import { Translate } from 'react-localize-redux';
 import Modal from "../../common/modal/Modal";
 import ModalTheme from './ModalTheme';
 import MobileActionSheet from '../../common/modal/MobileActionSheet';
-import FormButton from '../../common/FormButton';
 
 import LedgerImage from '../../svg/LedgerImage';
 
@@ -17,8 +16,6 @@ const LedgerConfirmActionModal = () => {
         ? (
             <Modal
                 id='ledger-confirm-action-modal'
-                isOpen={open}
-                onClose={onClose}
                 closeButton='desktop'
             >
                 <ModalTheme/>
@@ -26,8 +23,6 @@ const LedgerConfirmActionModal = () => {
                 <h2><Translate id='confirmLedgerModal.header'/></h2>
                 <LedgerImage animate={true}/>
                 <p><Translate id={ledger.modal.textId}/></p>
-                    <Translate id='button.cancelOperation'/>
-                </FormButton>
             </Modal>
         )
         : null
