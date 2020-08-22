6ec00edaf71f27c2e10677d203780f783621a30a
Vladimir Grichina <vgrichina@gmail.com>
2020-08-21 22:50:38 -0700

    Make sure to save account key before creating account

--- src/utils/wallet.js
+++ src/utils/wallet.js
@@ -322,6 +322,7 @@ class Wallet {
     async createNewAccount(accountId, fundingContract, fundingKey) {
         this.checkNewAccount(accountId);
         const keyPair = KeyPair.fromRandom('ed25519');
+        this.saveAccount(accountId, keyPair)
 
         try {
             if (fundingContract && fundingKey) {
