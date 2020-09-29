4747f046981684a7fcc9efb83c8c180d04223c4b
Patrick Tajima <patrick.tajima@gmail.com>
2020-09-28 17:25:46 -0700

    show valid amount indicator on UI

--- src/components/svg/ArrowCircleIcon.js
+++ src/components/svg/ArrowCircleIcon.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const ArrowCircleIcon = () => {
+const ArrowCircleIcon = ({ color }) => {
+    const stroke = color || '#CCCCCC'
     return (
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className='arrow-circle'>
-            <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#CCCCCC" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-            <path d="M8 12L12 16L16 12" stroke="#CCCCCC" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-            <path d="M12 8V16" stroke="#CCCCCC" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke={stroke} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M8 12L12 16L16 12" stroke={stroke} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M12 8V16" stroke={stroke} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
     )
 }
