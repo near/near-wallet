927267ac097d13e33330c019af9378fafcdb1e43
marcinbodnardesktop <me@marcinbodnar.com>
2020-09-29 23:57:39 +0200

    Create account - clear account state if addinh key fails (#911)

--- src/actions/account.js
+++ src/actions/account.js
@@ -377,6 +377,7 @@ export const { addAccessKey, createAccountWithSeedPhrase, addAccessKeySeedPhrase
                 await wallet.addAccessKey(accountId, contractName, newPublicKey, fullAccess)
                 await wallet.saveAccount(accountId, newKeyPair)
             } catch (error) {
+                wallet.clearAccountState()
                 throw new WalletError(error, 'account.create.addAccessKey.error')
             }
         },
