5f7ef5eb96928369a269d425e67f4b4485129c9c
marcinbodnardesktop <me@marcinbodnar.com>
2020-10-09 18:44:21 +0200

    Show proper details page if lockedAmount found (#981)

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -86,6 +86,7 @@ class Login extends Component {
                             {...props}
                             contractId={url && url.contract_id}
                             appTitle={url && url.title}
+                            fullAccess={fullAccess}
                         />
                     )}
                 />
