3113b97b0b1039bb59719643fb3c4ddbee23a8e4
Matt Lockyer <mattdlockyer@gmail.com>
2020-10-12 18:27:30 -0700

    fix formatNear

--- src/components/common/Balance.js
+++ src/components/common/Balance.js
@@ -28,14 +28,10 @@ const Balance = ({ amount, noSymbol = false }) => {
 }
 
 export const formatNEAR = (amount) => {
-    let ret =  utils.format.formatNearAmount(amount.toString(), FRAC_DIGITS)
-
     if (amount === '0') {
         return amount;
-    } else if (ret === '0') {
-        return `<${!FRAC_DIGITS ? `0` : `0.${'0'.repeat((FRAC_DIGITS || 1) - 1)}1`}`;
     }
-    return ret;
+    return utils.format.formatNearAmount(amount.toString(), FRAC_DIGITS);
 }
 
 const showInYocto = (amountStr) => {
