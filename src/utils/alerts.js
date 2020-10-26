34d01e17814dfca37d5c1386307af2de31f3541e
marcinbodnardesktop <me@marcinbodnar.com>
2020-10-26 23:03:05 +0100

    Alerts: add showAlert to handle showing alerts

--- src/utils/alerts.js
+++ src/utils/alerts.js
@@ -1,6 +1,18 @@
 import { store } from '..'
 import { clearAlert, clear } from '../actions/status'
 
+export const showAlert = ({data, onlyError, onlySuccess, console = true, requestStatus} = {}) => {
+    return {
+        alert: {
+            showAlert: requestStatus ? false : true,
+            onlyError,
+            onlySuccess,
+            console,
+            requestStatus
+        },
+        data
+    }
+}
 export const handleClearAlert = () => {
     const { dispatch, getState } = store
     const { state: { globalAlertPreventClear } = {} } = getState().router.location
