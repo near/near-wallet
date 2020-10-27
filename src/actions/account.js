8cba04aaeef18b87bba1007fc03adebc6c5f52aa
marcinbodnardesktop <me@marcinbodnar.com>
2020-10-27 21:04:40 +0100

    Alerts: use new request status for checking new account

--- src/actions/account.js
+++ src/actions/account.js
@@ -288,7 +288,7 @@ export const {
     ],
     CHECK_NEW_ACCOUNT: [
         wallet.checkNewAccount.bind(wallet),
-        () => defaultCodesFor('account.create')
+        () => showAlert({ requestStatus: true })
     ],
     CREATE_NEW_ACCOUNT: [
         wallet.createNewAccount.bind(wallet),
