2a47e61ac93d39463b03a3d1860f239ea1649a30
marcinbodnardesktop <me@marcinbodnar.com>
2020-11-03 18:50:04 +0100

    Throw error if no accounts were accepted

--- src/utils/wallet.js
+++ src/utils/wallet.js
@@ -567,6 +567,10 @@ class Wallet {
     async saveAndSelectLedgerAccounts(accounts) {
         const accountIds = Object.keys(accounts).filter(accountId => accounts[accountId].status === 'success')
 
+        if (!accountIds.length) {
+            throw new WalletError('No accounts were accepted.', 'signInLedger.getLedgerAccountIds.noAccountsAccepted')
+        }
+
         for (let i = 0; i < accountIds.length; i++) {
             const accountId = accountIds[i]
             if (i === accountIds.length - 1) {
