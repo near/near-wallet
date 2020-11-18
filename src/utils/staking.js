daf9b1052ebf81ccede0e7596dfe1372abba7580
Matt Lockyer <mattdlockyer@gmail.com>
2020-11-18 12:29:36 -0800

    add "next" to validator if they are coming up in next set

--- src/utils/staking.js
+++ src/utils/staking.js
@@ -280,6 +280,7 @@ export class Staking {
     async getValidators(accountIds) {
         const { current_validators, next_validators, current_proposals } = await this.provider.validators()
         const currentValidators = current_validators.map(({ account_id }) => account_id)
+        const nextValidators = current_proposals.map(({ account_id }) => account_id)
         
         if (!accountIds) {
             const rpcValidators = [...current_validators, ...next_validators, ...current_proposals].map(({ account_id }) => account_id)
@@ -301,6 +302,7 @@ export class Staking {
                     const validator = {
                         accountId: account_id,
                         current: currentValidators.includes(account_id),
+                        next: nextValidators.includes(account_id),
                         contract: await this.getContractInstance(account_id, stakingMethods)
                     }
                     const fee = validator.fee = await validator.contract.get_reward_fee_fraction()
