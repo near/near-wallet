24ed7429fef1ce1c064a969d3aa38f055f9048a8
Matt Lockyer <mattdlockyer@gmail.com>
2020-12-01 16:15:36 -0800

    rotate local key deployMultisig

--- src/utils/twoFactor.js
+++ src/utils/twoFactor.js
@@ -59,8 +59,19 @@ export class TwoFactor extends Account2FA {
     }
 
     async deployMultisig() {
+        const { accountId } = this
+        const newKeyPair = KeyPair.fromRandom('ed25519')
+        const newLocalPublicKey = newKeyPair.publicKey
         const contractBytes = new Uint8Array(await (await fetch('/multisig.wasm')).arrayBuffer())
-        await super.deployMultisig(contractBytes)
+
+        const result = await super.deployMultisig(contractBytes, newLocalPublicKey)
+        console.log('result', result)
+        // if (result !== 'true') {
+        //     throw 'Disable 2FA transaction error'
+        // }
+        await this.wallet.saveAccount(accountId, newKeyPair)
+        await store.dispatch(refreshAccount())
+        return result
     }
 
     async disableMultisig() {
