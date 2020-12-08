c7ae570c907db5d317712ae2ca013dd00b3e7497
marcinbodnardesktop <me@marcinbodnar.com>
2020-12-08 23:05:25 +0100

    Alerts: add showCustomAlert action to show custom alert

--- src/actions/status.js
+++ src/actions/status.js
@@ -1,6 +1,11 @@
 import { createActions } from 'redux-actions'
+import { showAlert, dispatchWithAlert } from '../utils/alerts'
 
-export const { clear, clearAlert } = createActions({
+export const { clear, clearAlert, showCustomAlert } = createActions({
     CLEAR: null,
-    CLEAR_ALERT: null
+    CLEAR_ALERT: null,
+    SHOW_CUSTOM_ALERT: [
+        (payload) => (payload),
+        (meta) => showAlert(meta)
+    ]
 })
