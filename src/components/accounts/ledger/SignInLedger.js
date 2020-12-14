277613e5468f14a8fe9826fbb6e2965e7f29722c
marcinbodnardesktop <me@marcinbodnar.com>
2020-12-14 20:09:59 +0100

    Alerts: use setMainLoader in SignInLedger component

--- src/components/accounts/ledger/SignInLedger.js
+++ src/components/accounts/ledger/SignInLedger.js
@@ -14,6 +14,7 @@ import {
     checkAccountAvailable, 
     clearSignInWithLedgerModalState
 } from '../../../actions/account';
+import { setMainLoader } from '../../../actions/status'
 import LocalAlertBox from '../../common/LocalAlertBox'
 import { controller as controllerHelperApi } from '../../../utils/helper-api'
 import { showCustomAlert } from '../../../actions/status'
