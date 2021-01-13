becb0600e297236f4e5ce6cfd94c70de11202814
Matt Lockyer <mattdlockyer@gmail.com>
2021-01-13 09:10:56 -0800

    add Validators

--- src/components/staking/components/Validators.js
+++ src/components/staking/components/Validators.js
@@ -35,7 +35,7 @@ export default function Validators({ validators, stakeFromAccount }) {
                     <ValidatorBox
                         key={i}
                         validator={validator}
-                />
+                    />
                 )}
             </ListWrapper>
         </>
