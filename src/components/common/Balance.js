2e48ffd79c798092e4cb27b0858fb79a65a28b9a
marcinbodnardesktop <me@marcinbodnar.com>
2021-02-01 20:59:15 +0100

    Refactor Balance component to handle loader if ammount is missing (#1394)

--- src/components/common/Balance.js
+++ src/components/common/Balance.js
@@ -17,21 +17,56 @@ const CustomDiv = styled(List)`
         font-weight: 700;
         margin-left: -2%;
     }
+    .dots {
+        color: #4a4f54;
+        margin: 0 12px 0 0;
+
+        :after {
+            content: '.';
+            animation: link 1s steps(5, end) infinite;
+        
+            @keyframes link {
+                0%, 20% {
+                    color: rgba(0,0,0,0);
+                    text-shadow:
+                        .3em 0 0 rgba(0,0,0,0),
+                        .6em 0 0 rgba(0,0,0,0);
+                }
+                40% {
+                    color: #4a4f54;
+                    text-shadow:
+                        .3em 0 0 rgba(0,0,0,0),
+                        .6em 0 0 rgba(0,0,0,0);
+                }
+                60% {
+                    text-shadow:
+                        .3em 0 0 #4a4f54,
+                        .6em 0 0 rgba(0,0,0,0);
+                }
+                80%, 100% {
+                    text-shadow:
+                        .3em 0 0 #4a4f54,
+                        .6em 0 0 #4a4f54;
+                }
+            }
+        }
+    }
 `
 
 const FRAC_DIGITS = 5
 const YOCTO_NEAR_THRESHOLD = new BN('10', 10).pow(new BN(utils.format.NEAR_NOMINATION_EXP - FRAC_DIGITS + 1, 10))
 
 const Balance = ({ amount, symbol, className }) => {
-    if (!amount) {
-        throw new Error('amount property should not be null')
-    }
-    
-    let amountShow = formatNEAR(amount)
+    let amountShow = amount && formatNEAR(amount)
 
     return (
         <CustomDiv title={showInYocto(amount)} className={className}>
-            {symbol !== false && symbol !== 'near' && <span className='symbol'>Ⓝ</span>}{amountShow}{symbol === 'near' && <span className='currency'>&nbsp;NEAR</span>}
+            {symbol !== false && symbol !== 'near' && <span className='symbol'>Ⓝ</span>}
+            {amount
+                ? amountShow
+                : <div className="dots"></div>
+            }
+            {symbol === 'near' && <span className='currency'>&nbsp;NEAR</span>}
         </CustomDiv>
     )
 }
