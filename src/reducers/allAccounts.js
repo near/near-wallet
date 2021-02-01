75ece5980e2ec8eeecf04e9851595a27e9819f0a
marcinbodnardesktop <me@marcinbodnar.com>
2021-02-01 21:04:17 +0100

    Refactor allAccounts reducer (#1394)

--- src/reducers/allAccounts.js
+++ src/reducers/allAccounts.js
@@ -6,58 +6,45 @@ import { refreshAccountExternal, updateStakingAccount, updateStakingLockup } fro
 const initialState = {}
 
 const allAccountsReducer = handleActions({
-    [refreshAccountExternal]: (state, { error, meta: { accountId }, payload, ready }) => {
-        if (!ready) return state
-
-        if (error) {
-            return state
-        }
-
-        return {
-            ...state,
-            [accountId]: { 
-                accountId, 
-                ...payload
-            }
-        }
-    },
-    [updateStakingAccount]: (state, { error, meta, payload, ready }) => {
-        if (!ready || error) {
-            return state
-        }
-
-        return {
-            ...state,
-            [meta.accountId]: { 
-                ...state[meta.accountId],
-                balance: {
-                    ...state[meta.accountId].balance,
-                    account: payload
+    [refreshAccountExternal]: (state, { error, meta: { accountId }, payload, ready }) => 
+        (!ready || error)
+            ? state
+            : ({
+                ...state,
+                [accountId]: { 
+                    accountId, 
+                    ...payload
                 }
-            }
-        }
-    },
-    [updateStakingLockup]: (state, { error, meta, payload, ready }) => {
-        if (!ready || error) {
-            return state
-        }
-
-        return {
-            ...state,
-            [meta.accountId]: { 
-                ...state[meta.accountId],
-                balance: {
-                    ...state[meta.accountId].balance,
-                    lockupAccount: payload
+            }),
+    [updateStakingAccount]: (state, { error, meta, payload, ready }) => 
+        (!ready || error)
+            ? state
+            : ({
+                ...state,
+                [meta.accountId]: { 
+                    ...state[meta.accountId],
+                    balance: {
+                        ...state[meta.accountId].balance,
+                        account: payload
+                    }
                 }
-            }
-        }
-    },
+            }),
+    [updateStakingLockup]: (state, { error, meta, payload, ready }) => 
+        (!ready || error)
+            ? state
+            : ({
+                ...state,
+                [meta.accountId]: { 
+                    ...state[meta.accountId],
+                    balance: {
+                        ...state[meta.accountId].balance,
+                        lockupAccount: payload
+                    }
+                }
+            })
 }, initialState)
 
 
 export default reduceReducers(
     allAccountsReducer
 )
-
-
