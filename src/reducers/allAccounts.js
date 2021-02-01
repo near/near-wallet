1ddb744ec32d29185377f37ce911d616c12f3e6f
marcinbodnardesktop <me@marcinbodnar.com>
2021-02-01 20:52:59 +0100

    Refactor allAccounts reducer (#1394)

--- src/reducers/allAccounts.js
+++ src/reducers/allAccounts.js
@@ -1,24 +1,31 @@
-import { handleAction } from 'redux-actions'
+import { handleActions, combineActions } from 'redux-actions'
+import reduceReducers from 'reduce-reducers'
+
 import { refreshAccountExternal } from '../actions/account'
 
 const initialState = {}
 
-const reducer = (state, event) => {
-    const { error, meta: { accountId }, payload, ready } = event
+const allAccountsReducer = handleActions({
+    [refreshAccountExternal]: (state, { error, meta: { accountId }, payload, ready }) => {
+        if (!ready) return state
 
-    if (!ready) return state
+        if (error) {
+            return state
+        }
 
-    if (error) {
-        return state
-    }
-
-    return {
-        ...state,
-        [accountId]: { 
-            accountId, 
-            ...payload
+        return {
+            ...state,
+            [accountId]: { 
+                accountId, 
+                ...payload
+            }
         }
     }
-}
+}, initialState)
 
-export default handleAction(refreshAccountExternal, reducer, initialState)
+
+export default reduceReducers(
+    allAccountsReducer
+)
+
+
