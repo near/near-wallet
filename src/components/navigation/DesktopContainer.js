03c31814250b9980b117c1fc1aadaf1beb1f1f72
Patrick Tajima <patrick.tajima@gmail.com>
2021-02-09 16:17:21 -0800

    Point 'HELP' button to Zendesk

--- src/components/navigation/DesktopContainer.js
+++ src/components/navigation/DesktopContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Translate } from 'react-localize-redux';
 import styled from 'styled-components';
+import helpIcon from '../../images/icon-help.svg';
 import userIcon from '../../images/user-icon-grey.svg';
 import languagesIcon from '../../images/icon-languages.svg';
 import Logo from './Logo';
@@ -35,6 +36,32 @@ const Container = styled.div`
         position: relative;
     }
 `
+const Help = styled.a`
+    color: white;
+    display: flex;
+    align-items: center;
+    margin-left: auto;
+    text-transform: uppercase;
+    cursor: pointer;
+    letter-spacing: 2px;
+    padding-top: 2px;
+    
+    &:hover {
+        color: white;
+        text-decoration: none;
+    }
+    &:before {
+        content: '';
+        background: url(${helpIcon});
+        background-repeat: no-repeat;
+        display: inline-block;
+        width: 23px;
+        height: 23px;
+        margin-right: 10px;
+        margin-top: -2px;
+    }
+`
+
 const User = styled.div`
     border-left: 2px solid #5d5f60;
     position: relative;
@@ -152,7 +179,7 @@ const UserIcon = styled.div`
 `
 
 const Lang = styled.div`
-    margin-left: auto;
+    margin-left: 20px;
     position: relative;
 
     &:after {
@@ -211,6 +238,9 @@ class DesktopContainer extends Component {
                 {showNavLinks &&
                     <NavLinks />
                 }
+                <Help href='https://nearhelp.zendesk.com/' target='_blank' rel='noopener noreferrer'>
+                    <Translate id='link.help'/>
+                </Help>
                 <Lang>
                     <LanguageToggle />
                 </Lang>
