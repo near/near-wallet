1d84c975bd7840fd8ee07637eab7b4acc38a5c33
marcinbodnardesktop <me@marcinbodnar.com>
2021-02-22 23:55:56 +0100

    ActivityDetailModal cleanup (#1147)

--- src/components/wallet/ActivityDetailModal.js
+++ src/components/wallet/ActivityDetailModal.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { Translate } from 'react-localize-redux'
 import styled from 'styled-components'
 
