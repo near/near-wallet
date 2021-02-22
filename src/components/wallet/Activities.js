1fe9948b86bfa64db4998eb92af71e13a6a12225
marcinbodnardesktop <me@marcinbodnar.com>
2021-02-22 23:44:06 +0100

    Add styles to Activities component (#1147)

--- src/components/wallet/Activities.js
+++ src/components/wallet/Activities.js
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
+import { Translate } from 'react-localize-redux'
+
 import ActivityBox from './ActivityBox'
 import ActivityDetailModal from './ActivityDetailModal'
+import FormButton from '../common/FormButton'
+import { EXPLORER_URL, TRANSACTIONS_REFRESH_INTERVAL } from '../../utils/wallet'
+import { actionsPending } from '../../utils/alerts'
+import classNames from '../../utils/classNames'
 
 const StyledContainer = styled.div`
     width: 100%;
@@ -11,6 +17,39 @@ const StyledContainer = styled.div`
 
         :last-of-type {
             border-bottom: 0;
+        }
+    }
+
+    .dots {
+        :after {
+            position: absolute;
+            content: '.';
+            animation: link 1s steps(5, end) infinite;
+        
+            @keyframes link {
+                0%, 20% {
+                    color: rgba(0,0,0,0);
+                    text-shadow:
+                        .3em 0 0 rgba(0,0,0,0),
+                        .6em 0 0 rgba(0,0,0,0);
+                }
+                40% {
+                    color: #24272a;
+                    text-shadow:
+                        .3em 0 0 rgba(0,0,0,0),
+                        .6em 0 0 rgba(0,0,0,0);
+                }
+                60% {
+                    text-shadow:
+                        .3em 0 0 #24272a,
+                        .6em 0 0 rgba(0,0,0,0);
+                }
+                80%, 100% {
+                    text-shadow:
+                        .3em 0 0 #24272a,
+                        .6em 0 0 #24272a;
+                }
+            }
         }
     }
 `
