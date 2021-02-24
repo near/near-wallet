8d71fc57661e6458c767a6d8f16befe0cf373daf
Patrick Tajima <patrick.tajima@gmail.com>
2021-02-23 19:21:53 -0800

    fix icons

--- src/components/svg/HelpIcon.js
+++ src/components/svg/HelpIcon.js
@@ -3,13 +3,13 @@ import React from 'react';
 const HelpIcon = ({ color = '#A2A2A8' }) => {
     return (
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g clipPath="url(#clip0)">
+            <g clipPath="url(#clip324230)">
                 <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                 <path d="M9.09 8.99996C9.3251 8.33163 9.78915 7.76807 10.4 7.40909C11.0108 7.05012 11.7289 6.9189 12.4272 7.03867C13.1255 7.15844 13.7588 7.52148 14.2151 8.06349C14.6713 8.60549 14.9211 9.29148 14.92 9.99996C14.92 12 11.92 13 11.92 13" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                 <path d="M12 17H12.01" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             </g>
             <defs>
-                <clipPath id="clip0">
+                <clipPath id="clip324230">
                     <rect width="24" height="24" fill="white"/>
                 </clipPath>
             </defs>
--- src/components/svg/VaultIcon.js
+++ src/components/svg/VaultIcon.js
@@ -3,7 +3,7 @@ import React from 'react';
 const VaultIcon = ({ color = '#A2A2A8' }) => {
     return (
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g clipPath="url(#clip0)">
+            <g clipPath="url(#clip32420)">
                 <path d="M2 17V3C2 1.89543 2.89543 1 4 1H20C21.1046 1 22 1.89543 22 3V17C22 18.1046 21.1046 19 20 19H4C2.89543 19 2 18.1046 2 17Z" stroke={color} strokeWidth="2"/>
                 <circle cx="12" cy="10" r="3" stroke={color} strokeWidth="2"/>
                 <line x1="12" y1="5" x2="12" y2="6.28408" stroke={color} strokeWidth="2" strokeLinecap="round"/>
@@ -14,7 +14,7 @@ const VaultIcon = ({ color = '#A2A2A8' }) => {
                 <line x1="15" y1="23" x2="18" y2="23" stroke={color} strokeWidth="2" strokeLinecap="round"/>
             </g>
             <defs>
-                <clipPath id="clip0">
+                <clipPath id="clip32420">
                     <rect width="24" height="24" fill="white"/>
                 </clipPath>
             </defs>
