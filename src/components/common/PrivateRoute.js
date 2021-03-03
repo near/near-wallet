21132ecfee7f9cf430a2f922868a8ad9fb9f120c
marcinbodnardesktop <me@marcinbodnar.com>
2021-03-03 18:04:38 +0100

    Change condition for PrivateRoute (#1497)

--- src/components/common/PrivateRoute.js
+++ src/components/common/PrivateRoute.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Route, withRouter, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { KEY_ACTIVE_ACCOUNT_ID } from '../../utils/wallet'
 
 const PrivateRoute = ({component: Component, account, ...rest}) => (
     <Route 
         {...rest} 
         render={(props) => (
-            !account.accountId
+            !localStorage.getItem(KEY_ACTIVE_ACCOUNT_ID)
                 ? (
                     <Redirect
                         to={{
