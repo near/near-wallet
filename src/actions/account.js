081b87fc344a35064ecb714e8040d04abcd19209
Vladimir Grichina <vgrichina@gmail.com>
2021-03-04 01:23:16 -0800

    Do not require adding access key on login

--- src/actions/account.js
+++ src/actions/account.js
@@ -142,12 +142,16 @@ export const allowLogin = () => async (dispatch, getState) => {
     const { success_url, public_key, title } = url
 
     if (success_url) {
-        await dispatchWithAlert(addAccessKey(account.accountId, url.contract_id, url.public_key), { onlyError: true })
+        if (public_key) {
+            await dispatchWithAlert(addAccessKey(account.accountId, url.contract_id, url.public_key), { onlyError: true })
+        }
         const availableKeys = await wallet.getAvailableKeys();
         const allKeys = availableKeys.map(key => key.toString());
         const parsedUrl = new URL(success_url)
         parsedUrl.searchParams.set('account_id', account.accountId)
-        parsedUrl.searchParams.set('public_key', public_key)
+        if (public_key) {
+            parsedUrl.searchParams.set('public_key', public_key)
+        }
         parsedUrl.searchParams.set('all_keys', allKeys.join(','))
         window.location = parsedUrl.href
     } else {
