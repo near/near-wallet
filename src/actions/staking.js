4fb8bccff1b938824a36bfa9a2a381fd31026c77
marcinbodnardesktop <me@marcinbodnar.com>
2021-03-10 01:42:49 +0100

    Add handleGetLockup action (#1446)

--- src/actions/staking.js
+++ src/actions/staking.js
@@ -71,6 +71,18 @@ const handleGetAccounts = () => async (dispatch, getState) => {
     }))
 }
 
+const handleGetLockup = (accountId) => async (dispatch, getState) => {
+    let lockup = {}
+    try {
+        lockup = await wallet.staking.getLockup(accountId)
+    } catch(e) {
+        if (!/No contract for account/.test(e.message)) {
+            throw e
+        }
+    }
+    dispatch(staking.getLockup(lockup))
+}
+
 const handleStakingUpdateAccount = (recentlyStakedValidators = []) => async (dispatch, getState) => {
     const { accountId, balance } = getState().account
 
