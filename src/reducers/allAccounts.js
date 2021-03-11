a731743e659e63eca591f0b76f179473d9908539
marcinbodnardesktop <me@marcinbodnar.com>
2021-03-11 02:53:27 +0100

    AllAccounts reducer will use staking actions for Profile balance (#1446)

--- src/reducers/allAccounts.js
+++ src/reducers/allAccounts.js
@@ -2,6 +2,7 @@ import { handleActions } from 'redux-actions'
 import reduceReducers from 'reduce-reducers'
 
 import { refreshAccountExternal, updateStakingAccount, updateStakingLockup } from '../actions/account'
+import { staking } from '../actions/staking'
 
 const initialState = {}
 
@@ -16,32 +17,30 @@ const allAccountsReducer = handleActions({
                     ...payload
                 }
             }),
-    [updateStakingAccount]: (state, { error, meta, payload, ready }) => 
-        (!ready || error || !meta.accountId)
-            ? state
-            : ({
-                ...state,
-                [meta.accountId]: { 
-                    ...state[meta.accountId],
-                    balance: {
-                        ...state[meta.accountId].balance,
-                        account: payload
-                    }
+    [staking.updateAccount]: (state, { ready, error, payload, meta }) => {
+        return {
+            ...state,
+            [payload.accountId]: { 
+                ...state[payload.accountId],
+                balance: {
+                    ...state[payload.accountId].balance,
+                    account: payload
                 }
-            }),
-    [updateStakingLockup]: (state, { error, meta, payload, ready }) => 
-        (!ready || error || !meta.accountId)
-            ? state
-            : ({
-                ...state,
-                [meta.accountId]: { 
-                    ...state[meta.accountId],
-                    balance: {
-                        ...state[meta.accountId].balance,
-                        lockupAccount: payload
-                    }
+            }
+        }
+    },
+    [staking.updateLockup]: (state, { ready, error, payload, meta }) => {
+        return {
+            ...state,
+            [payload.mainAccountId]: { 
+                ...state[payload.mainAccountId],
+                balance: {
+                    ...state[payload.mainAccountId].balance,
+                    lockupAccount: payload
                 }
-            })
+            }
+        }
+    },
 }, initialState)
 
 
