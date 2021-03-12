a7bb50009dbbfb7390fef366a06ea8a8f113fb38
marcinbodnardesktop <me@marcinbodnar.com>
2021-03-12 01:07:29 +0100

    updateAccount as action (#1446)

--- src/actions/staking.js
+++ src/actions/staking.js
@@ -84,7 +84,66 @@ export const {
 
     STAKING: {
         GET_ACCOUNTS: null,
-        UPDATE_ACCOUNT: null,
+        UPDATE_ACCOUNT: async (balance, validators, accountId, validatorDepositMap) => {
+            let totalUnstaked = new BN(balance.available)
+            if (totalUnstaked.lt(new BN(STAKING_AMOUNT_DEVIATION))) {
+                totalUnstaked = ZERO.clone();
+            }
+            let totalStaked = ZERO.clone();
+            let totalUnclaimed = ZERO.clone();
+            let totalAvailable = ZERO.clone();
+            let totalPending = ZERO.clone();
+
+            await Promise.all(validators.map(async (validator, i) => {
+                try {
+                    const total = new BN(await validator.contract.get_account_total_balance({ account_id: accountId }))
+                    if (total.lte(ZERO)) {
+                        validator.remove = true
+                        return
+                    }
+
+                    // try to get deposits from explorer
+                    const deposit = new BN(validatorDepositMap[validator.accountId] || '0')
+                    validator.staked = await validator.contract.get_account_staked_balance({ account_id: accountId })
+
+                    // rewards (lifetime) = total - deposits
+                    validator.unclaimed = total.sub(deposit).toString()
+                    if (!deposit.gt(ZERO) || new BN(validator.unclaimed).lt(MIN_DISPLAY_YOCTO)) {
+                        validator.unclaimed = ZERO.clone().toString()
+                    }
+
+                    validator.unstaked = new BN(await validator.contract.get_account_unstaked_balance({ account_id: accountId }))
+                    if (validator.unstaked.gt(MIN_DISPLAY_YOCTO)) {
+                        const isAvailable = await validator.contract.is_account_unstaked_balance_available({ account_id: accountId })
+                        if (isAvailable) {
+                            validator.available = validator.unstaked.toString()
+                            totalAvailable = totalAvailable.add(validator.unstaked)
+                        } else {
+                            validator.pending = validator.unstaked.toString()
+                            totalPending = totalPending.add(validator.unstaked)
+                        }
+                    }
+
+                    totalStaked = totalStaked.add(new BN(validator.staked))
+                    totalUnclaimed = totalUnclaimed.add(new BN(validator.unclaimed))
+                } catch (e) {
+                    if (e.message.indexOf('cannot find contract code') === -1) {
+                        console.warn('Error getting data for validator', validator.accountId, e)
+                    }
+                }
+            }))
+
+            return {
+                accountId,
+                validators: validators.filter((v) => !v.remove),
+                selectedValidator: null,
+                totalUnstaked: totalUnstaked.toString(),
+                totalStaked: totalStaked.toString(),
+                totalUnclaimed: (totalUnclaimed.lt(MIN_DISPLAY_YOCTO) ? ZERO : totalUnclaimed).toString(),
+                totalPending: totalPending.toString(),
+                totalAvailable: (totalAvailable.lt(MIN_DISPLAY_YOCTO) ? ZERO : totalAvailable).toString(),
+            }
+        },
         UPDATE_LOCKUP: null,
         UPDATE_CURRENT: null,
         GET_LOCKUP: async (accountId) => {
