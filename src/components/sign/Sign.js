f5f2e5a6519d85ffd1e5f6f4c9dd050eda7df220
Ryan Mehta <ryan.mehta@gmail.com>
2021-03-29 16:28:10 -0700

    no undefined meta

--- src/components/sign/Sign.js
+++ src/components/sign/Sign.js
@@ -97,7 +97,8 @@ class Sign extends Component {
 function addQueryParams(baseUrl, queryParams) {
     const url = new URL(baseUrl);
     for (let key in queryParams) {
-        url.searchParams.set(key, queryParams[key]);
+        const param = queryParams[key];
+        if(param) url.searchParams.set(key, param);
     }
     return url.toString();
 }
