c65cbc0890832642c17a5d129c672b1922b123fd
marcinbodnardesktop <me@marcinbodnar.com>
2021-04-08 22:16:30 +0200

    Format amount of tokens (#1623)

--- src/components/wallet/TokenAmount.js
+++ src/components/wallet/TokenAmount.js
@@ -2,11 +2,24 @@ import React from 'react'
 import Big from 'big.js'
 import { formatTokenAmount } from '../../utils/amounts'
 
+const FRAC_DIGITS = 5
 
 const TokenAmount = ({ token: { balance, decimals, symbol}, className }) => (
-    <div className={className}>
-        {balance}
+        {balance && formatToken(balance, decimals)}
     </div>
 )
+
+const formatToken = (amount, decimals) => {
+    if (amount === '0') {
+        return amount
+    }
+
+    let formattedAmount = formatTokenAmount(amount, decimals, FRAC_DIGITS)
+
+    if (formattedAmount === `0.${'0'.repeat(FRAC_DIGITS)}`) {
+        return `<${!FRAC_DIGITS ? `0` : `0.${'0'.repeat((FRAC_DIGITS || 1) - 1)}1`}`
+    }
+    return formattedAmount
+}
 
 export default TokenAmount
