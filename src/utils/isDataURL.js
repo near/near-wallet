4c243cf677600884656d74df4b6da76ab862ea89
Patrick Tajima <patrick.tajima@gmail.com>
2021-04-27 14:04:27 -0700

    Show token icon if available

---
+++ src/utils/isDataURL.js
@@ -0,0 +1,6 @@
+function isDataURL(s) {
+    return !!s.match(isDataURL.regex);
+}
+isDataURL.regex = /^\s*data:([a-z]+\/[a-z]+(;[a-z\-]+\=[a-z\-]+)?)?(;base64)?,[a-z0-9\!\$\&\'\,\(\)\*\+\,\;\=\-\.\_\~\:\@\/\?\%\s]*\s*$/i;
+
+export default isDataURL;
