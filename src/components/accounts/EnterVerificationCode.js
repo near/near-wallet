0f7171ba2f92b047ff3e27acb58cabb46db14ba5
Daryl Collins <daryl@darylcollins.com>
2021-05-07 23:48:32 -0700

    1659 - Initial cut of 2fa / confirmation code reCAPTCHA protection

--- src/components/accounts/EnterVerificationCode.js
+++ src/components/accounts/EnterVerificationCode.js
@@ -1,16 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Translate } from 'react-localize-redux';
 import FormButton from '../common/FormButton';
 import Container from '../common/styled/Container.css';
+import { Recaptcha } from '../Recaptcha';
+
+// FIXME: Use `debug` npm package so we can keep some debug logging around but not spam the console everywhere
+const ENABLE_DEBUG_LOGGING = false;
+const debugLog = (...args) => ENABLE_DEBUG_LOGGING && console.log('EnterVerificationCode:', ...args);
 
 const StyledContainer = styled(Container)`
 
     h4 {
         margin-top: 30px;
     }
-    
+
     input {
         width: 100%;
         margin-top: 8px !important;
@@ -55,9 +60,14 @@ const EnterVerificationCode = ({
     onResend,
     email,
     phoneNumber,
-    loading
+    loading,
+    isNewAccount,
+    onRecaptchaChange
 }) => {
-
+    debugLog('Rendering', { isNewAccount });
+    const recaptchaRef = useRef(null);
+    const [recaptchaToken, setRecaptchaToken] = useState(null);
+    const [recaptchaLoadFailed, setRecaptchaLoadFailed] = useState(false);
     const [code, setCode] = useState('');
 
     let useEmail = true;
@@ -65,17 +75,28 @@ const EnterVerificationCode = ({
         useEmail = false;
     }
 
-    const handleConfirm = () => {
+    const handleConfirm = async () => {
         if (code.length === 6 && !loading) {
-            onConfirm(code)
+            return onConfirm(code);
         }
     }
 
+    const handleRecaptchaLoadFailed = () => setRecaptchaLoadFailed(true)
+
+    const shouldRenderRecaptcha = process.env.RECAPTCHA_CHALLENGE_API_KEY && isNewAccount && !recaptchaLoadFailed;
     return (
         <StyledContainer className='small-centered'>
-            <form onSubmit={e => {handleConfirm(); e.preventDefault();}} autoComplete='off'>
+            <form
+                onSubmit={e => {
+                    handleConfirm().then(() => recaptchaRef?.current?.reset());
+                    e.preventDefault();
+                }}
+                autoComplete='off'
+            >
                 <h1><Translate id='setRecoveryConfirm.title'/></h1>
-                <h2><Translate id='setRecoveryConfirm.pageText'/> <Translate id={useEmail ? 'setRecoveryConfirm.email' : 'setRecoveryConfirm.phone'}/>: <span>{useEmail ? email : phoneNumber}</span></h2>
+                <h2><Translate id='setRecoveryConfirm.pageText'/> <Translate
+                    id={useEmail ? 'setRecoveryConfirm.email' : 'setRecoveryConfirm.phone'}/>: <span>{useEmail ? email : phoneNumber}</span>
+                </h2>
                 <h4 className='small'><Translate id='setRecoveryConfirm.inputHeader'/></h4>
                 <Translate>
                     {({ translate }) => (
@@ -93,14 +114,25 @@ const EnterVerificationCode = ({
                         </>
                     )}
                 </Translate>
+                {
+                    shouldRenderRecaptcha && <Recaptcha
+                        ref={recaptchaRef}
+                        onLoadFailed={handleRecaptchaLoadFailed}
+                        onChange={(token) => {
+                            debugLog('onChange from recaptcha - setting token in state', token);
+                            setRecaptchaToken(token);
+                            onRecaptchaChange(token)
+                        }}
+                    />
+                }
                 <FormButton
                     color='blue'
                     type='submit'
-                    disabled={code.length !== 6 || loading}
+                    disabled={code.length !== 6 || loading || (!recaptchaToken && shouldRenderRecaptcha)}
                     sending={loading}
                     sendingString='button.verifying'
                 >
-                    <Translate id='button.verifyCodeEnable' />
+                    <Translate id='button.verifyCodeEnable'/>
                 </FormButton>
             </form>
 
@@ -108,7 +140,10 @@ const EnterVerificationCode = ({
                 <div><Translate id='setRecoveryConfirm.didNotRecive'/></div>
                 <div>
                     <span onClick={onResend} className='link'><Translate id='setRecoveryConfirm.resendCode'/></span>
-                    &nbsp;<Translate id='setRecoveryConfirm.or'/>&nbsp;<span onClick={onGoBack} className='link'><Translate id='setRecoveryConfirm.sendToDifferent'/> <Translate id={`setRecoveryConfirm.${useEmail ? 'email' : 'phone'}`}/></span>
+                    &nbsp;<Translate id='setRecoveryConfirm.or'/>&nbsp;<span onClick={onGoBack}
+                                                                             className='link'><Translate
+                    id='setRecoveryConfirm.sendToDifferent'/> <Translate
+                    id={`setRecoveryConfirm.${useEmail ? 'email' : 'phone'}`}/></span>
                 </div>
             </div>
         </StyledContainer>
