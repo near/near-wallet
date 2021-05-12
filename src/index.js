dc5b9d4a2813f24a05fed9c4c79135d9ca226755
marcinbodnardesktop <me@marcinbodnar.com>
2021-05-12 22:11:24 +0200

    Simplify index.js (#1666)

--- src/index.js
+++ src/index.js
@@ -4,22 +4,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import { initSentry } from './utils/sentry'
-import { Provider } from 'react-redux'
-import { LocalizeProvider } from 'react-localize-redux';
-import { PersistGate } from 'redux-persist/integration/react'
-
 import Routing from './components/Routing'
-import { store, persistor, history } from './redux/store'
+import StoreConfiguration from './redux/store'
 
 initSentry();
 
 ReactDOM.render(
-    <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-            <LocalizeProvider store={store}>
-                <Routing history={history} />
-            </LocalizeProvider>
-        </PersistGate>
-    </Provider>,
+    <StoreConfiguration>
+        <Routing history={history} />
+    </StoreConfiguration>,
     document.getElementById('root')
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
+import React from 'react'
 import { createStore } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import { createBrowserHistory } from 'history'
+
+import { Provider } from 'react-redux'
+import { LocalizeProvider } from 'react-localize-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 
 import createRootReducer from '../reducers'
 import createMiddleware from '../middleware'
@@ -11,10 +16,20 @@ const persistConfig = {
     storage,
 }
 
-export const history = createBrowserHistory()
+const history = createBrowserHistory()
 
 const persistedReducer = persistReducer(persistConfig, createRootReducer(history))
 
 export const store = createStore(persistedReducer, createMiddleware(history))
 
-export let persistor = persistStore(store)
+let persistor = persistStore(store)
+
+export default ({ children }) => (
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <LocalizeProvider store={store}>
+                {children}
+            </LocalizeProvider>
+        </PersistGate>
+    </Provider>
+)
