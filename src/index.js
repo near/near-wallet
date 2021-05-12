b0aad608752debc28179a7b7749cbb66c1945aec
marcinbodnardesktop <me@marcinbodnar.com>
2021-05-12 22:01:57 +0200

    Refactor index.js to use store configuration file (#1666)

--- src/index.js
+++ src/index.js
@@ -4,35 +4,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import { initSentry } from './utils/sentry'
-
-import { createStore } from 'redux'
-
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
-import { PersistGate } from 'redux-persist/integration/react'
-
 import { Provider } from 'react-redux'
 import { LocalizeProvider } from 'react-localize-redux';
-import { createBrowserHistory } from 'history'
-
-import createRootReducer from './reducers'
-import createMiddleware from './middleware'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import Routing from './components/Routing'
+import { store, persistor, history } from './redux/store'
 
 initSentry();
-
-const persistConfig = {
-    key: 'root',
-    storage,
-}
-
-const history = createBrowserHistory()
-
-const persistedReducer = persistReducer(persistConfig, createRootReducer(history))
-
-export const store = createStore(persistedReducer, createMiddleware(history))
-let persistor = persistStore(store)
 
 ReactDOM.render(
     <Provider store={store}>
