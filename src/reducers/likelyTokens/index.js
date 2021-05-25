fa09b09e2986961ad8cef69e81d17b85ea16ed2e
marcinbodnardesktop <me@marcinbodnar.com>
2021-05-25 21:29:02 +0200

    Handle likelyTokens.get action in reducer (#1642)

--- src/reducers/likelyTokens/index.js
+++ src/reducers/likelyTokens/index.js
@@ -4,11 +4,18 @@ import reduceReducers from 'reduce-reducers'
 import { likelyTokens } from '../../actions/likelyTokens'
 
 const initialState = {
+    likelyContracts: [],
     tokens: []
 }
 
 const tokens = handleActions({
-    
+    [likelyTokens.get]: (state, { ready, error, payload }) => 
+        (!ready || error)
+            ? state
+            : ({
+                ...state,
+                likelyContracts: payload
+            }),
 }, initialState)
 
 export default reduceReducers(
