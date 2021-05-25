52f310c4fc4a14336ce6161c3afa759997595974
marcinbodnardesktop <me@marcinbodnar.com>
2021-05-25 20:33:22 +0200

    Fetch likelyContracts using likelyTokens.get action (#1642)

--- src/actions/likelyTokens.js
+++ src/actions/likelyTokens.js
@@ -2,11 +2,13 @@ import { createActions } from 'redux-actions'
 
 export const handleGetLikelyTokens = () => async (dispatch, getState) => {
 
+    const { tokens } = getState()?.likelyTokens
+    const likelyContracts = await dispatch(likelyTokens.get())
 
 }
 
 export const { likelyTokens } = createActions({
     LIKELY_TOKENS: {
-        GET: null
+        GET: wallet.getLikelyTokens.bind(wallet),
     }
 })
