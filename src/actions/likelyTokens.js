4ebb4cf7d0e75b220094f697b6e2586f84717fb1
marcinbodnardesktop <me@marcinbodnar.com>
2021-05-25 20:28:42 +0200

    Add handleGetLikelyTokens async action (#1642)

--- src/actions/likelyTokens.js
+++ src/actions/likelyTokens.js
@@ -1,5 +1,10 @@
 import { createActions } from 'redux-actions'
 
+export const handleGetLikelyTokens = () => async (dispatch, getState) => {
+
+
+}
+
 export const { likelyTokens } = createActions({
     LIKELY_TOKENS: {
         GET: null
