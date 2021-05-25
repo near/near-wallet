caf87784171026516d6b5812744d49f1861b45ba
marcinbodnardesktop <me@marcinbodnar.com>
2021-05-25 21:32:59 +0200

    Handle loadedTokens (#1642)

--- src/actions/likelyTokens.js
+++ src/actions/likelyTokens.js
@@ -1,14 +1,23 @@
 import { createActions } from 'redux-actions'
 import { wallet } from '../utils/wallet'
 
+const WHITELISTED_CONTRACTS = (process.env.TOKEN_CONTRACTS || 'berryclub.ek.near,farm.berryclub.ek.near,wrap.near').split(',');
+
 export const handleGetLikelyTokens = () => async (dispatch, getState) => {
 
-    const { tokens } = getState()?.likelyTokens
     const likelyContracts = await dispatch(likelyTokens.get())
+
+    const contracts = [...new Set([...likelyContracts, ...WHITELISTED_CONTRACTS])];
+    let loadedTokens = contracts.map(contract => ({
+        [contract]: { contract }
+    }));
+    loadedTokens = loadedTokens.reduce((a, b) => Object.assign(a, b), {});
+
+    dispatch(tokens.set(loadedTokens))
 
 }
 
-export const { likelyTokens } = createActions({
+export const { likelyTokens, tokens } = createActions({
     LIKELY_TOKENS: {
         GET: wallet.getLikelyTokens.bind(wallet),
     },
