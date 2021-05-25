fb70f0810e15e4eae8d55cab4202bd44a1de1aa0
marcinbodnardesktop <me@marcinbodnar.com>
2021-05-25 22:49:07 +0200

    Check token balances in handleGetLikelyTokens action (#1642)

--- src/actions/likelyTokens.js
+++ src/actions/likelyTokens.js
@@ -15,6 +15,32 @@ export const handleGetLikelyTokens = () => async (dispatch, getState) => {
 
     dispatch(tokens.set(loadedTokens))
 
+
+    const { accountId } = getState().account
+    const account =  await wallet.getAccount(accountId)
+
+    contracts.forEach(async contract => {
+        try {
+            // TODO: Parallelize balance and metadata calls, use cached metadata?
+            let { name, symbol, decimals, icon } = await account.viewFunction(contract, 'ft_metadata')
+            const balance = await account.viewFunction(contract, 'ft_balance_of', { account_id: accountId })
+            loadedTokens = {
+                ...loadedTokens,
+                [contract]: { contract, balance, name, symbol, decimals, icon }
+            }
+        } catch (e) {
+            console.log('e', e);
+            if (e.message.includes('FunctionCallError(MethodResolveError(MethodNotFound))')) {
+                loadedTokens = {...loadedTokens};
+                delete loadedTokens[contract];
+                return;
+            }
+            // logError(e);
+        } finally {
+            dispatch(tokens.set(loadedTokens))
+        }
+    })
+
 }
 
 export const { likelyTokens, tokens } = createActions({
