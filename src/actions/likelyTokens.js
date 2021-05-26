d2e5e61c2ad92ac0b7cec9e975b4ea8d177c7943
marcinbodnardesktop <me@marcinbodnar.com>
2021-05-26 23:35:41 +0200

    Fetch tokens metadata and balance using getMetadata and getBalanceOf actions (#1642)

--- src/actions/likelyTokens.js
+++ src/actions/likelyTokens.js
@@ -1,12 +1,15 @@
 import { createActions } from 'redux-actions'
 import { wallet } from '../utils/wallet'
-import { getLikelyTokens } from '../utils/tokens'
+import { getLikelyTokens, getMetadata, getBalanceOf } from '../utils/tokens'
 
 const WHITELISTED_CONTRACTS = (process.env.TOKEN_CONTRACTS || 'berryclub.ek.near,farm.berryclub.ek.near,wrap.near').split(',');
 
 export const handleGetLikelyTokens = () => async (dispatch, getState) => {
 
-    const likelyContracts = await dispatch(likelyTokens.get())
+    const { accountId } = getState().account
+    const account = await wallet.getAccount(accountId)
+
+    const likelyContracts = await dispatch(likelyTokens.get(accountId))
 
     const contracts = [...new Set([...likelyContracts, ...WHITELISTED_CONTRACTS])];
     let loadedTokens = contracts.map(contract => ({
@@ -15,31 +18,10 @@ export const handleGetLikelyTokens = () => async (dispatch, getState) => {
     loadedTokens = loadedTokens.reduce((a, b) => Object.assign(a, b), {});
 
     dispatch(tokens.set(loadedTokens))
-
-
-    const { accountId } = getState().account
-    const account =  await wallet.getAccount(accountId)
-
+    
     contracts.forEach(async contract => {
-        try {
-            // TODO: Parallelize balance and metadata calls, use cached metadata?
-            let { name, symbol, decimals, icon } = await account.viewFunction(contract, 'ft_metadata')
-            const balance = await account.viewFunction(contract, 'ft_balance_of', { account_id: accountId })
-            loadedTokens = {
-                ...loadedTokens,
-                [contract]: { contract, balance, name, symbol, decimals, icon }
-            }
-        } catch (e) {
-            console.log('e', e);
-            if (e.message.includes('FunctionCallError(MethodResolveError(MethodNotFound))')) {
-                loadedTokens = {...loadedTokens};
-                delete loadedTokens[contract];
-                return;
-            }
-            // logError(e);
-        } finally {
-            dispatch(tokens.set(loadedTokens))
-        }
+        dispatch(tokens.getMetadata(contract, account))
+        dispatch(tokens.getBalanceOf(contract, account, accountId))
     })
 
 }
