85dfb92b55fd95b6a35519e589e19555f881cd9a
marcinbodnardesktop <me@marcinbodnar.com>
2021-05-26 23:32:52 +0200

    Add getMetadata function (#1642)

--- src/utils/tokens.js
+++ src/utils/tokens.js
@@ -6,3 +6,18 @@ import { ACCOUNT_HELPER_URL } from './wallet'
 export const getLikelyTokens = async (accountId) => {
     return await sendJson('GET', `${ACCOUNT_HELPER_URL}/account/${accountId}/likelyTokens`)
 }
+
+export const getMetadata = async (contract, account) => {
+    let metadata
+
+    try {
+        metadata = await account.viewFunction(contract, 'ft_metadata')
+    } catch (e) {
+        // logError(e);
+    } finally {
+        return {
+            contract,
+            metadata
+        }
+    }
+}
