f19e17ebe28a01dca12bbf0831faaccec8d2d354
marcinbodnardesktop <me@marcinbodnar.com>
2021-05-27 21:48:56 +0200

    Log error on getLikelyTokens (#1642)

--- src/utils/tokens.js
+++ src/utils/tokens.js
@@ -4,7 +4,11 @@ import { ACCOUNT_HELPER_URL } from './wallet'
 import * as Sentry from '@sentry/browser'
 
 export const getLikelyTokens = async (accountId) => {
-    return await sendJson('GET', `${ACCOUNT_HELPER_URL}/account/${accountId}/likelyTokens`)
+    try {
+        return await sendJson('GET', `${ACCOUNT_HELPER_URL}/account/${accountId}/likelyTokens`)
+    } catch (e) {
+        logError(e);
+    }
 }
 
 export const getMetadata = async (contract, account) => {
