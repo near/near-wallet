899c0815c33a3e61863878ec260598d0e50fb914
marcinbodnardesktop <me@marcinbodnar.com>
2021-05-27 21:48:42 +0200

    Log error on getMetadata and getBalanceOf (#1642)

--- src/utils/tokens.js
+++ src/utils/tokens.js
@@ -1,7 +1,7 @@
 
 import sendJson from '../tmp_fetch_send_json'
 import { ACCOUNT_HELPER_URL } from './wallet'
-
+import * as Sentry from '@sentry/browser'
 
 export const getLikelyTokens = async (accountId) => {
     return await sendJson('GET', `${ACCOUNT_HELPER_URL}/account/${accountId}/likelyTokens`)
@@ -13,7 +13,7 @@ export const getMetadata = async (contract, account) => {
     try {
         metadata = await account.viewFunction(contract, 'ft_metadata')
     } catch (e) {
-        // logError(e);
+        logError(e);
     } finally {
         return {
             contract,
@@ -28,7 +28,7 @@ export const getBalanceOf = async (contract, account, accountId) => {
     try {
         balance = await account.viewFunction(contract, 'ft_balance_of', { account_id: accountId })
     } catch (e) {
-        // logError(e);
+        logError(e);
     } finally {
         return {
             contract,
