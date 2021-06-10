8c0b29033e2d3bf5aba8540b0e2c3a7ef071f2c7
marcinbodnardesktop <me@marcinbodnar.com>
2021-06-10 22:04:54 +0200

    Add combinedMainReducers to combine main reducers, not related to account (#1239)

---
+++ src/redux/reducers/combinedMainReducers.js
@@ -0,0 +1,30 @@
+import { connectRouter } from 'connected-react-router'
+import { localizeReducer } from 'react-localize-redux'
+
+import allAccounts from '../../reducers/allAccounts'
+import availableAccounts from '../../reducers/available-accounts'
+import account from '../../reducers/account'
+import transactions from '../../reducers/transactions'
+import sign from '../../reducers/sign'
+import recoveryMethods from '../../reducers/recoveryMethods'
+import ledger from '../../reducers/ledger'
+import staking from '../../reducers/staking'
+import status from '../../reducers/status'
+import flowLimitation from '../../reducers/flowLimitation'
+
+export default (history) => ({
+    localize: localizeReducer,
+    router: connectRouter(history),
+
+    // to be removed after redux refactor finish
+    allAccounts,
+    availableAccounts,
+    account,
+    transactions,
+    sign,
+    recoveryMethods,
+    ledger,
+    staking,
+    status,
+    flowLimitation,
+})
