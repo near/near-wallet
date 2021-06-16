ee7a82872ed0e4710038a8f77221a59373dfc482
marcinbodnar <me@marcinbodnar.com>
2021-06-17 01:29:30 +0200

    Reorganize action names (#1642)

--- src/actions/tokens.js
+++ src/actions/tokens.js
@@ -7,7 +7,7 @@ export const handleGetTokens = () => async (dispatch, getState) => {
     const { accountId } = getState().account
 
     const contractNames = [...new Set([...(await getLikelyContracts(accountId)), ...WHITELISTED_CONTRACTS])]
-    
+
     await Promise.all(contractNames.map(async contractName => {
         await dispatch(tokens.tokensDetails.getMetadata(contractName, accountId))
     }))
@@ -17,12 +17,10 @@ export const handleGetTokens = () => async (dispatch, getState) => {
     })
 }
 
-export const { tokens } = createActions({
-    TOKENS: {
-        TOKENS_DETAILS: {
-            GET_METADATA: getMetadata,
-            GET_BALANCE_OF: getBalanceOf
-        },
-        CLEAR_STATE: null
-    }
+export const tokens = createActions({
+    TOKENS_DETAILS: {
+        GET_METADATA: getMetadata,
+        GET_BALANCE_OF: getBalanceOf
+    },
+    CLEAR_STATE: null
 })
