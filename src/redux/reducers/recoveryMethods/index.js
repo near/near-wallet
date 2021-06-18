0607f3683fa744162b940e12b02779eccf323ef3
marcinbodnar <me@marcinbodnar.com>
2021-06-18 23:25:37 +0200

    Move recoverMethods reducer to new folder structure (#1239)

---
+++ src/redux/reducers/recoveryMethods/index.js
@@ -0,0 +1,15 @@
+import { handleAction } from 'redux-actions'
+import { loadRecoveryMethods } from '../../../actions/account'
+
+const initialState = {}
+
+const reducer = (state, event) => {
+    const { error, payload, ready } = event
+
+    if (!ready || error) return state
+
+    // payload is an array of recoveryMethods
+    return { ...state, [payload.accountId]: payload.data }
+}
+
+export default handleAction(loadRecoveryMethods, reducer, initialState)
