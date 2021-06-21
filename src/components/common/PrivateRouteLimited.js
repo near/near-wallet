25a7eefc0e5efc39a334ba97cbbafe481c46d10f
marcinbodnar <me@marcinbodnar.com>
2021-06-21 20:51:55 +0200

    Use state from active account reducer in PrivateRouteLimited component (#1239)

--- src/components/common/PrivateRouteLimited.js
+++ src/components/common/PrivateRouteLimited.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, withRouter, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { KEY_ACTIVE_ACCOUNT_ID } from '../../utils/wallet'
+import connectAccount from '../../redux/connectAccount'
 
 const PrivateRouteLimited = ({component: Component, account, refreshAccountOwnerEnded, ...rest}) => (
     <Route 
@@ -20,10 +21,10 @@ const PrivateRouteLimited = ({component: Component, account, refreshAccountOwner
     />
 )
 
-const mapStateToProps = ({ account, status }) => ({
+const mapStateToProps = ({ account }, { status }) => ({
     account,
     localAlert: status.localAlert,
     refreshAccountOwnerEnded: status.actionStatus.REFRESH_ACCOUNT_OWNER?.success === true
 })
 
-export default withRouter(connect(mapStateToProps)(PrivateRouteLimited))
+export default withRouter(connectAccount(mapStateToProps)(PrivateRouteLimited))
