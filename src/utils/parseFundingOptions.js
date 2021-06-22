8f5f827c5c8773c894cdf64edf9d95f3d836b258
Patrick Tajima <patrick.tajima@gmail.com>
2021-06-22 15:06:37 -0700

    fix wording

--- src/utils/parseFundingOptions.js
+++ src/utils/parseFundingOptions.js
@@ -1,7 +1,7 @@
 import { parse as parseQuery } from 'query-string'
 
-const parseFundingOptions = (string) => {
-    return JSON.parse(parseQuery(string).fundingOptions || 'null')
+const parseFundingOptions = (queryString) => {
+    return JSON.parse(parseQuery(queryString).fundingOptions || 'null')
 }
 
 export default parseFundingOptions;
