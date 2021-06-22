f59428609980ca872496d0c2e390dd2260d672c2
marcinbodnar <me@marcinbodnar.com>
2021-06-23 01:17:06 +0200

    Remove account reducer from main reducers (#1239)

--- src/redux/reducers/combinedMainReducers.js
+++ src/redux/reducers/combinedMainReducers.js
@@ -1,12 +1,7 @@
 import { connectRouter } from 'connected-react-router'
 import { localizeReducer } from 'react-localize-redux'
 
-import account from '../../reducers/account'
-
 export default (history) => ({
     localize: localizeReducer,
     router: connectRouter(history),
-
-    // to be removed after redux refactor finish
-    account,
 })
