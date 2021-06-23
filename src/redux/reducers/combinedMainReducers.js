da2fbbbd09464d91230e2ceeba43dbae42c1809c
marcinbodnar <me@marcinbodnar.com>
2021-06-23 23:19:07 +0200

    Add statusMain reducer to handle create and recover account for not logedin userd (#1239)

--- src/redux/reducers/combinedMainReducers.js
+++ src/redux/reducers/combinedMainReducers.js
@@ -1,7 +1,10 @@
 import { connectRouter } from 'connected-react-router'
 import { localizeReducer } from 'react-localize-redux'
 
+import status from './status'
+
 export default (history) => ({
     localize: localizeReducer,
     router: connectRouter(history),
+    statusMain: status
 })
