a7db44cab47a6db1a52186f80adb072437c0556b
marcinbodnar <me@marcinbodnar.com>
2021-06-24 23:41:05 +0200

    Move nft reducer to new folder structure (#1239)

---
+++ src/redux/reducers/nft/index.js
@@ -0,0 +1,64 @@
+import { handleActions } from 'redux-actions'
+import reduceReducers from 'reduce-reducers'
+
+import { nft } from '../../actions/nft'
+
+const initialState = {
+    likelyContracts: [],
+    nft: {}
+}
+
+const likelyContractsReducer = handleActions({
+    [nft.likelyContracts.get]: (state, { ready, error, payload: likelyContracts }) =>
+        (!ready || error)
+            ? state
+            : ({
+                ...state,
+                likelyContracts
+            }),
+}, initialState)
+
+const nftReducer = handleActions({
+    [nft.tokensDetails.getMetadata]: (state, { ready, error, payload }) =>
+        (!ready || error || !payload.metadata)
+            ? state
+            : ({
+                ...state,
+                nft: {
+                    ...state.nft,
+                    [payload.contractName]: {
+                        ...state.nft[payload.contractName],
+                        ...payload.metadata,
+                        contractName: payload.contractName
+                    }
+                }
+            }),
+    [nft.tokensDetails.getTokens]: (state, { ready, error, payload }) => 
+        (!ready || error)
+            ? state
+            : ({
+                ...state,
+                nft: {
+                    ...state.nft,
+                    [payload.contractName]: {
+                        ...state.nft[payload.contractName],
+                        tokens: payload.tokens
+                    }
+                }
+            }),
+}, initialState)
+
+const clearReducer = handleActions({
+    [nft.clearState]: () => ({
+        ...initialState
+    }),
+}, initialState)
+
+export default reduceReducers(
+    initialState,
+    likelyContractsReducer,
+    nftReducer,
+    clearReducer
+)
+
+export const selectNFT = state => state.nft.nft
