c5d93b330185a077ae92d5030f81f55561ab3019
marcinbodnar <me@marcinbodnar.com>
2021-06-25 20:40:00 +0200

    Add ledger reducer to main reducers (#1239)

--- src/redux/reducers/combinedMainReducers.js
+++ src/redux/reducers/combinedMainReducers.js
@@ -2,9 +2,11 @@ import { connectRouter } from 'connected-react-router'
 import { localizeReducer } from 'react-localize-redux'
 
 import status from './status'
+import ledger from './ledger'
 
 export default (history) => ({
     localize: localizeReducer,
     router: connectRouter(history),
-    statusMain: status
+    statusMain: status,
+    ledger
 })
