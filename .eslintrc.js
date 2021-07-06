97b285e5cd30847459bf2604226af1a5110a558f
Daryl Collins <daryl@darylcollins.com>
2021-07-06 12:43:20 -0700

    1888 - Add `import/order` and `semi: always` rules to eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,23 @@
 module.exports = {
     extends: 'react-app',
     rules: {
-        'jsx-a11y/no-access-key': 'off'
+        'jsx-a11y/no-access-key': 'off',
+        'semi': ['error', 'always'],
+        'import/order': [
+            'error',
+            {
+                alphabetize: {
+                    order: 'asc',
+                    caseInsensitive: true
+                },
+                'newlines-between': 'always',
+                groups: [
+                    'builtin',
+                    ['external', 'internal'],
+                    ['sibling', 'parent', 'index'],
+                    'object'
+                ]
+            }
+        ],
     }
 }
