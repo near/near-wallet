1c821de2d6fa26a7f8e90286cf6a9797a1d8951d
marcinbodnar <me@marcinbodnar.com>
2021-07-08 00:12:09 +0200

    Add transferStorageDeposit method (#1867)

--- src/services/fungibleTokens.js
+++ src/services/fungibleTokens.js
@@ -1,6 +1,14 @@
+import * as Sentry from '@sentry/browser';
+import * as nearApiJs from 'near-api-js';
 import { wallet } from '../utils/wallet';
 
 const FT_MINIMUM_STORAGE_BALANCE = '1250000000000000000000';
+const FT_STORAGE_DEPOSIT_GAS = '30000000000000';
+const {
+    transactions: {
+        functionCall
+    }
+} = nearApiJs;
 
 class FungibleTokens {
     constructor() {
@@ -13,6 +21,15 @@ class FungibleTokens {
 
     async checkStorageBalance(contractName, accountId) {
         return await this.account.viewFunction(contractName, 'storage_balance_of', { account_id: accountId }).catch(logError);
+    }
+
+    async transferStorageDeposit(contractName, accountId) {
+        await this.signAndSendTransaction(contractName, [
+            functionCall('storage_deposit', {
+                account_id: accountId,
+                registration_only: true,
+            }, FT_STORAGE_DEPOSIT_GAS, FT_MINIMUM_STORAGE_BALANCE)
+        ]);
     }
 
     async signAndSendTransaction(receiverId, actions) {
