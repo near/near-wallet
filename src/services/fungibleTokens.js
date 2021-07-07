0b5c15d65e0190a12e7e86a9306b72d57dfce6c8
marcinbodnar <me@marcinbodnar.com>
2021-07-08 00:05:38 +0200

    Add constructor (#1867)

--- src/services/fungibleTokens.js
+++ src/services/fungibleTokens.js
@@ -1,4 +1,9 @@
+import { wallet } from '../utils/wallet';
+
 class FungibleTokens {
+    constructor() {
+        this.account = wallet.getAccountBasic();
+    }
 }
 
 export const fungibleTokens = new FungibleTokens();
