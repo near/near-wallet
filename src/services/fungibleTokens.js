b5223a0fd96612514f77e2362eada9bcb7563022
marcinbodnar <me@marcinbodnar.com>
2021-07-08 00:08:11 +0200

    Add checkStorageBalance method (#1867)

--- src/services/fungibleTokens.js
+++ src/services/fungibleTokens.js
@@ -4,6 +4,15 @@ class FungibleTokens {
     constructor() {
         this.account = wallet.getAccountBasic();
     }
+
+    async checkStorageBalance(contractName, accountId) {
+        return await this.account.viewFunction(contractName, 'storage_balance_of', { account_id: accountId }).catch(logError);
+    }
 }
+
+const logError = (error) => {
+    console.warn(error);
+    Sentry.captureException(error);
+};
 
 export const fungibleTokens = new FungibleTokens();
