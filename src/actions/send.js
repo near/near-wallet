e8e7d85337668edcc9e899435c28974fe26d4f24
marcinbodnar <me@marcinbodnar.com>
2021-07-09 17:03:12 +0200

    Dispatch transfer action based on contractName prop (#1865)

--- src/actions/send.js
+++ src/actions/send.js
@@ -1,11 +1,14 @@
 import { createActions } from 'redux-actions';
 
 import { showAlert } from '../utils/alerts';
+import { wallet } from '../utils/wallet';
 
 export const transfer = ({ contractName, amount, memo, receiverId, isStorageBalanceAvailable }) => async (dispatch, gesState) => {
     if (!isStorageBalanceAvailable) {
         await dispatch(send.transfer.storageDeposit(contractName, receiverId));
     }
+
+    const { transaction, status } = await dispatch(send.transfer[contractName ? 'TOKENS' : 'NEAR'](contractName, amount, memo, receiverId));
 };
 
 export const { send } = createActions({
