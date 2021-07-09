71f1b59df32198eacb0ccdd9af926db87e6d94b4
marcinbodnar <me@marcinbodnar.com>
2021-07-09 17:01:22 +0200

    Dispatch storageDeposit action if storage balance is not available (#1865)

--- src/actions/send.js
+++ src/actions/send.js
@@ -1,7 +1,11 @@
 import { createActions } from 'redux-actions';
 
 import { showAlert } from '../utils/alerts';
+
 export const transfer = ({ contractName, amount, memo, receiverId, isStorageBalanceAvailable }) => async (dispatch, gesState) => {
+    if (!isStorageBalanceAvailable) {
+        await dispatch(send.transfer.storageDeposit(contractName, receiverId));
+    }
 };
 
 export const { send } = createActions({
