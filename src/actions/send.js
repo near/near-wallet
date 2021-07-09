aefda24055a6b3bb236acc83558af0f06ad3f976
marcinbodnar <me@marcinbodnar.com>
2021-07-09 17:00:38 +0200

    Add transfer.storageDeposit action (#1865)

--- src/actions/send.js
+++ src/actions/send.js
@@ -1,2 +1,16 @@
+import { createActions } from 'redux-actions';
+
+import { showAlert } from '../utils/alerts';
 export const transfer = ({ contractName, amount, memo, receiverId, isStorageBalanceAvailable }) => async (dispatch, gesState) => {
 };
+
+export const { send } = createActions({
+    SEND: {
+        TRANSFER: {
+            STORAGE_DEPOSIT: [
+                () => {},
+                () => showAlert({ onlyError: true })
+            ],
+        },
+    }
+});
