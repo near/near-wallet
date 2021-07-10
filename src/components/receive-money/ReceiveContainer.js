2fa90f5635805336d2cf81294d7c8adda5770966
Patrick Tajima <patrick.tajima@gmail.com>
2021-07-09 18:00:16 -0700

    Add 'TabSelector'

--- src/components/receive-money/ReceiveContainer.js
+++ src/components/receive-money/ReceiveContainer.js
@@ -4,21 +4,18 @@ import { withRouter } from 'react-router-dom';
 import styled from 'styled-components';
 
 import Container from '../common/styled/Container.css';
+import TabSelector from '../send/components/TabSelector';
 import AccountId from './components/AccountId';
 import AccountIdQRCode from './components/AccountIdQRCode';
 import AvailableBalance from './components/AvailableBalance';
 
 const StyledContainer = styled(Container)`
     > div {
-        :nth-of-type(1) {
-            margin-top: 60px;
-        }
-
         :nth-of-type(2) {
-            margin-top: 50px;
+            margin: 70px auto;
         }
 
-        :nth-of-type(3) {
+        :nth-of-type(4) {
             margin-top: 5px;
         }
     }
@@ -32,6 +29,7 @@ class ReceiveContainer extends Component {
 
         return (
             <StyledContainer className='small-centered'>
+                <TabSelector/>
                 <AccountIdQRCode
                     accountId={accountId}
                 />
---
+++ src/components/send/components/TabSelector.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Translate } from 'react-localize-redux';
+import { useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+import styled from 'styled-components';
+
+import { redirectTo } from '../../../actions/account';
+
+const StyledContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    border: 1px solid #F0F0F1;
+    border-radius: 8px;
+    overflow: hidden;
+    margin-top: 50px;
+
+    > div {
+        flex: 1;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: #F0F0F1;
+        color: #A2A2A8;
+        font-weight: 600;
+        padding: 15px;
+        cursor: pointer;
+        font-size: 16px;
+        transition: color 100ms;
+
+        &:not(.active) {
+            :hover {
+                color: black;
+            }
+        }
+
+        &.active {
+            background-color: white;
+            color: #0072CE;
+            cursor: default;
+        }
+    }
+
+    @media (max-width: 500px) {
+        margin: -26px -14px 0 -14px;
+        border-radius: 0;
+        border-bottom: 0;
+    }
+`;
+
+const TabSelector = () => {
+    const dispatch = useDispatch();
+    const location = useLocation();
+    const pathname = location.pathname;
+    const sendMoneyRoute = '/send-money';
+    const receiveMoneyRoute = '/receive-money';
+
+    return (
+        <StyledContainer>
+            <div 
+                role='button'
+                className={pathname === sendMoneyRoute ? 'active' : ''}
+                onClick={pathname !== sendMoneyRoute ? () => dispatch(redirectTo(sendMoneyRoute)) : null}
+            >
+                <Translate id='button.send'/>
+            </div>
+            <div
+                role='button'
+                className={pathname === receiveMoneyRoute ? 'active' : ''}
+                onClick={pathname !== receiveMoneyRoute ? () => dispatch(redirectTo(receiveMoneyRoute)) : null}
+            >
+                <Translate id='button.receive'/>
+            </div>
+        </StyledContainer>
+    );
+};
+
+export default TabSelector;
