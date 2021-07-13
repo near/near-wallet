0cf820fd73cb35f873e29be1f1a44ea8c2df741c
marcinbodnar <me@marcinbodnar.com>
2021-07-13 20:31:30 +0200

    Handle send.transfer.near action (#1866)

--- src/reducers/send/index.js
+++ src/reducers/send/index.js
@@ -1,11 +1,36 @@
 import reduceReducers from 'reduce-reducers';
 import { handleActions } from 'redux-actions';
 
+import { send } from '../../actions/send'
+
 const initialState = {
     items: []
 }
 
 const sendReducer = handleActions({
+    [send.transfer.near]: (state, { payload, meta }) => 
+        (!ready || error)
+            ? state
+            : ({
+                ...state,
+                items: [
+                    ...state.items,
+                    {
+                        hash: payload.transaction.hash,
+                        type: 'NEAR',
+                        details: {
+                            amount: meta.amount,
+                            receiver: {
+                                accountId: meta.receiverId
+                            }
+                        },
+                        status: {
+                            txStatus: 'pending',
+                            networkFees: payload.transaction_outcome.outcome.gas_burnt,
+                        }
+                    }
+                ]
+            }),
 }, initialState)
 
 export default reduceReducers(
