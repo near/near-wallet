631b9e7e50f47a70a060f96cc21de6acb8a5768c
marcinbodnar <me@marcinbodnar.com>
2021-07-23 01:00:30 +0200

    Set has2fa to false on disableMultisig success (#1879)

--- src/utils/twoFactor.js
+++ src/utils/twoFactor.js
@@ -63,12 +63,14 @@ export class TwoFactor extends Account2FA {
     async deployMultisig() {
         const contractBytes = new Uint8Array(await (await fetch('/multisig.wasm')).arrayBuffer());
         await super.deployMultisig(contractBytes);
+        this.has2fa = true
     }
 
     async disableMultisig() {
         const contractBytes = new Uint8Array(await (await fetch('/main.wasm')).arrayBuffer());
         const result = await this.disable(contractBytes);
         await store.dispatch(refreshAccount());
+        this.has2fa = false
         return result;
     }
 }
