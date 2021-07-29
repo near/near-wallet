543f686eab90efdf867f1fc7e0c608e192010728
Patrick Tajima <patrick.tajima@gmail.com>
2021-07-29 11:13:54 -0700

    Token fiat value: add reducer and action

---
+++ src/actions/tokenFiatValue.js
@@ -0,0 +1,8 @@
+import { createAction } from 'redux-actions';
+
+import sendJson from '../tmp_fetch_send_json';
+import { ACCOUNT_HELPER_URL } from '../utils/wallet';
+
+export const getTokenFiatValue = createAction('GET_TOKEN_FIAT_VALUE', async () => {
+    return await sendJson('GET', ACCOUNT_HELPER_URL + `/fiat`);
+});
