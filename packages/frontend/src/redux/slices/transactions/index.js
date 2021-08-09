86b9b27e0e099f65984f5357b4fbf5288ec3ab90
marcinbodnar <me@marcinbodnar.com>
2021-08-09 20:20:10 +0200

    Create selectOneTransactionByHash selector (#2003)

--- packages/frontend/src/redux/slices/transactions/index.js
+++ packages/frontend/src/redux/slices/transactions/index.js
@@ -10,10 +10,17 @@ function createParameterSelector(selector) {
 
 const getAccountIdParam = createParameterSelector((params) => params.accountId);
 
+const getHashParam = createParameterSelector((params) => params.hash);
+
 // Top level selectors
 const selectTransactionsSlice = (state) => state[SLICE_NAME] || {};
 
 export const selectTransactionsByAccountId = createSelector(
     [selectTransactionsSlice, getAccountIdParam],
     (transactions, accountId) => transactions[accountId] || []
+);
+
+export const selectOneTransactionByHash = createSelector(
+    [selectTransactionsByAccountId, getHashParam],
+    (transactionsByAccountId, hash) => transactionsByAccountId.find((transaction) => `${transaction.hash}-${transaction.kind}` === hash)
 );
