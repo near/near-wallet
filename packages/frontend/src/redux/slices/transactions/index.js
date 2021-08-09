1d198d945fc55bc5a9afe46fa57db360a10ed7fd
marcinbodnar <me@marcinbodnar.com>
2021-08-09 20:16:56 +0200

    Create createParameterSelector (#2003)

--- packages/frontend/src/redux/slices/transactions/index.js
+++ packages/frontend/src/redux/slices/transactions/index.js
@@ -1,1 +1,7 @@
 const SLICE_NAME = 'transactions';
+
+// A helper function to create the parameter selectors
+// Ref: https://flufd.github.io/reselect-with-multiple-parameters/
+function createParameterSelector(selector) {
+    return (_, params) => selector(params);
+}
