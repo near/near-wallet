5f1ce1d8625e6ed597a804ea8bf650c00b0a67d0
marcinbodnar <me@marcinbodnar.com>
2021-08-09 20:18:14 +0200

    Create selectTransactionsByAccountId selector (#2003)

--- packages/frontend/src/redux/slices/transactions/index.js
+++ packages/frontend/src/redux/slices/transactions/index.js
@@ -12,3 +12,8 @@ const getAccountIdParam = createParameterSelector((params) => params.accountId);
 
 // Top level selectors
 const selectTransactionsSlice = (state) => state[SLICE_NAME] || {};
+
+export const selectTransactionsByAccountId = createSelector(
+    [selectTransactionsSlice, getAccountIdParam],
+    (transactions, accountId) => transactions[accountId] || []
+);
