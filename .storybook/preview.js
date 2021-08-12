9f9b84641932f7524c566859349299865485fcb9
Corwin Harrell <hello@corwinharrell.com>
2021-08-12 10:30:04 -0400

    Upgrade storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,3 @@
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import { theme } from '../src/theme';
-
-export const decorators = [
-  (Story) => (
-    <ThemeProvider theme={theme}>
-      <Story />
-    </ThemeProvider>
-  ),
-];
-
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   layout: 'centered',
--- src/stitches.config.js
+++ src/stitches.config.js
@@ -1,23 +1,52 @@
 import { createCss } from '@stitches/react';
 
-export const {
-  styled,
-  css,
-  global,
-  keyframes,
-  getCssString,
-  theme,
-} = createCss({
+export const { styled, css } = createCss({
   theme: {
     colors: {
-      gray400: 'gainsboro',
-      gray500: 'lightgray',
+      gray500: 'hsl(206,10%,76%)',
+      blue500: 'hsl(206,100%,50%)',
+      purple500: 'hsl(252,78%,60%)',
+      green500: 'hsl(148,60%,60%)',
+      red500: 'hsl(352,100%,62%)',
     },
+
+    space: {
+      1: '5px',
+      2: '10px',
+      3: '15px',
+    },
+
+    fontSizes: {
+      1: '12px',
+      2: '13px',
+      3: '15px',
+    },
+
+    fonts: {
+      untitled: 'Untitled Sans, apple-system, sans-serif',
+      mono: 'Söhne Mono, menlo, monospace',
+    },
+
+    fontWeights: {},
+    lineHeights: {},
+    letterSpacings: {},
+    sizes: {},
+    borderWidths: {},
+    borderStyles: {},
+    radii: {},
+    shadows: {},
+    zIndices: {},
+    transitions: {},
   },
+
   media: {
     bp1: '(min-width: 480px)',
   },
+
   utils: {
-    marginX: (config) => (value) => ({ marginLeft: value, marginRight: value }),
+    marginX: (config) => (value) => ({
+      marginLeft: value,
+      marginRight: value,
+    }),
   },
 });
