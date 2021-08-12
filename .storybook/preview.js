01ecde2c3769540e5d66f29e8573456585229af9
Corwin Harrell <hello@corwinharrell.com>
2021-08-12 10:19:00 -0400

    Add decorator to storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,17 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../src/theme';
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   layout: 'centered',
   controls: { expanded: true },
-}
+};
