9220151b73ec83608df9778f4b92a738a174557f
Corwin Harrell <hello@corwinharrell.com>
2021-08-12 10:24:27 -0400

    Add stitches.config

---
+++ src/stitches.config.js
@@ -0,0 +1,23 @@
+import { createCss } from '@stitches/react';
+
+export const {
+  styled,
+  css,
+  global,
+  keyframes,
+  getCssString,
+  theme,
+} = createCss({
+  theme: {
+    colors: {
+      gray400: 'gainsboro',
+      gray500: 'lightgray',
+    },
+  },
+  media: {
+    bp1: '(min-width: 480px)',
+  },
+  utils: {
+    marginX: (config) => (value) => ({ marginLeft: value, marginRight: value }),
+  },
+});
