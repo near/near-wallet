fd5521218263e82e3648538412c1576f2e731fb4
marcinbodnar <me@marcinbodnar.com>
2021-08-18 22:12:38 +0200

    Remove unused switchAccount from actions

--- packages/frontend/src/actions/account.js
+++ packages/frontend/src/actions/account.js
@@ -1,4 +1,3 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
 import { BN } from 'bn.js';
 import { push } from 'connected-react-router';
 import { utils } from 'near-api-js';
@@ -570,16 +569,6 @@ export const refreshAccount = (basicData = false) => async (dispatch, getState) 
         dispatch(getBalance('', flowLimitation.accountData));
     }
 };
-
-export const switchAccount = createAsyncThunk(
-    'switchAccount',
-    async (accountId, { dispatch }) => {
-        dispatch(makeAccountActive(accountId));
-        dispatch(handleRefreshUrl());
-        dispatch(refreshAccount());
-        dispatch(clearAccountState());
-    }
-);
 
 export const clearAccountState = () => async (dispatch, getState) => {
     dispatch(staking.clearState());
