a2e3a8d0db060e56b2642446dd6e110f8ea58a4d
marcinbodnar <me@marcinbodnar.com>
2021-08-21 00:17:24 +0200

    Add initialState for tokens slice (#2049)

--- packages/frontend/src/redux/slices/tokens/index.js
+++ packages/frontend/src/redux/slices/tokens/index.js
@@ -1,1 +1,16 @@
 const SLICE_NAME = 'tokens';
+
+const initialState = {
+    ownedTokens: {
+        byAccountId: {}
+    },
+    metadata: {
+        byContractName: {}
+    }
+};
+
+const initialOwnedTokenState = {
+    balance: '',
+    loading: false,
+    error: null
+};
