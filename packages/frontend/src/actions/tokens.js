ec42137eeeb902942ee2d754f432c7da2c5ec533
Henry Tafolla <htafolla@gmail.com>
2021-08-23 11:36:05 -0500

    Hotfix for METAPool token in wallet

--- packages/frontend/src/actions/tokens.js
+++ packages/frontend/src/actions/tokens.js
@@ -2,7 +2,7 @@ import { createActions } from 'redux-actions';
 
 import { getLikelyTokenContracts, getMetadata, getBalanceOf } from '../utils/tokens';
 
-const WHITELISTED_CONTRACTS = (process.env.TOKEN_CONTRACTS || 'berryclub.ek.near,farm.berryclub.ek.near,wrap.near,token.cheddar.testnet').split(',');
+const WHITELISTED_CONTRACTS = (process.env.TOKEN_CONTRACTS || 'berryclub.ek.near,farm.berryclub.ek.near,wrap.near,token.cheddar.testnet,meta-pool.near,meta-token.near').split(',');
 
 export const handleGetTokens = () => async (dispatch, getState) => {
     const { accountId } = getState().account;
