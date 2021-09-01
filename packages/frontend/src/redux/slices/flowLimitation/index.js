0b9d213e51a786eb0d9fa2e5cd9f5d93b7aadf63
marcinbodnar <me@marcinbodnar.com>
2021-09-01 17:57:41 +0200

    Create flowLimitationSlice (#2079)

--- packages/frontend/src/redux/slices/flowLimitation/index.js
+++ packages/frontend/src/redux/slices/flowLimitation/index.js
@@ -8,3 +8,15 @@ const initialState = {
     accountBalance: false
 };
 
+const flowLimitationSlice = createSlice({
+    name: SLICE_NAME,
+    initialState,
+    reducers: {
+        setFlowLimitation(state, { payload }) {
+            assign(state, payload);
+        },
+        clearFlowLimitation(state) {
+            assign(state, initialState);
+        }
+    }
+});
