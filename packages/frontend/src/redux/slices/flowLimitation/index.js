4a92dbe2036d253d099a5b98fae89b6e541741c5
marcinbodnar <me@marcinbodnar.com>
2021-09-01 17:59:28 +0200

    Create handleFlowLimitation action (#2079)

--- packages/frontend/src/redux/slices/flowLimitation/index.js
+++ packages/frontend/src/redux/slices/flowLimitation/index.js
@@ -8,6 +8,36 @@ const initialState = {
     accountBalance: false
 };
 
+const handleFlowLimitation = createAsyncThunk(
+    `${SLICE_NAME}/handleFlowLimitation`,
+    async (_, thunkAPI) => {
+        const { dispatch, getState } = thunkAPI;
+        const { pathname } = getState().router.location;
+        const { redirect_url } = getState().account.url;
+        const { actions: { setFlowLimitation } } = flowLimitationSlice;
+
+        const redirectUrl = redirect_url || pathname;
+
+        if (redirectUrl.includes(WALLET_LOGIN_URL)) {
+            dispatch(setFlowLimitation({
+                mainMenu: true,
+                subMenu: false,
+                accountPages: false,
+                accountData: true,
+                accountBalance: true
+            }));
+        } 
+        else if (redirectUrl.includes(WALLET_SIGN_URL)) {
+            dispatch(setFlowLimitation({
+                mainMenu: true,
+                subMenu: true,
+                accountPages: true,
+                accountData: true,
+                accountBalance: false
+            }));
+        }
+    }
+);
 const flowLimitationSlice = createSlice({
     name: SLICE_NAME,
     initialState,
