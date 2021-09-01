bf6b271bc396b76f85fa1406cd8f5bf1503ede92
marcinbodnar <me@marcinbodnar.com>
2021-09-01 17:57:19 +0200

    Add initialState for flow limitation slice (#2079)

--- packages/frontend/src/redux/slices/flowLimitation/index.js
+++ packages/frontend/src/redux/slices/flowLimitation/index.js
@@ -1,1 +1,10 @@
 const SLICE_NAME = 'flowLimitation';
+
+const initialState = {
+    mainMenu: false,
+    subMenu: false,
+    accountPages: false,
+    accountData: false,
+    accountBalance: false
+};
+
