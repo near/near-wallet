86cfd57062b43ba24ef2fd9fa16f1b5a823e52e1
BenKurrek <bkurrek@uwaterloo.ca>
2021-09-09 17:11:13 -0400

    Added account filtering to account dropdowns and account selector (only on desktop for the latter)

--- packages/frontend/src/components/login/SelectAccountDropdown.js
+++ packages/frontend/src/components/login/SelectAccountDropdown.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { Translate } from 'react-localize-redux';
-import { Segment } from 'semantic-ui-react';
-import styled from 'styled-components';
+import { filter } from "lodash";
+import React, { useEffect, useState } from "react";
+import { Translate } from "react-localize-redux";
+import { Segment } from "semantic-ui-react";
+import styled from "styled-components";
 
-import AddBlueImage from '../../images/icon-add-blue.svg';
-import ArrowDownImage from '../../images/icon-arrow-down.svg';
-import ArrowUpImage from '../../images/icon-arrow-up.svg';
-import classNames from '../../utils/classNames';
-import { DISABLE_CREATE_ACCOUNT } from '../../utils/wallet';
+import AddBlueImage from "../../images/icon-add-blue.svg";
+import ArrowDownImage from "../../images/icon-arrow-down.svg";
+import ArrowUpImage from "../../images/icon-arrow-up.svg";
+import classNames from "../../utils/classNames";
+import { DISABLE_CREATE_ACCOUNT } from "../../utils/wallet";
 
 const CustomSegment = styled(Segment)`
     &&& {
@@ -15,10 +16,8 @@ const CustomSegment = styled(Segment)`
         width: 100%;
         height: 50px;
         padding: 0px;
-
         &.disabled {
             cursor: not-allowed;
-
             .item {
                 :hover {
                     cursor: not-allowed !important;
@@ -26,7 +25,6 @@ const CustomSegment = styled(Segment)`
                 }
             }
         }
-
         .segment {
             padding: 0px;
             position: absolute;
@@ -36,108 +34,97 @@ const CustomSegment = styled(Segment)`
             border: 2px solid #24272a;
             border-radius: 3px;
             background: #fff;
-
-        .item {
-            height: 46px;
-            color: #24272a;
-            padding: 0 0 0 12px;
-            font-size: 18px;
-            font-weight: 600;
-            text-overflow: ellipsis;
-            overflow: hidden;
-            cursor: pointer;
-            transition: 100ms;
-            display: flex;
-            align-items: center;
-
-            :hover {
-                color: #0072ce;
-            }
-        }
-
-        .list-title {
-            border-bottom: 2px solid #f2f2f2;
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-
-            > div {
-                float: left;
+            .item {
+                height: 46px;
+                color: #24272a;
+                padding: 0 0 0 12px;
+                font-size: 18px;
+                font-weight: 600;
                 text-overflow: ellipsis;
                 overflow: hidden;
-                width: 82%;
-            }
-            .arrow {
-                float: right;
-                width: 48px;
-                height: 100%;
-                background-image: url(${ArrowDownImage});
-                background-repeat: no-repeat;
-                background-position: center center;
-                background-size: 16px auto;
-
-                &.up {
-                    background-image: url(${ArrowUpImage});
+                cursor: pointer;
+                transition: 100ms;
+                display: flex;
+                align-items: center;
+                :hover {
+                    color: #0072ce;
                 }
             }
-            .dots {
-                color: #24272a;
-
-                :after {
-                    content: '.';
-                    animation: link 1s steps(5, end) infinite;
-                
-                    @keyframes link {
-                        0%, 20% {
-                            color: rgba(0,0,0,0);
-                            text-shadow:
-                                .3em 0 0 rgba(0,0,0,0),
-                                .6em 0 0 rgba(0,0,0,0);
-                        }
-                        40% {
-                            color: #24272a;
-                            text-shadow:
-                                .3em 0 0 rgba(0,0,0,0),
-                                .6em 0 0 rgba(0,0,0,0);
-                        }
-                        60% {
-                            text-shadow:
-                                .3em 0 0 #24272a,
-                                .6em 0 0 rgba(0,0,0,0);
-                        }
-                        80%, 100% {
-                            text-shadow:
-                                .3em 0 0 #24272a,
-                                .6em 0 0 #24272a;
+            .list-title {
+                border-bottom: 2px solid #f2f2f2;
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                > div {
+                    float: left;
+                    text-overflow: ellipsis;
+                    overflow: hidden;
+                    width: 82%;
+                }
+                .arrow {
+                    float: right;
+                    width: 48px;
+                    height: 100%;
+                    background-image: url(${ArrowDownImage});
+                    background-repeat: no-repeat;
+                    background-position: center center;
+                    background-size: 16px auto;
+                    &.up {
+                        background-image: url(${ArrowUpImage});
+                    }
+                }
+                .dots {
+                    color: #24272a;
+                    :after {
+                        content: ".";
+                        animation: link 1s steps(5, end) infinite;
+                        @keyframes link {
+                            0%,
+                            20% {
+                                color: rgba(0, 0, 0, 0);
+                                text-shadow: 0.3em 0 0 rgba(0, 0, 0, 0),
+                                    0.6em 0 0 rgba(0, 0, 0, 0);
+                            }
+                            40% {
+                                color: #24272a;
+                                text-shadow: 0.3em 0 0 rgba(0, 0, 0, 0),
+                                    0.6em 0 0 rgba(0, 0, 0, 0);
+                            }
+                            60% {
+                                text-shadow: 0.3em 0 0 #24272a,
+                                    0.6em 0 0 rgba(0, 0, 0, 0);
+                            }
+                            80%,
+                            100% {
+                                text-shadow: 0.3em 0 0 #24272a,
+                                    0.6em 0 0 #24272a;
+                            }
                         }
                     }
                 }
             }
-        }
-        .list-scroll {
-            max-height: 140px;
-            overflow: auto;
-
-            .item {
-                border-top: 2px solid #f2f2f2;
-
-               :first-of-type {
-                    border-top: 0px solid #f2f2f2;
+            .list-scroll {
+                max-height: 140px;
+                overflow: auto;
+                .item {
+                    border-top: 2px solid #f2f2f2;
+                    :first-of-type {
+                        border-top: 0px solid #f2f2f2;
+                    }
                 }
             }
-        }
-        .list-create {
-            background: #24272a;
-            text-transform: uppercase;
-            color: #24272a;
-            padding: 0 0 0 60px;
-            background-image: url(${AddBlueImage});
-            background-position: 12px center;
-            background-repeat: no-repeat;
-            background-size: 24px 24px;
+            .list-create {
+                background: #24272a;
+                text-transform: uppercase;
+                color: #24272a;
+                padding: 0 0 0 60px;
+                background-image: url(${AddBlueImage});
+                background-position: 12px center;
+                background-repeat: no-repeat;
+                background-size: 24px 24px;
+            }
         }
     }
-}
 `;
 
 const SelectAccountDropdown = ({
@@ -147,51 +134,122 @@ const SelectAccountDropdown = ({
     dropdown,
     handleSelectAccount,
     redirectCreateAccount,
-    disabled
-    }) => (
-    <Translate>
-        {({ translate }) => (
-            <CustomSegment
-                basic
-                onClick={!disabled ? handleOnClick : () => { }}
-                className={disabled && 'disabled'}
-                title={!dropdown ? (!disabled ? translate('selectAccountDropdown.switchAccount') : translate('selectAccountDropdown.switchAccounthNotAllowed')) : ''}
-            >
-                <Segment basic>
-                    <div className='item list-title'>
-                        {dropdown ? translate('button.close') : <div className={classNames({dots: !account.accountId})}>{account.accountId}</div>}
-                        <div className='arrow' />
-                    </div>
-                    <div className={`${dropdown ? '' : 'hide'}`}>
-                        <div className='list-scroll'>
-                            {availableAccounts
-                                .filter(a => a !== account.accountId)
-                                .map(a => (
-                                    <div
-                                        onClick={() => handleSelectAccount(a)}
-                                        className='item'
-                                        key={a}
-                                        title={translate('selectAccountDropdown.selectAccount')}
-                                    >{a}</div>
-                                ))}
-                            {availableAccounts.length < 2 &&
-                                <div className='item'>{translate('selectAccountDropdown.noOtherAccounts')}</div>
-                            }
+    disabled,
+}) => {
+    const [filteredAvailableAccounts, setFilteredAvailableAccounts] = useState(
+        availableAccounts
+    );
+    const [currentFilter, setCurrentFilter] = useState("");
+
+    useEffect(() => {
+        if (availableAccounts.length > 0) {
+            setFilteredAvailableAccounts(availableAccounts);
+        }
+    }, [availableAccounts]);
+
+    useEffect(() => {
+        setFilteredAvailableAccounts(
+            availableAccounts.filter(
+                (f) => f.includes(currentFilter) || currentFilter === ""
+            )
+        );
+    }, [currentFilter]);
+
+    return (
+        <Translate>
+            {({ translate }) => (
+                <CustomSegment
+                    basic
+                    className={disabled && "disabled"}
+                    title={
+                        !dropdown
+                            ? !disabled
+                                ? translate(
+                                      "selectAccountDropdown.switchAccount"
+                                  )
+                                : translate(
+                                      "selectAccountDropdown.switchAccounthNotAllowed"
+                                  )
+                            : ""
+                    }
+                >
+                    <Segment basic>
+                        <div className="item list-title"
+                        onClick={!disabled ? handleOnClick : () => {}}
+                        >
+                            {dropdown ? (
+                                translate("button.close")
+                            ) : (
+                                <div
+                                    className={classNames({
+                                        dots: !account.accountId,
+                                    })}
+                                >
+                                    {account.accountId}
+                                </div>
+                            )}
+                            <div className="arrow" />
                         </div>
-                        {!DISABLE_CREATE_ACCOUNT &&
-                            <div
-                                onClick={redirectCreateAccount}
-                                className='item list-create color-seafoam-blue'
-                                title={translate('selectAccountDropdown.createAccount')}
+                        <div className={`${dropdown ? "" : "hide"}`}>
+                            <input
+                                id="filter"
+                                name="filter"
+                                type="text"
+                                placeholder={'Search For Accounts'}
+                                value={currentFilter}
+                                onChange={(event) =>
+                                    setCurrentFilter(event.target.value)
+                                }
+                            />
+                            <div className="list-scroll"
+                            onClick={!disabled ? handleOnClick : () => {}}
                             >
-                                {translate('selectAccountDropdown.createAccount')}
+                                {filteredAvailableAccounts
+                                    .filter((a) => a !== account.accountId)
+                                    .map((a) => (
+                                        <div
+                                            onClick={() => {
+                                                !disabled && handleOnClick
+                                                handleSelectAccount(a)
+                                            }
+                                                
+                                            }
+                                            className="item"
+                                            key={a}
+                                            title={translate(
+                                                "selectAccountDropdown.selectAccount"
+                                            )}
+                                        >
+                                            {a}
+                                        </div>
+                                    ))}
+                                {availableAccounts.length < 2 && (
+                                    <div className="item">
+                                        {translate(
+                                            "selectAccountDropdown.noOtherAccounts"
+                                        )}
+                                    </div>
+                                )}
                             </div>
-                        }
-                    </div>
-                </Segment>
-            </CustomSegment>
-        )}
-    </Translate>
-);
+                            {!DISABLE_CREATE_ACCOUNT && (
+                                <div
+                                    onClick={redirectCreateAccount}
+                                    className="item list-create color-seafoam-blue"
+                                    title={translate(
+                                        "selectAccountDropdown.createAccount"
+                                    )}
+                                >
+                                    {translate(
+                                        "selectAccountDropdown.createAccount"
+                                    )}
+                                </div>
+                            )}
+                        </div>
+                    </Segment>
+                </CustomSegment>
+            )}
+        </Translate>
+    );
+};
 
 export default SelectAccountDropdown;
--- packages/frontend/src/components/navigation/DesktopMenu.js
+++ packages/frontend/src/components/navigation/DesktopMenu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { Translate } from 'react-localize-redux';
 import styled from 'styled-components';
 
@@ -16,11 +16,9 @@ const Menu = styled.div`
     width: 320px;
     box-shadow: 0px 45px 56px rgba(0, 0, 0, 0.07), 0px 10.0513px 12.5083px rgba(0, 0, 0, 0.0417275), 0px 2.99255px 3.72406px rgba(0, 0, 0, 0.0282725);
     padding: 16px;
-
     .user-links {
         padding: 20px;
     }
-
     button {
         width: 100% !important;
     }
@@ -38,12 +36,30 @@ const DesktopMenu = ({
     getBalance,
     isInactiveAccount
 }) => {
+    const [filteredAvailableAccounts, setFilteredAvailableAccounts] = useState(
+        accounts
+    );
+    const [currentFilter, setCurrentFilter] = useState("");
+
+    useEffect(() => {
+        if (accounts.length > 0) {
+            setFilteredAvailableAccounts(accounts);
+        }
+    }, [accounts]);
+
+    useEffect(() => {
+        setFilteredAvailableAccounts(
+            accounts.filter(
+                (f) => f.includes(currentFilter) || currentFilter === ""
+            )
+        );
+    }, [currentFilter]);
     if (show) {
         return (
             <Menu id='desktop-menu'>
                 <h6><Translate id='link.switchAccount'/></h6>
                 <UserAccounts
-                    accounts={accounts}
+                    accounts={filteredAvailableAccounts}
                     accountId={accountId}
                     accountIdLocalStorage={accountIdLocalStorage}
                     handleSelectAccount={handleSelectAccount}
@@ -51,6 +67,16 @@ const DesktopMenu = ({
                     balance={balance}
                     refreshBalance={refreshBalance}
                     getBalance={getBalance}
+                />
+                <input
+                    id="filter"
+                    name="filter"
+                    type="text"
+                    placeholder={'Search For Accounts'}
+                    value={currentFilter}
+                    onChange={(event) =>
+                        setCurrentFilter(event.target.value)
+                    }
                 />
                 <AccessAccountBtn/>
                 {!isInactiveAccount &&
