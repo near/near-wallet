8e84b02d35a0817eac523513a38dd6fde4412a33
marcinbodnar <me@marcinbodnar.com>
2021-09-14 01:49:48 +0200

    Use selectSlice in linkdrop slice (#2107)

--- packages/frontend/src/redux/slices/linkdrop/index.js
+++ packages/frontend/src/redux/slices/linkdrop/index.js
@@ -1,6 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { createSelector } from 'reselect';
 
+import selectSlice from '../selectSlice';
+
 const SLICE_NAME = 'linkdrop';
 
 const initialState = {
@@ -24,5 +26,5 @@ export const actions = {
 };
 export const reducer = linkdropSlice.reducer;
 
-const selectLinkdropSlice = (state) => state[linkdropSlice.name];
+const selectLinkdropSlice = selectSlice(SLICE_NAME);
 export const selectLinkdropAmount = createSelector(selectLinkdropSlice, ({ amount }) => amount);
