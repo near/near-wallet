9517fcb589e2ffaa3860e8eb8906afb8cbb8f26d
Daryl Collins <daryl@darylcollins.com>
2021-09-17 04:38:30 -0700

    Add react v3 provider and environment var for configuration

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -3,6 +3,7 @@ import "regenerator-runtime/runtime";
 import { createBrowserHistory } from 'history';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 import { LocalizeProvider } from 'react-localize-redux';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
@@ -11,6 +12,7 @@ import Routing from './components/Routing';
 import createRootReducer from './redux/combineReducers';
 import createMiddleware from './redux/middleware';
 import { initSentry } from './utils/sentry';
+import { RECAPTCHA_ENTERPRISE_SITE_KEY } from './utils/wallet';
 
 initSentry();
 
@@ -19,10 +21,16 @@ const history = createBrowserHistory();
 export const store = createStore(createRootReducer(history), createMiddleware(history));
 
 ReactDOM.render(
-    <Provider store={store}>
-        <LocalizeProvider store={store}>
-            <Routing history={history} />
-        </LocalizeProvider>
-    </Provider>,
+    <GoogleReCaptchaProvider
+        reCaptchaKey={RECAPTCHA_ENTERPRISE_SITE_KEY}
+        useRecaptchaNet={true}
+        useEnterprise={true}
+    >
+        <Provider store={store}>
+            <LocalizeProvider store={store}>
+                <Routing history={history}/>
+            </LocalizeProvider>
+        </Provider>
+    </GoogleReCaptchaProvider>,
     document.getElementById('root')
 );
