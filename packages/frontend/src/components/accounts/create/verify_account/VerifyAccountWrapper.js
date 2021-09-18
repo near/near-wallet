e8c5d78dfae9184852c2b343d39ee6790bbc6e3a
Daryl Collins <daryl@darylcollins.com>
2021-09-18 05:31:47 -0700

    Revert "Disable email verification method"
    This reverts commit c62893d36bc68ebb4c5adc407e5de4eeef8cfe8c.

--- packages/frontend/src/components/accounts/create/verify_account/VerifyAccountWrapper.js
+++ packages/frontend/src/components/accounts/create/verify_account/VerifyAccountWrapper.js
@@ -25,7 +25,7 @@ export function VerifyAccountWrapper() {
     const implicitAccountId = URLParams.get('implicitAccountId');
     const recoveryMethod = URLParams.get('recoveryMethod');
 
-    const [activeVerificationOption, setActiveVerificationOption] = useState('phone');
+    const [activeVerificationOption, setActiveVerificationOption] = useState('email');
     const [showEnterVerificationCode, setShowEnterVerificationCode] = useState(false);
     const [verifyingAndCreatingAccount, setVerifyingAndCreatingAccount] = useState(false);
     const [showOptionAlreadyUsedModal, setShowOptionAlreadyUsedModal] = useState(false);
@@ -159,7 +159,7 @@ export function VerifyAccountWrapper() {
             verificationNumber={verificationNumber}
             onChangeVerificationNumber={number => setVerificationNumber(number)}
             handleContinue={async () => {
-                if (activeVerificationOption === 'phone') {
+                if (activeVerificationOption === 'email' || activeVerificationOption === 'phone') {
                     try {
                         const recaptchaToken = await handleReCaptchaVerify('verifiedIdentityCreateVerificationMethod');
 
