62c2af25aea691ab8b431986a3fad084163af57d
marcinbodnar <me@marcinbodnar.com>
2021-09-21 20:54:14 +0200

    Add initial states (#2146)

--- packages/frontend/src/redux/slices/recoveryMethods/index.js
+++ packages/frontend/src/redux/slices/recoveryMethods/index.js
@@ -1,1 +1,13 @@
 const SLICE_NAME = 'recoveryMethods';
+
+const initialState = {
+    byAccountId: {}
+};
+
+const initialAccountIdState = {
+    items: [],
+    status: {
+        loading: false,
+        error: initialErrorState
+    }
+};
