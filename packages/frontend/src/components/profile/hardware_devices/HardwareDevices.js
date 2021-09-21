4d88a3ed59d94e8880e0561c20c66f2e3129acb3
marcinbodnar <me@marcinbodnar.com>
2021-09-21 21:02:36 +0200

    Use selectors in HardwareDevice componentn (#2146)

--- packages/frontend/src/components/profile/hardware_devices/HardwareDevices.js
+++ packages/frontend/src/components/profile/hardware_devices/HardwareDevices.js
@@ -12,7 +12,7 @@ import {
     addLedgerAccessKey,
     loadRecoveryMethods
 } from '../../../redux/actions/account';
-import { actionsPending } from '../../../utils/alerts';
+import { selectRecoveryMethodsLoading } from '../../../redux/slices/recoveryMethods';
 import FormButton from '../../common/FormButton';
 import SkeletonLoading from '../../common/SkeletonLoading';
 import Card from '../../common/styled/Card.css';
@@ -72,7 +72,7 @@ const HardwareDevices = ({ recoveryMethods }) => {
     const hasLedger = userRecoveryMethods.filter(method => method.kind === 'ledger').map(key => key.publicKey).some(key => publicKeys.includes(key));
     const ledgerIsConnected = account.ledgerKey;
     const hasLedgerButNotConnected = hasLedger && !ledgerIsConnected;
-    const recoveryLoader = (actionsPending('LOAD_RECOVERY_METHODS') && !userRecoveryMethods.length) || !account.accountId;
+    const recoveryLoader = (useSelector((state) => selectRecoveryMethodsLoading(state, { accountId: account.accountId })) && !userRecoveryMethods.length) || !account.accountId;
 
     const handleConfirmDisable = async () => {
         await Mixpanel.withTracking("SR-Ledger Handle confirm disable",
