576c70f6a34b8c413083614efa2d8eb27649571a
marcinbodnar <me@marcinbodnar.com>
2021-09-21 20:55:31 +0200

    Add recovery methods reducer (#2146)

--- packages/frontend/src/redux/slices/recoveryMethods/index.js
+++ packages/frontend/src/redux/slices/recoveryMethods/index.js
@@ -1,3 +1,11 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import set from 'lodash.set';
+import { createSelector } from 'reselect';
+
+import { wallet } from '../../../utils/wallet';
+import createParameterSelector from '../createParameterSelector';
+import initialErrorState from '../initialErrorState';
+
 const SLICE_NAME = 'recoveryMethods';
 
 const initialState = {
@@ -11,3 +19,12 @@ const initialAccountIdState = {
         error: initialErrorState
     }
 };
+
+
+const recoveryMethodsSlice = createSlice({
+        name: SLICE_NAME,
+        initialState,
+        reducers: {
+        },
+    }
+);
