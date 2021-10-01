6612a82d2299b907f1fcead81c6c8439a441be43
marcinbodnar <me@marcinbodnar.com>
2021-10-01 23:32:31 +0200

    Add selectStatusMainLoader selector (#1966)

--- packages/frontend/src/redux/slices/status/index.js
+++ packages/frontend/src/redux/slices/status/index.js
@@ -1,3 +1,8 @@
 import { createSelector } from "reselect";
 
 const SLICE_NAME = 'status';
+
+// Top level selectors
+export const selectStatusSlice = (state) => state[SLICE_NAME];
+
+export const selectStatusMainLoader = createSelector(selectStatusSlice, (status) => status.mianLoader);
