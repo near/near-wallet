45e2a5fc11b56ccebcab7be71b21785b53ceba04
marcinbodnar <me@marcinbodnar.com>
2021-10-01 22:42:29 +0200

    Add selectAccountId, selectBalance (#1966)

--- packages/frontend/src/redux/slices/account/index.js
+++ packages/frontend/src/redux/slices/account/index.js
@@ -1,3 +1,11 @@
 import { createSelector } from "reselect";
 
 const SLICE_NAME = 'account';
+
+// Top level selectors
+export const selectAccountSlice = (state) => state[SLICE_NAME];
+
+export const selectAccountId = createSelector(selectAccountSlice, (account) => account.accountId);
+
+export const selectBalance = createSelector(selectAccountSlice, (account) => account.balance);
+
