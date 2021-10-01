faa605f9bccfb821592fdf8a792d977a9bc630b8
marcinbodnar <me@marcinbodnar.com>
2021-10-02 00:51:56 +0200

    Add selectLedgerTxSigned and selectLedgerModal selectors (#1966)

--- packages/frontend/src/redux/slices/ledger/index.js
+++ packages/frontend/src/redux/slices/ledger/index.js
@@ -1,3 +1,10 @@
 import { createSelector } from "reselect";
 
 const SLICE_NAME = 'ledger';
+
+// Top level selectors
+export const selectLedgerSlice = (state) => state[SLICE_NAME];
+
+export const selectLedgerTxSigned = createSelector(selectLedgerSlice, (ledger) => ledger.txSigned);
+
+export const selectLedgerModal = createSelector(selectLedgerSlice, (ledger) => ledger.modal || {});
