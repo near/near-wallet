7fcf7d7797ed9f446df288b3f9f66f91a6f168c9
marcinbodnar <me@marcinbodnar.com>
2021-10-02 00:56:19 +0200

    Add selectLedgerHasLedger selector (#1966)

--- packages/frontend/src/redux/slices/ledger/index.js
+++ packages/frontend/src/redux/slices/ledger/index.js
@@ -8,3 +8,5 @@ export const selectLedgerSlice = (state) => state[SLICE_NAME];
 export const selectLedgerTxSigned = createSelector(selectLedgerSlice, (ledger) => ledger.txSigned);
 
 export const selectLedgerModal = createSelector(selectLedgerSlice, (ledger) => ledger.modal || {});
+
+export const selectLedgerHasLedger = createSelector(selectLedgerSlice, (ledger) => ledger.hasLedger);
