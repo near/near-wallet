3988b517b95797ba1f5e19ccc64dfcd6ae7fa98c
marcinbodnar <me@marcinbodnar.com>
2021-10-05 22:34:30 +0200

    Organize account selectors (#1966)

--- packages/frontend/src/redux/slices/account/index.js
+++ packages/frontend/src/redux/slices/account/index.js
@@ -5,20 +5,25 @@ const SLICE_NAME = 'account';
 // Top level selectors
 export const selectAccountSlice = (state) => state[SLICE_NAME];
 
+// Second level selectors 
 export const selectAccountId = createSelector(selectAccountSlice, (account) => account.accountId);
+export const selectAccountHas2fa = createSelector(selectAccountSlice, (account) => account.has2fa);
 
+// balance - state
 export const selectBalance = createSelector(selectAccountSlice, (account) => account.balance || {});
 
+// helperWalletState - state
 export const selectAccountHelperWalletState = createSelector(selectAccountSlice, (account) => account.accountHelperWalletState || {});
 
 export const selectAccountRequiredUnlockBalance = createSelector(selectAccountHelperWalletState, (accountHelperWalletState) => accountHelperWalletState.requiredUnlockBalance);
 
 export const selectAccountFundedAccountNeedsDeposit = createSelector(selectAccountHelperWalletState, (accountHelperWalletState) => accountHelperWalletState.fundedAccountNeedsDeposit);
 
-export const selectAccountHas2fa = createSelector(selectAccountSlice, (account) => account.has2fa);
-
+// url - state
 export const selectAccountUrl = createSelector(selectAccountSlice, (account) => account.url || {});
 
 export const selectAccountUrlReferrer = createSelector(selectAccountUrl, (url) => url.referrer);
 
 export const selectAccountUrlContractId = createSelector(selectAccountUrl, (url) => url.contract_id);
+
+
