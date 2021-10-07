6ae4f5937127a90ba7022cda0a17e48f02aa3a6a
marcinbodnar <me@marcinbodnar.com>
2021-10-07 20:28:23 +0200

    Use selectors for ReceiveContainer component (#1966)

--- packages/frontend/src/components/receive-money/ReceiveContainerWrapper.js
+++ packages/frontend/src/components/receive-money/ReceiveContainerWrapper.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
+import { selectAccountId, selectBalance } from '../../redux/slices/account';
 import ReceiveContainer from './ReceiveContainer';
 
 export function ReceiveContainerWrapper() {
-    const { accountId, balance } = useSelector(({ account }) => account);
+    const accountId = useSelector((state) => selectAccountId(state));
+    const balance = useSelector ((state) => selectBalance(state));
+
     const availableBalance = balance?.balanceAvailable;
 
     //TODO: Add support for receiver in URL
