ea43a25aaa95c5f7860bbea6b6711b78cb9b9e87
Daniel Wang <danielwpz@gmail.com>
2021-10-11 15:20:16 +0800

    ui

--- packages/frontend/src/components/wallet/NFTDetailModal.js
+++ packages/frontend/src/components/wallet/NFTDetailModal.js
@@ -13,13 +13,17 @@ const StyledContainer = styled.div`
     align-items: center;
     padding: 40px 0 30px 0;
 
-    h2 {
-        color: #72727A !important;
-        font-size: 16px !important;
-        font-weight: 400 !important;
-        line-height: 150%;
-        text-align: center;
-        margin: 20px 0 30px 0;
+    .container {
+        align-items: left;
+        width: 100% !important;
+        max-width: 400px;
+
+        img {
+            width: 100% !important;
+            margin-bottom: 83px;
+            max-width: 400px;
+            filter: drop-shadow(0px 100px 80px rgba(0, 0, 0, 0.07)) drop-shadow(0px 41.7776px 33.4221px rgba(0, 0, 0, 0.0503198)) drop-shadow(0px 22.3363px 17.869px rgba(0, 0, 0, 0.0417275)) drop-shadow(0px 12.5216px 10.0172px rgba(0, 0, 0, 0.035)) drop-shadow(0px 6.6501px 5.32008px rgba(0, 0, 0, 0.0282725)) drop-shadow(0px 2.76726px 2.21381px rgba(0, 0, 0, 0.0196802));
+        }
     }
 
     a {
@@ -43,55 +47,6 @@ const StyledContainer = styled.div`
         }
     } 
 
-    .title {
-        position: static;
-        width: 429px;
-        left: 0px;
-        top: 0px;
-        
-        /* heading/H1 */
-        
-        font-family: Inter;
-        font-style: normal;
-        font-weight: 900;
-        font-size: 31px;
-        line-height: 130%;
-        /* or 40px */
-        
-        display: flex;
-        align-items: center;
-        font-feature-settings: 'zero' on;
-        
-        /* gray/neutral/800 */
-        
-        color: #272729;
-    }
-    margin: 8px 0px;
-
-    .desc {
-        position: static;
-        width: 429px;
-        left: 0px;
-        top: 128px;
-
-        /* paragraph/body/default */
-
-        font-family: Inter;
-        font-style: normal;
-        font-weight: 500;
-        font-size: 16px;
-        line-height: 150%;
-        /* or 24px */
-
-        display: flex;
-        align-items: center;
-        font-feature-settings: 'zero' on;
-
-        /* gray/neutral/800 */
-
-        color: #272729;
-    }
-
     button {
         &.gray-blue {
             width: 100% !important;
@@ -111,34 +66,35 @@ export default function NFTDetailModal({ open, onClose, nft }) {
             id='nft-detail-modal'
             isOpen={open}
             onClose={onClose}
-            closeButton='false'
+            closeButton={false}
             modalSize='lg'
         >
             <StyledContainer className='small-centered'>
-                <FormButton
-                    color='link go-back'
-                    onClick={() => onClose()}
+                <div className='container'>
+                    <FormButton
+                        color='link go-back'
+                        onClick={() => onClose()}>
+                        <ArrowIcon />
+                    </FormButton>
+
+                    <img src={metadata.media} alt='NFT'/>
+                    <h1 className="title">{metadata.title}</h1>
+                    <p className="desc">{metadata.description}</p>
+                </div>
+
+                <FormButton 
+                    color='gray-blue' 
+                    onClick={() => setTransferNftDetail(nft)}
                 >
-                    <ArrowIcon />
+                    Transfer
                 </FormButton>
-
-            <h1 className="title">{metadata.title}</h1>
-            <p className="desc">{metadata.description}</p>
-            {console.log('modal open')}
-
-            <FormButton 
-                color='gray-blue' 
-                onClick={() => setTransferNftDetail(nft)}
-            >
-                Transfer
-            </FormButton>
-            {transferNftDetail &&
-                <NFTTransferModal
-                    open={!!transferNftDetail}
-                    onClose={() => setTransferNftDetail()}
-                    nft={transferNftDetail}>
-                </NFTTransferModal>
-            }
+                {transferNftDetail &&
+                    <NFTTransferModal
+                        open={!!transferNftDetail}
+                        onClose={() => setTransferNftDetail()}
+                        nft={transferNftDetail}>
+                    </NFTTransferModal>
+                }
 
             </StyledContainer>
         </Modal>
