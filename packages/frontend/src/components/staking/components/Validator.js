ec583c26c33744f1ee10b727bd6ab4009ac0f5bd
marcinbodnar <me@marcinbodnar.com>
2021-10-14 01:09:09 +0200

    Use createMatchSelector selector in Validator component (#1966)

--- packages/frontend/src/components/staking/components/Validator.js
+++ packages/frontend/src/components/staking/components/Validator.js
@@ -1,6 +1,7 @@
+import { createMatchSelector } from 'connected-react-router';
 import React, { useState } from 'react';
 import { Translate } from 'react-localize-redux';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Mixpanel } from '../../../mixpanel';
 import { redirectTo } from '../../../redux/actions/account';
@@ -22,7 +23,8 @@ export default function Validator({
 }) {
     const [confirm, setConfirm] = useState(null);
     const dispatch = useDispatch();
-    const stakeNotAllowed = !!selectedValidator && selectedValidator !== match.params.validator && !!currentValidators.length;
+    const validatorParam = useSelector(createMatchSelector('/staking/:validator'))?.params.validator;
+    const stakeNotAllowed = !!selectedValidator && selectedValidator !== validatorParam && !!currentValidators.length;
     const showConfirmModal = confirm === 'withdraw';
 
     const handleStakeAction = async () => {
@@ -42,9 +44,9 @@ export default function Validator({
                 />
                 : null
             }
-            <h1><SafeTranslate id='staking.validator.title' data={{ validator: match.params.validator }}/></h1>
+            <h1><SafeTranslate id='staking.validator.title' data={{ validator: validatorParam }}/></h1>
             <FormButton
-                linkTo={`/staking/${match.params.validator}/stake`} 
+                linkTo={`/staking/${validatorParam}/stake`} 
                 disabled={(stakeNotAllowed || !validator) ? true : false}
                 trackingId="STAKE Click stake with validator button"
             >
@@ -58,7 +60,7 @@ export default function Validator({
                         info='staking.balanceBox.staked.info'
                         amount={validator.staked || '0'}
                         onClick={() => {
-                            dispatch(redirectTo(`/staking/${match.params.validator}/unstake`));
+                            dispatch(redirectTo(`/staking/${validatorParam}/unstake`));
                             Mixpanel.track("UNSTAKE Click unstake button");
                         }}
                         button='staking.balanceBox.staked.button'
