18995e12af7ee88af1cbd71584445836b802d05a
Daniel Wang <danielwpz@gmail.com>
2021-10-14 16:40:32 +0800

    transfer ui

--- packages/frontend/src/components/wallet/NFTTransferModal.js
+++ packages/frontend/src/components/wallet/NFTTransferModal.js
@@ -20,10 +20,78 @@ const StyledContainer = styled.div`
         width: 100% !important;
         max-width: 386px;
         border-radius: 8px;
+        margin-bottom: 16px;
     }
 
-    h3, p {
+    .transfer-txt {
         margin-top: 16px;
+    }
+
+    .confirm-txt {
+        margin-bottom: 4px !important; 
+    }
+
+    .confirm-img {
+        width: 172px;
+    }
+
+    .confirm-nft-card {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        position: relative;
+
+        width: 385px;
+        margin-top: 16px;
+
+        background: #FFFFFF;
+        box-shadow: 0px 45px 56px rgba(0, 0, 0, 0.07), 0px 10.0513px 12.5083px rgba(0, 0, 0, 0.0417275), 0px 2.99255px 3.72406px rgba(0, 0, 0, 0.0282725);
+        border-radius: 8px;
+
+        .from-box {
+            width: 385px;
+            height: 74px;
+        }
+
+        .to-box {
+            width: 385px;
+            height: 53px;
+        }
+
+        .confirm-txt {
+            font-family: Inter;
+            font-style: normal;
+            font-weight: 500;
+            font-size: 14px;
+            line-height: 150%;
+            /* identical to box height, or 21px */
+            
+            font-feature-settings: 'zero' on;
+            
+            /* gray/neutral/600 */
+            
+            color: #72727A;
+
+            margin-left: 16px;
+        }
+
+        .confirm-id {
+            position: absolute;
+            right: 16px;
+            font-family: Inter;
+            font-style: normal;
+            font-weight: bold;
+            font-size: 14px;
+            line-height: 150%;
+            /* identical to box height, or 21px */
+
+            text-align: right;
+            font-feature-settings: 'zero' on;
+
+            /* gray/neutral/800 */
+
+            color: #272729;
+        }
     }
 
     h3 {
@@ -92,7 +160,7 @@ async function sendNFT (nft, receiverId, onSuccess) {
 
 export default function NFTTransferModal({ open, onClose, nft }) {
     const [ receiverId, setReceiverId ] = useState();
-    const [ success, setSuccess ] = useState(false);
+    const [ viewType, setViewType ] = useState('transfer');
     const [ accountIdIsValid, setAccountIdIsValid] = useState(false);
 
     const dispatch = useDispatch();
@@ -106,12 +174,12 @@ export default function NFTTransferModal({ open, onClose, nft }) {
             closeButton='false'
             modalSize='md'
         >
-            {!success &&
+            {viewType === 'transfer' &&
             <StyledContainer className='small-centered'>
                 <img src={nft.metadata.media} alt='NFT'/>
 
                 <h3>Transfer NFT</h3>
-                <p>Enter a recipient address, then proceed to confirm your transaction. </p>
+                <p className='transfer-txt'>Enter a recipient address, then proceed to confirm your transaction. </p>
 
                 <div className='receiver-input'>
                     <ReceiverInputWithLabel
@@ -136,15 +204,58 @@ export default function NFTTransferModal({ open, onClose, nft }) {
                     <FormButton
                         className='next-btn'
                         type='submit'
-                        disabled={!accountIdIsValid}
-                        onClick={() => sendNFT(nft, receiverId, () => setSuccess(true))}
+                        // disabled={!accountIdIsValid}
+                        // onClick={() => sendNFT(nft, receiverId, () => setSuccess(true))}
+                        onClick={() => setViewType('confirm')}
                     >
                         Next
                     </FormButton>
                 </div>
             </StyledContainer>
             }
-            {success &&
+
+            {viewType === 'confirm' &&
+                <StyledContainer className='small-centered'>
+                    <p className='confirm-txt'>Confirm Transaction:</p>
+                    <h3>Transfer NFT</h3>
+
+                    <div className='confirm-nft-card'>
+                        <div className='confirm-img'>
+                            <img src={nft.metadata.media} alt='NFT'/>
+                        </div>
+                        <div className='from-box'>
+                            <span className='confirm-txt'>From</span>
+                            <span className='confirm-id'>a.near</span>
+                        </div>
+                        <div className='to-box'>
+                            <span className='confirm-txt'>To</span>
+                            <span className='confirm-id'>a.near</span>
+                        </div>
+                    </div>
+
+                    <div className='buttons'>
+                        <FormButton
+                            className='link'
+                            type='button'
+                            onClick={onClose}
+                            color='gray'
+                        >
+                            Cancel 
+                        </FormButton>
+                        <FormButton
+                            className='next-btn'
+                            type='submit'
+                            // disabled={!accountIdIsValid}
+                            // onClick={() => sendNFT(nft, receiverId, () => setSuccess(true))}
+                            onClick={() => setViewType('success')}
+                        >
+                            Confirm 
+                        </FormButton>
+                    </div>
+                </StyledContainer>
+            }
+
+            {viewType === 'success' &&
                 <StyledContainer className='small-centered'>
                     Success
                 </StyledContainer>
