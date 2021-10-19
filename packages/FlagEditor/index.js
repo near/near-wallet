b689c652506a50811dd461d3022cad68db8f8561
Andy Haynes <andy@near.org>
2021-10-18 17:12:35 -0700

    export flag proxy

--- packages/FlagEditor/index.js
+++ packages/FlagEditor/index.js
@@ -1,11 +1,30 @@
-const FlagEditor = require('./FlagEditor');
-const prompts = require('./prompts');
 const { ENVIRONMENTS } = require('./constants');
 
-const editorInstance = new FlagEditor({ prompts, environments: ENVIRONMENTS })
+function initFeatureFlags(flagState, environment) {
+    if (typeof flagState !== 'object') {
+        throw Error('invalid flags');
+    }
 
-async function engage() {
-    await editorInstance.init();
+    if (!Object.values(ENVIRONMENTS).includes(environment)) {
+        throw Error(`invalid environment: "${environment}"`);
+    }
+
+    return new Proxy(flagState, {
+       get(flags, flag) {
+           const feature = flags[flag];
+           if (!feature) {
+               throw Error(`invalid feature: "${flag}"`);
+           }
+
+           if (!feature[environment]) {
+               throw Error(`${flag} missing definition for environment: "${environment}"`);
+           }
+
+           return feature[environment].enabled;
+       },
+    });
 }
 
-engage();
+module.exports = {
+    initFeatureFlags,
+};
