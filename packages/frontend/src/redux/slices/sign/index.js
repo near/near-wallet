d2b8bc98f007fe2e077db5ee74c2c334024d0e1f
marcinbodnar <me@marcinbodnar.com>
2021-10-26 00:46:30 +0200

    Add selectors for sign reducer (#2111)

--- packages/frontend/src/redux/slices/sign/index.js
+++ packages/frontend/src/redux/slices/sign/index.js
@@ -2,3 +2,18 @@ const SLICE_NAME = 'sign';
 
 // Top level selectors
 export const selectSignSlice = (state) => state[SLICE_NAME];
+
+export const selectSignTransactions = createSelector(
+    [selectSignSlice],
+    (sign) => sign.transactions || []
+);
+
+export const selectSignCallbackUrl = createSelector(
+    [selectSignSlice],
+    (sign) => sign.callbackUrl
+);
+
+export const selectSignMeta = createSelector(
+    [selectSignSlice],
+    (sign) => sign.meta
+);
