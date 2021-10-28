ece106bb4dacd67f158c4d629bcbc4c2aaca16bb
Andy Haynes <andy@near.org>
2021-10-28 10:44:37 -0700

    handle empty flag state

--- packages/feature-flags/prompts/action.js
+++ packages/feature-flags/prompts/action.js
@@ -1,13 +1,13 @@
 const inquirer = require('inquirer');
 const { ACTIONS } = require('../constants');
 
-module.exports = async function actionPrompt() {
+module.exports = async function actionPrompt(hasFlags) {
     const { action } = await inquirer.prompt({
         name: 'action',
         type: 'list',
         message: 'What do you want to do?',
-        choices: [ACTIONS.ADD_FLAG, ACTIONS.EDIT_FLAG, ACTIONS.REMOVE_FLAG],
-        default: ACTIONS.EDIT_FLAG,
+        choices: hasFlags ? [ACTIONS.ADD_FLAG, ACTIONS.EDIT_FLAG, ACTIONS.REMOVE_FLAG] : [ACTIONS.ADD_FLAG],
+        default: hasFlags ? ACTIONS.EDIT_FLAG : ACTIONS.ADD_FLAG,
     });
 
     return action;
