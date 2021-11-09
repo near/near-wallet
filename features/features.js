9fbf253363f7d0a53e44062cfac2d0d3b3e6babe
Daryl Collins <daryl@darylcollins.com>
2021-11-08 22:13:03 -0800

    Move to named export so that .d.ts type inferrance works correctly

--- features/features.js
+++ features/features.js
@@ -2,8 +2,10 @@ const { initFeatureFlags } = require('@near-wallet/feature-flags');
 const Environments = require('./environments.json');
 const FeatureFlags = require('./flags.json');
 
-module.exports = initFeatureFlags({
+const Features = initFeatureFlags({
     flagState: FeatureFlags,
     currentEnvironment: process.env.NEAR_WALLET_ENV,
     environments: Environments,
 });
+
+module.exports = { Features };
