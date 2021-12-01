2d7e5eb065694d1c5d0d0be8cfd0f0e976d34d2e
marcinbodnar <me@marcinbodnar.com>
2021-12-01 02:33:48 +0100

    Add necessary selectors (#2278)

--- packages/frontend/src/redux/slices/staking/index.js
+++ packages/frontend/src/redux/slices/staking/index.js
@@ -1,4 +1,84 @@
+import { createSelector } from "reselect";
+
+import createParameterSelector from "../createParameterSelector";
+
 const SLICE_NAME = 'staking';
 
+const getAccountIdParam = createParameterSelector((params) => params.accountId);
+const getValidatorIdParam = createParameterSelector((params) => params.validatorId);
+
 // Top level selectors
-export const selectStakingSlice = (state) => state[SLICE_NAME];
+export const selectStakingSlice = (state) => state[SLICE_NAME] || {};
+
+export const selectStakingAccounts = createSelector([selectStakingSlice], (staking) => staking.accounts || []);
+
+export const selectStakingCurrentAccountbyAccountId = createSelector(
+    [selectStakingAccounts, getAccountIdParam], 
+    (accounts, accountId) => accounts.find((account) => account.accountId === accountId)
+);
+
+export const selectStakingAccountsFirst = createSelector(
+    selectStakingAccounts,
+    (accounts) => accounts[0] || {}
+);
+
+export const selectStakingAccountsSecond = createSelector(
+    selectStakingAccounts,
+    (accounts) => accounts[1] || {}
+);
+
+export const selectStakingLockup = createSelector(
+    selectStakingSlice,
+    (staking) => staking.lockup || {}
+);
+
+export const selectStakingLockupId = createSelector(
+    selectStakingLockup,
+    (lockup) => lockup.lockupId || ''
+);
+
+export const selectStakingContract = createSelector(
+    selectStakingLockup,
+    (lockup) => lockup.contract || {}
+);
+
+export const selectStakingAllValidators = createSelector(
+    selectStakingSlice,
+    (staking) => staking.allValidators || []
+);
+
+export const selectStakingAllValidatorsLength = createSelector(
+    selectStakingAllValidators,
+    (allValidators) => allValidators.length
+);
+
+export const selectStakingFindContractByValidatorId = createSelector(
+    [selectStakingAllValidators, getValidatorIdParam],
+    (allValidators, validatorId) => allValidators.find((validator) => validator.accountId === validatorId)?.contract || {}
+);
+
+export const selectStakingAccountsObj = createSelector(
+    selectStakingSlice,
+    (staking) => staking.accountsObj || {}
+);
+
+export const selectStakingAccountsObjAccountId = createSelector(
+    selectStakingAccountsObj,
+    (accountsObj) => accountsObj.accountId || ''
+);
+
+export const selectStakingAccountsObjLockupId = createSelector(
+    selectStakingAccountsObj,
+    (accountsObj) => accountsObj.lockupId || ''
+);
+
+export const selectStakingCurrentAccount = createSelector(
+    selectStakingSlice,
+    (staking) => staking.currentAccount || {}
+);
+
+export const selectStakingCurrentAccountAccountId = createSelector(
+    selectStakingCurrentAccount,
+    (currentAccount) => currentAccount.accountId || ''
+);
+
