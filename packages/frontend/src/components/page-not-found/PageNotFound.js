c5f337131a32ed350657cf7a0f013f6e9c9de3ae
worldclassdev <philip.obosi@eng.uniben.edu>
2021-12-03 10:46:58 +0100

    cleanup: code review

--- packages/frontend/src/components/page-not-found/PageNotFound.js
+++ packages/frontend/src/components/page-not-found/PageNotFound.js
@@ -6,27 +6,20 @@ import textBackgroundImage  from "../../images/bg-landing-patterned.svg";
 import FormButton from "../common/FormButton";
 import Container from "../common/styled/Container.css";
 
-
-
-const StyledContainer = styled.div`
+const StyledContainer = styled(Container)`
   &&& {
-    margin: 35px 5px 0 5px;
-    position: relative;
     text-align: center;
+    padding-top: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 
     @media (max-width: 767px) {
-      margin: 0;
-      padding: 0 20px;
+      margin-top: 35px;
+      padding: 35px 20px;
       overflow: hidden;
       margin-top: -13px;
-    }
-
-    .small-centered {
-      padding-top: 20px;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
     }
 
     h1.title {
@@ -34,7 +27,6 @@ const StyledContainer = styled.div`
     }
 
     h1.displayTitle{
-        position: relative;
         font-size: 20vw;
         line-height: 1.2;
         background: url(${textBackgroundImage});
@@ -44,40 +36,17 @@ const StyledContainer = styled.div`
         color: rgba(0,0,0,0.08);
 
         @media (max-width: 767px) {
-            font-size: 45vw;
+            font-size: 40vw;
+        }
+    
+        @media (min-width: 1200px) {
+            font-size: 240px;
         }
     }
 
-    .buttons {
-      margin-top: 16px;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      z-index: 1;
-
-      .blue {
-        font-weight: 500;
+    button {
         margin: 0;
-        text-transform: none;
-
-        :not(.link) {
-          min-width: 200px;
-          max-width: 220px;
-          height: auto;
-          text-transform: none;
-          padding: 12px 6px;
-        }
-      }
-      }
-
-      span {
-        margin: 20px;
-      }
-
-      @media (min-width: 768px) {
-        flex-direction: row;
-      }
+        min-width: 200px;
     }
   }
 `;
@@ -85,19 +54,15 @@ const StyledContainer = styled.div`
 export function PageNotFound() {
   return (
     <StyledContainer>
-      <Container className="small-centered">
         <h1 className="title">
-          <Translate id="pageNotFound.title" />
+            <Translate id="pageNotFound.title" />
         </h1>
         <h1 className="displayTitle">
             <Translate id="pageNotFound.displayTitle" />
         </h1>
-        <div className="buttons">
-          <FormButton linkTo="/">
+        <FormButton linkTo="/">
             <Translate id="pageNotFound.returnToWallet" />
-          </FormButton>
-        </div>
-      </Container>
+        </FormButton>
     </StyledContainer>
   );
 }
