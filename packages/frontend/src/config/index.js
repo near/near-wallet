c12a75c83b6b800069142f892e569937214fa309
esaminu <esaminu1@icloud.com>
2021-12-09 21:27:39 +0400

    ADD defaults & check for valid NEAR_WALLET_ENV

---
+++ packages/frontend/src/config/index.js
@@ -0,0 +1,25 @@
+import assert from "assert";
+
+import Environments from "../../../../features/environments.json";
+import development from "./environmentDefaults/development";
+import testnet from "./environmentDefaults/testnet";
+import testnet_STAGING from "./environmentDefaults/testnet_STAGING";
+import mainnet from "./environmentDefaults/mainnet";
+import mainnet_STAGING from "./environmentDefaults/mainnet_STAGING";
+
+const NEAR_WALLET_ENV = process.env.NEAR_WALLET_ENV;
+
+assert(
+    Object.values(Environments).some((env) => NEAR_WALLET_ENV === env),
+    `Invalid environment: "${NEAR_WALLET_ENV}"`
+);
+
+const defaults = {
+    [Environments.DEVELOPMENT]: development,
+    [Environments.TESTNET]: testnet,
+    [Environments.TESTNET_STAGING]: testnet_STAGING,
+    [Environments.MAINNET]: mainnet,
+    [Environments.MAINNET_STAGING]: mainnet_STAGING,
+};
+
+module.exports = defaults[NEAR_WALLET_ENV];
