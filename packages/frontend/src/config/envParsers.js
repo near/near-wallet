36b3b07cde333a6e5c37d7f2c598562fc387cbe1
esaminu <esaminu1@icloud.com>
2021-12-16 18:41:35 +0400

    REFACTOR envParser to be more succinct

--- packages/frontend/src/config/envParsers.js
+++ packages/frontend/src/config/envParsers.js
@@ -4,15 +4,11 @@ const parseBooleanFromShell = (envVal) =>
     envValIsSet(envVal) ? envVal === "yes" || envVal === "true" : undefined;
 
 const parseObjectFromShell = (envVal) => {
-    if (envValIsSet(envVal)) {
-        try {
-            let parsed = JSON.parse(envVal);
-            return parsed;
-        } catch (error) {
-            return undefined;
-        }
+    try {
+        return JSON.parse(envVal);
+    } catch (error) {
+        return undefined;
     }
-    return undefined;
 };
 
 const parseCommaSeperatedStringAsArrayFromShell = (envVal) =>
