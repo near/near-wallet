bf52fb5ba3399e73ba7acac63a39d1b8a62739a8
Patrick Tajima <patrick.tajima@gmail.com>
2022-01-11 16:37:13 -0800

    Fix iOS appearance style bug

--- packages/frontend/src/components/common/BackArrowButton.js
+++ packages/frontend/src/components/common/BackArrowButton.js
@@ -4,21 +4,26 @@ import styled from 'styled-components';
 import ArrowIcon from '../svg/ArrowIcon';
 
 const StyledContainer = styled.div`
-    transition: 100ms;
-    width: 40px;
-    height: 40px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 50%;
-    cursor: pointer;
+    &&& {
+        appearance: none;
+        transition: 100ms;
+        width: 40px;
+        height: 40px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 50%;
+        cursor: pointer;
 
-    :hover {
-        background-color: #eeefee;
+        :hover {
+            background-color: #eeefee;
 
-        svg {
-            path {
-                stroke: #0072CE;
+            svg {
+                min-width: 24px;
+                min-height: 24px;
+                path {
+                    stroke: #0072CE;
+                }
             }
         }
     }
