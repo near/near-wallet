323dd67fbc0a0a1fffcd721ed9bbc0a292b8d165
marcinbodnar <me@marcinbodnar.com>
2022-01-13 00:09:10 +0100

    feat: add selectActionsPending selector

--- packages/frontend/src/redux/slices/status/index.js
+++ packages/frontend/src/redux/slices/status/index.js
@@ -1,6 +1,10 @@
 import { createSelector } from "reselect";
 
+import createParameterSelector from '../createParameterSelector';
+
 const SLICE_NAME = 'status';
+
+const getTypes = createParameterSelector((params) => params.types);
 
 // Top level selectors
 export const selectStatusSlice = (state) => state[SLICE_NAME];
@@ -10,3 +14,9 @@ export const selectStatusMainLoader = createSelector(selectStatusSlice, (status)
 export const selectStatusLocalAlert = createSelector(selectStatusSlice, (status) => status.localAlert || {});
 
 export const selectStatusActionStatus = createSelector(selectStatusSlice, (status) => status.actionStatus || []);
+
+export const selectActionsPending = createSelector(
+    [getTypes, selectStatusActionStatus],
+    (types, actionStatus) => (typeof types === 'string' ? [types] : types).some((type) => actionStatus[type]?.pending)
+);
+
