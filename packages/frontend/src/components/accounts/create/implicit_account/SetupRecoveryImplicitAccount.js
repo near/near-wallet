9a5a6c730f3a57d4f7ffab942de367a0a8186e6c
Patrick Tajima <patrick.tajima@gmail.com>
2022-01-13 15:47:11 -0800

    ADD email option when creating implicit account

--- packages/frontend/src/components/accounts/create/implicit_account/SetupRecoveryImplicitAccount.js
+++ packages/frontend/src/components/accounts/create/implicit_account/SetupRecoveryImplicitAccount.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Translate } from 'react-localize-redux';
 import styled from 'styled-components';
 
+import { validateEmail } from '../../../../utils/account';
 import RecoveryOption from '../../../accounts/recovery_setup/RecoveryOption';
 import FormButton from '../../../common/FormButton';
 import Container from '../../../common/styled/Container.css';
@@ -25,13 +26,33 @@ const StyledContainer = styled(Container)`
 `;
 
 export default ({
-    onClickSecureMyAccount
+    onClickSecureMyAccount,
+    email,
+    setEmail
 }) => {
     const [recoveryOption, setRecoveryOption] = useState('phrase');
+    const [emailIsInvalid, setEmailisInvalid] = useState(false);
+
+    const isValidInput = () => {
+        switch (recoveryOption) {
+            case 'email':
+                return validateEmail(email);
+            case 'phrase':
+                return true;
+            case 'ledger':
+                return true;
+            default:
+                return false;
+        }
+    };
+
     return (
         <StyledContainer className='small-centered border'>
             <form onSubmit={e => {
-                onClickSecureMyAccount({ recoveryOption });
+                onClickSecureMyAccount({
+                    recoveryOption,
+                    email
+                });
                 e.preventDefault();
             }}>
                 <h1><Translate id='setupRecovery.header' /></h1>
@@ -50,9 +71,36 @@ export default ({
                     option='ledger'
                     active={recoveryOption}
                 />
+                <h4>
+                    <Translate id='setupRecovery.basicSecurity' />
+                    <Tooltip translate='profile.security.lessSecureDesc' icon='icon-lg' />
+                </h4>
+                <RecoveryOption
+                    onClick={() => setRecoveryOption('email')}
+                    option='email'
+                    active={recoveryOption}
+                    problem={recoveryOption === 'email' && emailIsInvalid}
+                >
+                    <Translate>
+                        {({ translate }) => (
+                            <input
+                                type='email'
+                                placeholder={translate('setupRecovery.emailPlaceholder')}
+                                value={email}
+                                onChange={e => {
+                                    setEmail(e.target.value);
+                                    setEmailisInvalid(false);
+                                }}
+                                onBlur={() => setEmailisInvalid(email !== '' && !isValidInput())}
+                                tabIndex='1'
+                            />
+                        )}
+                    </Translate>
+                </RecoveryOption>
                 <FormButton
                     color='blue'
                     type='submit'
+                    disabled={!isValidInput()}
                     trackingId='SR Click submit button'
                     data-test-id="submitSelectedRecoveryOption"
                 >
--- packages/frontend/src/utils/wallet.js
+++ packages/frontend/src/utils/wallet.js
@@ -780,7 +780,7 @@ class Wallet {
         if (!accountId) {
             return false;
         }
-        
+
         const account = await this.getAccount(accountId);
         return await account.getAccountBalance(limitedAccountData);
     }
@@ -816,6 +816,20 @@ class Wallet {
         });
     }
 
+    async initializeRecoveryMethodNewImplicitAccount(method) {
+        const { seedPhrase } = generateSeedPhrase();
+        const { secretKey } = parseSeedPhrase(seedPhrase);
+        const recoveryKeyPair = KeyPair.fromString(secretKey);
+        const implicitAccountId = Buffer.from(recoveryKeyPair.publicKey.data).toString('hex');
+        const body = {
+            accountId: implicitAccountId,
+            method,
+            seedPhrase
+        };
+        await sendJson('POST', ACCOUNT_HELPER_URL + '/account/initializeRecoveryMethodForTempAccount', body);
+        return seedPhrase;
+    }
+
     async initializeRecoveryMethod(accountId, method) {
         const { seedPhrase } = generateSeedPhrase();
         const isNew = await this.checkIsNew(accountId);
@@ -830,6 +844,19 @@ class Wallet {
             await this.postSignedJson('/account/initializeRecoveryMethod', body);
         }
         return seedPhrase;
+    }
+
+    async validateSecurityCodeNewImplicitAccount(implicitAccountId, method, securityCode) {
+        try {
+            const response = await sendJson('POST', ACCOUNT_HELPER_URL + '/account/validateSecurityCodeForTempAccount', {
+                accountId: implicitAccountId,
+                method,
+                securityCode,
+            });
+            console.log('validateSecurityCodeNewImplicitAccount', response);
+        } catch(e) {
+            throw new WalletError('Invalid code', 'setupRecoveryMessageNewAccount.invalidCode');
+        }
     }
 
     async validateSecurityCode(accountId, method, securityCode, enterpriseRecaptchaToken, recaptchaAction) {
