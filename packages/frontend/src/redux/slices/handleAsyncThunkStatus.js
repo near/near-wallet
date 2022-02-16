23a94712eff791108eb178881e775157022a7c92
marcinbodnar <me@marcinbodnar.com>
2022-02-16 23:20:38 +0100

    refactor: refactor handleAsyncThunkStatus

--- packages/frontend/src/redux/slices/handleAsyncThunkStatus.js
+++ packages/frontend/src/redux/slices/handleAsyncThunkStatus.js
@@ -1,6 +1,18 @@
+import { isFulfilled, isPending, isRejected } from '@reduxjs/toolkit';
 import set from 'lodash.set';
 
 import initialErrorState from './initialErrorState';
+
+const fullPath = (action, buildStatusPath) => [...buildStatusPath(action), 'status'];
+
+const setLoading = (buildStatusPath, loading) => (state, action) => set(state, [...fullPath(action, buildStatusPath), 'loading'], loading);
+
+const clearError = (buildStatusPath) => (state, action) =>  set(state, [...fullPath(action, buildStatusPath), 'error'], initialErrorState);
+
+const setError = (buildStatusPath) => (state, action) =>  set(state, [...fullPath(action, buildStatusPath), 'error'], {
+    message: action.error?.message || 'An error was encountered.',
+    code: action.error?.code
+});
 
 /**
  * Automatically handle status part of reducer based on initialErrorState
@@ -9,34 +21,16 @@ import initialErrorState from './initialErrorState';
  * @param buildStatusPath array of strings representing the path to status state, needed because in a few cases we want to place the status object in more than one place
  * @param builder objct provides addCase, addMatcher, addDefaultCase functions
  */
+
+// TODO: consider to keying `status` by thunk typePrefix, this could be useful if we would need to track status of several thunks independently in one slice
 export default ({
     asyncThunk,
     buildStatusPath,
     builder
 }) => builder
-    // TODO: use the same status object to all reducers, which will allow simplifying buildStatusPath
-    // TODO: consider to keying `status` by thunk typePrefix, this could be useful if we would need to track status of several thunks independently in one slice
-    .addMatcher(
-        (action) => action.type === `${asyncThunk.typePrefix}/pending`,
-        (state, action) => {
-            set(state, [...buildStatusPath(action), 'status', 'loading'], true);
-            set(state, [...buildStatusPath(action), 'status', 'error'], initialErrorState);
-        }
-    )
-    .addMatcher(
-        (action) => action.type === `${asyncThunk.typePrefix}/fulfilled`,
-        (state, action) => {
-            set(state, [...buildStatusPath(action), 'status', 'loading'], false);
-            set(state, [...buildStatusPath(action), 'status', 'error'], initialErrorState);
-        }
-    )
-    .addMatcher(
-        (action) => action.type === `${asyncThunk.typePrefix}/rejected`,
-        (state, action) => {
-            set(state, [...buildStatusPath(action), 'status', 'loading'], false);
-            set(state, [...buildStatusPath(action), 'status', 'error'], {
-                message: action.error?.message || 'An error was encountered.',
-                code: action.error?.code
-            });
-        }
-    );
+    .addMatcher(isPending(asyncThunk), setLoading(buildStatusPath, true))
+    .addMatcher(isPending(asyncThunk), clearError(buildStatusPath))
+    .addMatcher(isFulfilled(asyncThunk), setLoading(buildStatusPath, false))
+    .addMatcher(isFulfilled(asyncThunk), clearError(buildStatusPath))
+    .addMatcher(isRejected(asyncThunk), setLoading(buildStatusPath, false))
+    .addMatcher(isRejected(asyncThunk), setError(buildStatusPath));
