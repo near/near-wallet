416c39d1b6b33c8bbdd50a159928810a6ec7a5b6
esaminu <esaminu1@icloud.com>
2022-02-25 04:42:57 +0400

    fix: restore project validator naming

--- packages/frontend/src/utils/constants.js
+++ packages/frontend/src/utils/constants.js
@@ -1,31 +1,31 @@
 export const VALIDATOR_VERSION = 'VALIDATOR';
-export const FARMING_VALIDATOR_VERSION = 'FARMING_VALIDATOR';
+export const PROJECT_VALIDATOR_VERSION = 'PROJECT_VALIDATOR';
 
 export const ValidatorVersion = {
   [VALIDATOR_VERSION]: VALIDATOR_VERSION,
-  [FARMING_VALIDATOR_VERSION]: FARMING_VALIDATOR_VERSION
+  [PROJECT_VALIDATOR_VERSION]: PROJECT_VALIDATOR_VERSION
 };
 
 export const MAINNET = 'mainnet';
 export const TESTNET = 'testnet';
-export const FARMING_VALIDATOR_PREFIXES_MAINNET = ['.pool.near'];
+export const PROJECT_VALIDATOR_PREFIXES_MAINNET = ['.pool.near'];
 export const VALIDATOR_PREFIXES_MAINNET = ['.poolv1.near'];
-export const FARMING_VALIDATOR_PREFIXES_TESTNET = ['.factory01.littlefarm.testnet','.factory.colorpalette.testnet'];
+export const PROJECT_VALIDATOR_PREFIXES_TESTNET = ['.factory01.littlefarm.testnet','.factory.colorpalette.testnet'];
 export const VALIDATOR_PREFIXES_TESTNET = ['.m0'];
 
-export const FARMING_VALIDATOR_REGEXP_TESTNET = new RegExp(`.*(${FARMING_VALIDATOR_PREFIXES_TESTNET.join('|')}|${VALIDATOR_PREFIXES_TESTNET.join('|')})`);
-export const FARMING_VALIDATOR_REGEXP_MAINNET = new RegExp(`.*(${FARMING_VALIDATOR_PREFIXES_MAINNET.join('|')}|${VALIDATOR_PREFIXES_MAINNET.join('|')})`);
+export const PROJECT_VALIDATOR_REGEXP_TESTNET = new RegExp(`.*(${PROJECT_VALIDATOR_PREFIXES_TESTNET.join('|')}|${VALIDATOR_PREFIXES_TESTNET.join('|')})`);
+export const PROJECT_VALIDATOR_REGEXP_MAINNET = new RegExp(`.*(${PROJECT_VALIDATOR_PREFIXES_MAINNET.join('|')}|${VALIDATOR_PREFIXES_MAINNET.join('|')})`);
 
 export const getFarmingValidatorPrefixes = (networkId) => {
   switch (networkId) {
     case (MAINNET): {
-      return FARMING_VALIDATOR_PREFIXES_MAINNET;
+      return PROJECT_VALIDATOR_PREFIXES_MAINNET;
     }
     case (TESTNET): {
-      return FARMING_VALIDATOR_PREFIXES_TESTNET;
+      return PROJECT_VALIDATOR_PREFIXES_TESTNET;
     }
     default: {
-      return FARMING_VALIDATOR_PREFIXES_TESTNET;
+      return PROJECT_VALIDATOR_PREFIXES_TESTNET;
     }
   }
 };
@@ -33,32 +33,32 @@ export const getFarmingValidatorPrefixes = (networkId) => {
 export const getValidatorRegExp = (networkId) => {
   switch (networkId) {
     case (MAINNET): {
-      return FARMING_VALIDATOR_REGEXP_MAINNET;
+      return PROJECT_VALIDATOR_REGEXP_MAINNET;
     }
     case (TESTNET): {
-      return FARMING_VALIDATOR_REGEXP_TESTNET;
+      return PROJECT_VALIDATOR_REGEXP_TESTNET;
     }
     default: {
-      return FARMING_VALIDATOR_REGEXP_TESTNET;
+      return PROJECT_VALIDATOR_REGEXP_TESTNET;
     }
   }
 };
 
 export const getValidationVersion = (networkId, accountId) => {
   const prefixes = getValidationNetworkPrefixes(networkId);
-  return prefixes.some((prefix) => accountId.indexOf(prefix) !== -1) ? FARMING_VALIDATOR_VERSION : VALIDATOR_VERSION;
+  return prefixes.some((prefix) => accountId.indexOf(prefix) !== -1) ? PROJECT_VALIDATOR_VERSION : VALIDATOR_VERSION;
 };
 
 export const getValidationNetworkPrefixes = (networkId) => {
   switch (networkId) {
     case (MAINNET): {
-      return FARMING_VALIDATOR_PREFIXES_MAINNET;
+      return PROJECT_VALIDATOR_PREFIXES_MAINNET;
     }
     case (TESTNET): {
-      return FARMING_VALIDATOR_PREFIXES_TESTNET;
+      return PROJECT_VALIDATOR_PREFIXES_TESTNET;
     }
     default: {
-      return FARMING_VALIDATOR_PREFIXES_TESTNET;
+      return PROJECT_VALIDATOR_PREFIXES_TESTNET;
     }
   }
 };
