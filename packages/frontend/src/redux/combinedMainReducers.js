e849a18bf02c44f9b60a630e093939a7013dfca0
marcinbodnar <me@marcinbodnar.com>
2022-03-28 15:13:57 +0200

    refactor: combined main reducers

---
+++ packages/frontend/src/redux/combinedMainReducers.js
@@ -0,0 +1,36 @@
+import { connectRouter } from 'connected-react-router';
+import { localizeReducer } from 'react-localize-redux';
+
+import account from './reducers/account';
+import allAccounts from './reducers/allAccounts';
+import sign from './reducers/sign';
+import staking from './reducers/staking';
+import status from './reducers/status';
+import availableAccountsSlice from './slices/availableAccounts';
+import createFromImplicitSlice from './slices/createFromImplicit';
+import flowLimitationSlice from './slices/flowLimitation';
+import ledgerSlice from './slices/ledger';
+import linkdropSlice from './slices/linkdrop';
+import nftSlice from './slices/nft';
+import recoveryMethodsSlice from './slices/recoveryMethods';
+import tokenFiatValuesSlice from './slices/tokenFiatValues';
+import transactionsSlice from './slices/transactions';
+
+export default (history) => ({
+    localize: localizeReducer,
+    allAccounts,
+    account,
+    sign,
+    staking,
+    status,
+    [nftSlice.name]: nftSlice.reducer,
+    [tokenFiatValuesSlice.name]: tokenFiatValuesSlice.reducer,
+    [linkdropSlice.name]: linkdropSlice.reducer,
+    [transactionsSlice.name]: transactionsSlice.reducer,
+    [flowLimitationSlice.name]: flowLimitationSlice.reducer,
+    [createFromImplicitSlice.name]: createFromImplicitSlice.reducer,
+    [recoveryMethodsSlice.name]: recoveryMethodsSlice.reducer,
+    [availableAccountsSlice.name]: availableAccountsSlice.reducer,
+    [ledgerSlice.name]: ledgerSlice.reducer,
+    router: connectRouter(history)
+});
