ec8afc164f18ea53e3ec1a4a281d54e180fe85b9
marcinbodnar <me@marcinbodnar.com>
2022-04-14 16:20:53 +0200

    refactor: create Authorize component

---
+++ packages/frontend/src/components/accounts/ledger/SignInLedgerViews/Authorize.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Translate } from 'react-localize-redux';
+
+import { Mixpanel } from '../../../../mixpanel/index';
+import FormButton from '../../../common/FormButton';
+import LocalAlertBox from '../../../common/LocalAlertBox';
+import LedgerImageCircle from '../../../svg/LedgerImageCircle';
+import LedgerHdPaths from '../LedgerHdPaths';
+
+const Authorize = ({
+    status,
+    path,
+    setPath,
+    setConfirmedPath,
+    handleSignIn,
+    signingIn,
+    props
+}) => {
+    return (
+        <>
+            <LedgerImageCircle />
+            <h1><Translate id='signInLedger.header' /></h1>
+            <Translate id='signInLedger.one' />
+            <br /><br />
+            <LocalAlertBox localAlert={status.localAlert} />
+            <LedgerHdPaths
+                path={path}
+                onSetPath={(path) => setPath(path)}
+                onConfirmHdPath={() => {
+                    setConfirmedPath(path);
+                    Mixpanel.track('IE-Ledger Sign in set custom HD path');
+                }}
+            />
+            <div className='buttons-bottom-buttons'>
+                <FormButton
+                    onClick={handleSignIn}
+                    sending={signingIn}
+                    sendingString='button.signingIn'
+                >
+                    <Translate id='button.authorize' />
+                </FormButton>
+                <FormButton
+                    className='gray link'
+                    onClick={() => props.history.goBack()}
+                    trackingId='IE-Ledger Click cancel button'
+                >
+                    <Translate id='button.cancel' />
+                </FormButton>
+            </div>
+        </>
+    );
+};
+
+export default Authorize;
