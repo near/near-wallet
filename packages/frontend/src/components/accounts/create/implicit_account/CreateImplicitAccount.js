94b3fcf7793e4183b70172c73295c4c6fd7b3cfa
samkrew <samkrew@0hub.com>
2022-04-15 10:28:40 +0800

    fix: Remove direct top-up buttons.

--- packages/frontend/src/components/accounts/create/implicit_account/CreateImplicitAccount.js
+++ packages/frontend/src/components/accounts/create/implicit_account/CreateImplicitAccount.js
@@ -54,40 +54,6 @@ const StyledContainer = styled(Container)`
     }
 `;
 
-const StyledBuyButton = styled(FormButton)`
-    &&&& {
-        border-radius: 16px;
-        flex: 1;
-        margin-right: 8px;
-
-        :last-child {
-            margin-right: 0;
-        }
-    }
-`;
-
-const BuyButton = ({
-    amountUSD,
-    onClickBuyButton
-}) => {
-
-    const getBuyButtonLabel = (amountUSD) => {
-        if (amountUSD) {
-            return `$${amountUSD}`;
-        }
-        return (<Translate id='account.createImplicitAccount.customAmount' />);
-    };
-
-    return (
-        <StyledBuyButton
-            color='light-blue'
-            onClick={() => onClickBuyButton(amountUSD)}
-        >
-            {getBuyButtonLabel(amountUSD)}
-        </StyledBuyButton>
-    );
-};
-
 export default ({
     implicitAccountId,
     formattedMinDeposit,
@@ -99,27 +65,6 @@ export default ({
         <>
             <StyledContainer className='border small-centered'>
                 <h3><Translate id='account.createImplicitAccount.title' /></h3>
-                {<div className='flex-center-center'>
-                    {['30', '50', '100', ''].map((amount) => (
-                        <BuyButton
-                            key={`${amount}`}
-                            amountUSD={amount}
-                            onClickBuyButton={() => {
-                                window.open(
-                                    buildUtorgPayLink(implicitAccountId, undefined, amount, 'USD'),
-                                    '_blank'
-                                );
-                            }}
-                        />
-                    ))}
-                </div>}
-                {/*
-                TEMPORARILY DISABLE MOONPAY PRE-SPECIFIED AMOUNT BUTTONS,
-                WHILE MOONPAY 'baseCurrencyAmount' IS BROKEN
-                ISSUE: https://github.com/near/near-wallet/issues/2408
-
-                ENABLED UTORG
-                */}
                 <FormButton
                     linkTo={buildUtorgPayLink(implicitAccountId)}
                     color='black'
