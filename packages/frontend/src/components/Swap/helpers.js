70208d3ae557a22524b970e19bb6b94769f6a338
Andy Haynes <andy@near.org>
2022-04-20 17:41:31 -0700

    style(swap): remove unreferenced code

--- packages/frontend/src/components/Swap/helpers.js
+++ packages/frontend/src/components/Swap/helpers.js
@@ -9,28 +9,10 @@ export const currentToken = (tokens, value) => {
     return tokens.find((el) => el.onChainFTMetadata.symbol === value);
 };
 
-export const swapTokens = (tokens, from, setFrom, setTo, to) => {
-    if (to?.balance === '0' || !to?.balance) return;
-
-    if (from?.onChainFTMetadata?.symbol === 'NEAR') {
-        setFrom(currentToken(tokens, 'USN'));
-        setTo(tokens[0]);
-    } else {
-        setFrom(tokens[0]);
-        setTo(currentToken(tokens, 'USN'));
-    }
-};
-
 export const exchangeRateTranslation = ({ token, balance, exchangeRate }) => {
     return token?.onChainFTMetadata?.symbol === 'NEAR'
         ? balance / exchangeRate
         : balance * exchangeRate;
-};
-
-export const tradingFree = ({ token, balance, exchangeRate }) => {
-    return token === 'NEAR'
-        ? (balance / exchangeRate) * 0.002
-        : balance * exchangeRate * 0.002;
 };
 
 export const MinimumReceived = ({ token, balance, exchangeRate }) => {
