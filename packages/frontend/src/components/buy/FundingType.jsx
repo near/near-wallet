e70f3d7fca35a0e211869cf4e72dbc66c924b64b
AnyMac Store <andrii.s@4irelabs.com>
2022-04-25 11:52:54 +0300

    Top up redesign

---
+++ packages/frontend/src/components/buy/FundingType.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import arrow from './assets/arrow.svg';
+const LinkWrap = styled.a`
+    display: block;
+    border-top: 1px solid #F0F0F1;
+
+    &:hover{
+        background: #f9f9f9;
+    }
+    
+    @media (max-width: 992px) {
+        :first-child{
+            border: 0;
+        }
+    }
+    @media (max-width: 580px) {
+        :first-child{
+            border-top: 1px solid #F0F0F1;
+        }
+    }
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 68px;
+    padding: 0 32px;
+    @media (max-width: 580px) {
+        padding: 0 12px;
+    }
+`;
+
+const Button = styled.div`
+    background-image: url(${arrow});
+    width: 24px;
+    height: 24px;
+`;
+
+const WrapperImg = styled.div`
+    display: flex;
+`;
+
+export const FundingType = ({ icon, link, name, track, disabled }) => {
+    return !disabled && <LinkWrap href={link} target='_blank' rel='noreferrer'  onClick={(e) => {
+        if (track) {
+            track();
+        }
+    }}>
+        <Wrapper>
+            <WrapperImg title={name}>
+                <img src={icon} alt="" />
+            </WrapperImg>
+            <Button />
+        </Wrapper>
+    </LinkWrap>;
+};
