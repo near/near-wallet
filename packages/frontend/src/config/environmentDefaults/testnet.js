10e67065465733d10dcb1832a101c2dc0be9f582
esaminu <esaminu1@icloud.com>
2022-04-29 23:24:21 +0400

    chore: update LOCKUP_ACCOUNT_ID_SUFFIX defaults

--- packages/frontend/src/config/environmentDefaults/testnet.js
+++ packages/frontend/src/config/environmentDefaults/testnet.js
@@ -17,7 +17,7 @@ export default {
     EXPLORER_URL: 'https://explorer.testnet.near.org',
     HIDE_SIGN_IN_WITH_LEDGER_ENTER_ACCOUNT_ID_MODAL: false,
     LINKDROP_GAS: '100000000000000',
-    LOCKUP_ACCOUNT_ID_SUFFIX: 'lockup.m0',
+    LOCKUP_ACCOUNT_ID_SUFFIX: 'lockup.devnet',
     MIN_BALANCE_FOR_GAS: nearApiJs.utils.format.parseNearAmount('0.05'),
     MIN_BALANCE_TO_CREATE: nearApiJs.utils.format.parseNearAmount('0.1'),
     MOONPAY_API_KEY: 'pk_test_wQDTsWBsvUm7cPiz9XowdtNeL5xasP9',
