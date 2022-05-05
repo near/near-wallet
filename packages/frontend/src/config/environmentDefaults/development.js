628a8bc9e9caeef64ab8c51aae90129fe7235ee6
Andy Haynes <andy@near.org>
2022-05-05 13:59:46 -0700

    feat(indexer): URL for indexer queries

--- packages/frontend/src/config/environmentDefaults/development.js
+++ packages/frontend/src/config/environmentDefaults/development.js
@@ -15,6 +15,7 @@ export default {
     EXPLORE_DEFI_URL: 'https://awesomenear.com/categories/defi/',
     EXPLORER_URL:'https://explorer.testnet.near.org',
     HIDE_SIGN_IN_WITH_LEDGER_ENTER_ACCOUNT_ID_MODAL: false,
+    INDEXER_SERVICE_URL: 'https://testnet-api.kitwallet.app',
     LINKDROP_GAS: '100000000000000',
     LOCKUP_ACCOUNT_ID_SUFFIX: 'lockup.m0',
     MIN_BALANCE_FOR_GAS: nearApiJs.utils.format.parseNearAmount('0.05'),
