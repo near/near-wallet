cb13302232dbb0116569b6024d64426250a59f73
marcinbodnar <me@marcinbodnar.com>
2022-05-27 16:58:54 +0200

    refactor: add setContractMetadata action

--- packages/frontend/src/redux/slices/tokensMetadata/index.js
+++ packages/frontend/src/redux/slices/tokensMetadata/index.js
@@ -1,13 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit';
+import set from 'lodash.set';
 const SLICE_NAME = 'tokensMetadata';
 
 const initialState = {
     byContractName: {}
 };
-
 export const tokensMetadataSlice = createSlice({
     name: SLICE_NAME,
     initialState,
     reducers: {
+        setContractMetadata(state, { payload }) {
+            const { metadata, contractName } = payload;
+            set(state, ['byContractName', contractName], metadata);
+        }
     }
 });
