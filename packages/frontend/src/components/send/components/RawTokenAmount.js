77298190c212628b3472ba4044ea34efc9e09edc
Judith Lao <judith@near.org>
2022-05-27 15:03:33 -0400

    style: fix colors to match figma, bug: decimals should equal null if none is provided

--- packages/frontend/src/components/send/components/RawTokenAmount.js
+++ packages/frontend/src/components/send/components/RawTokenAmount.js
@@ -6,7 +6,7 @@ import TokenAmount from '../../wallet/TokenAmount';
 const RawTokenAmount = ({
     symbol,
     amount,
-    decimals,
+    decimals = null,
     withSymbol = true,
     showFiatAmountForNonNearToken
 }) => {
