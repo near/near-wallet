1220940110e808fc6e0faba4baa288dca0293e52
marcinbodnar <me@marcinbodnar.com>
2022-05-27 17:01:24 +0200

    refactor: create and refactor selectors

--- packages/frontend/src/redux/slices/tokensMetadata/index.js
+++ packages/frontend/src/redux/slices/tokensMetadata/index.js
@@ -1,5 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
 import set from 'lodash.set';
+import { createSelector } from 'reselect';
+
+import createParameterSelector from '../../selectors/mainSelectors/createParameterSelector';
+
 const SLICE_NAME = 'tokensMetadata';
 
 const initialState = {
@@ -15,3 +19,22 @@ export const tokensMetadataSlice = createSlice({
         }
     }
 });
+
+export default tokensMetadataSlice;
+
+export const actions = {
+    ...tokensMetadataSlice.actions
+};
+export const reducer = tokensMetadataSlice.reducer;
+
+// Top level selectors
+const selectTokensMetadataSlice = (state) => state[SLICE_NAME] || initialState;
+
+const getContractNameParam = createParameterSelector((params) => params.contractName);
+
+export const selectContractsMetadata = createSelector(selectTokensMetadataSlice, ({ byContractName }) => byContractName || {});
+
+export const selectOneContractMetadata = createSelector(
+    [selectContractsMetadata, getContractNameParam],
+    (metadataByContractName, contractName) => metadataByContractName[contractName]
+);
