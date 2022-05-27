dd340b496239fb458e1658d6b09f7d6d6524a88f
marcinbodnar <me@marcinbodnar.com>
2022-05-27 17:16:51 +0200

    refactor: cleanup

--- packages/frontend/src/redux/slices/tokensMetadata/index.js
+++ packages/frontend/src/redux/slices/tokensMetadata/index.js
@@ -3,7 +3,7 @@ import set from 'lodash.set';
 import { createSelector } from 'reselect';
 
 import FungibleTokens from '../../../services/FungibleTokens';
-import createParameterSelector from '../../selectors/mainSelectors/createParameterSelector';
+import { createParameterSelector } from '../../selectors/topLevel';
 
 const SLICE_NAME = 'tokensMetadata';
 
@@ -32,17 +32,12 @@ export const tokensMetadataSlice = createSlice({
 
 export default tokensMetadataSlice;
 
-export const actions = {
-    ...tokensMetadataSlice.actions
-};
-export const reducer = tokensMetadataSlice.reducer;
-
 // Top level selectors
 const selectTokensMetadataSlice = (state) => state[SLICE_NAME] || initialState;
 
 const getContractNameParam = createParameterSelector((params) => params.contractName);
 
-export const selectContractsMetadata = createSelector(selectTokensMetadataSlice, ({ byContractName }) => byContractName || {});
+export const selectContractsMetadata = createSelector(selectTokensMetadataSlice, ({ byContractName }) => byContractName);
 
 export const selectOneContractMetadata = createSelector(
     [selectContractsMetadata, getContractNameParam],
