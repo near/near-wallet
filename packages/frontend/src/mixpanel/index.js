eed4fbaf25cecb61e051cc2ed55fbe2ff687a83d
Andy Haynes <andy@near.org>
2022-06-06 15:20:15 -0700

    fix(mixpanel): redact sensitive URLs

--- packages/frontend/src/mixpanel/index.js
+++ packages/frontend/src/mixpanel/index.js
@@ -2,6 +2,15 @@ import mixpanel from 'mixpanel-browser';
 
 import { BROWSER_MIXPANEL_TOKEN } from '../config';
 
+function buildTrackingProps() {
+    const sanitizedUrl = decodeURI(window.location.href)
+        .replace(/(?:\w{3,12} ){11}(?:\w{3,12})/g, 'REDACTED');
+
+    return {
+        $current_url: encodeURI(sanitizedUrl),
+    };
+}
+
 let Mixpanel = {
     get_distinct_id: () => {},
     identify: () => {},
@@ -43,7 +52,10 @@ if (BROWSER_MIXPANEL_TOKEN) {
             mixpanel.alias(id);
         },
         track: (name, props) => {
-            mixpanel.track(name, props);
+            mixpanel.track(name, {
+                ...props,
+                ...buildTrackingProps(),
+            });
         },
         people: {
             set: (props) => {
@@ -55,11 +67,14 @@ if (BROWSER_MIXPANEL_TOKEN) {
         },
         withTracking: async (name, fn, errorOperation, finalOperation) => {
             try {
-                mixpanel.track(`${name} start`);
+                mixpanel.track(`${name} start`, buildTrackingProps());
                 await fn();
-                mixpanel.track(`${name} finish`);
+                mixpanel.track(`${name} finish`, buildTrackingProps());
             } catch (e) {
-                mixpanel.track(`${name} fail`, {error: e.message});
+                mixpanel.track(`${name} fail`, {
+                    error: e.message,
+                    ...buildTrackingProps(),
+                });
                 if (errorOperation) {
                     await errorOperation(e);
                 } else {
