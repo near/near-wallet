6c91d4d8be461f335b85c9650406e756baaf047f
Danny Cho <hj.danny.cho@gmail.com>
2022-06-07 16:03:14 +1200

    feat: added a copy button on UserAccount UI

--- packages/frontend/src/components/navigation/UserAccount.js
+++ packages/frontend/src/components/navigation/UserAccount.js
@@ -2,8 +2,10 @@ import React from 'react';
 import styled from 'styled-components';
 
 import classNames from '../../utils/classNames';
+import ClickToCopy from '../common/ClickToCopy';
 import ChevronIcon from '../svg/ChevronIcon';
 import UserIcon from '../svg/UserIcon';
+import CopyIcon from '../svg/CopyIcon';
 
 const Container = styled.div`
     background-color: #F0F0F1;
@@ -28,12 +30,23 @@ const Container = styled.div`
         margin: 0 14px 0 9px;
         white-space: nowrap;
         max-width: 150px;
-        overflow: hidden;
-        text-overflow: ellipsis;
+        
         color: #72727A;
+        display: flex;
+        align-items: center;
 
         @media (max-width: 991px) {
             margin: 0 14px 0 12px;
+        }
+
+        .account-id {
+            max-width: 120px;
+            overflow: hidden;
+            text-overflow: ellipsis;
+        }
+
+        .copy-wrapper {
+            margin-left: 10px;
         }
     }
 
@@ -67,7 +80,14 @@ const UserAccount = ({ accountId = '', onClick, withIcon = true, flowLimitationS
     <Container className={classNames(['user-account', {'no-click' : flowLimitationSubMenu }])} onClick={onClick}>
         {withIcon && <UserIcon color='#A2A2A8'/>}
         <div className="account-wrapper" data-test-id="currentUser">
-            {accountId}
+            <div className="account-id">
+                {accountId}
+            </div>
+            <div className="copy-wrapper" onClick={(e) => e.stopPropagation()}>
+                <ClickToCopy copy={accountId}>
+                    <CopyIcon />
+                </ClickToCopy>
+            </div>
         </div>
         <div className='icon-wrapper'>
             <ChevronIcon/>
