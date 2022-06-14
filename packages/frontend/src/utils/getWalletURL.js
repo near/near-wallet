ec56ac2fdccb6eb51db285c69d309919ebca1136
Den Ilin <followdarko@gmail.com>
2022-06-14 13:23:16 +0300

    fix: bugfix

--- packages/frontend/src/utils/getWalletURL.js
+++ packages/frontend/src/utils/getWalletURL.js
@@ -11,7 +11,7 @@ export default (https = true) => {
     }
 
     const protocol = https ? 'https://' : '';
-    if (isWhitelabel) {
+    if (isWhitelabel()) {
         return `${protocol}app.mynearwallet.com`;
     }
 
