7f92c710d1963feda16ab0a68d6c0540405c2b07
Judith Lao <judith@near.org>
2022-06-20 11:03:56 -0400

    fix: do not break page when mediaUrl is undefined

--- packages/frontend/src/components/nft/NFTMedia.js
+++ packages/frontend/src/components/nft/NFTMedia.js
@@ -6,8 +6,8 @@ export function NFTMedia({ mediaUrl, autoPlay = false }) {
     const [isVideo, mimeType] = useMemo(() => {
         let mimeType;
         // check mediaUrl string for .webm or .mp4 endings (case-insensitive)
-        if (mediaUrl.match(/\.webm$/i)) mimeType = "webm";
-        else if (mediaUrl.match(/\.mp4$/i)) mimeType = "mp4";
+        if (mediaUrl && mediaUrl.match(/\.webm$/i)) mimeType = "webm";
+        else if (mediaUrl && mediaUrl.match(/\.mp4$/i)) mimeType = "mp4";
         // if there is a mediaUrl and a truthy mimeType (webm or mp4), we have a video
         const isVideo = !!mediaUrl && mimeType;
         return [isVideo, mimeType];
