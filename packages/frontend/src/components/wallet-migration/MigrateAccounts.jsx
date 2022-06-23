0ae4606bc78d2631e352b1131aaedae30e1e90f3
Lapko <artyom.lapko@ostrovok.ru>
2022-06-23 11:59:28 +0300

    fix: fix encode method

--- packages/frontend/src/components/wallet-migration/MigrateAccounts.jsx
+++ packages/frontend/src/components/wallet-migration/MigrateAccounts.jsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react';
+import React from 'react';
 import { Translate } from 'react-localize-redux';
 import styled from 'styled-components';
 
--- packages/frontend/src/components/wallet-migration/WalletMigration.jsx
+++ packages/frontend/src/components/wallet-migration/WalletMigration.jsx
@@ -48,20 +48,28 @@ const encodeAccountsToURL = async (accounts, publicKey) => {
     return href;
 };
 
-const redirect = (accountId, keyPair, migrationKeyPair) => {
-    const accData = `${accountId}=${keyPair.secretKey}=${getLedgerHDPath(accountId)||''}`;
-    const encoded = encodeMessage(accData, migrationKeyPair.secretKey);
+const encodeAccountsToURL = async (accounts, publicKey) => {
+    const accountsData = [];
+    for (let i = 0; i < accounts.length; i++) {
+        const accountId = accounts[i];
+        const keyPair = await wallet.getLocalKeyPair(accountId);
+        accountsData.push([
+            accountId,
+            keyPair.secretKey,
+            getLedgerHDPath(accountId),
+        ]);
+    }
 
+    const hash = encodeAccountsTo(accountsData, publicKey);
     const subdomain = isWhitelabelTestnet() ? 'testnet' : 'app';
-    location.href = `https://${subdomain}.mynearwallet.com/batch-import#${btoa(encoded)}`;
-    // location.href = `https://localhost:1234/batch-import#${btoa(encoded)}`;
+    const href = `https://${subdomain}.mynearwallet.com/batch-import#${hash}`;
+
+    return href;
 };
 
 const WalletMigration = ({ open, history, onClose }) => {
     const [state, setState] = React.useState(initialState);
     const availableAccounts = useSelector(selectAvailableAccounts);
-    const sortedAccountsByUsing = [...availableAccounts]
-        .sort((a) => a === wallet.accountId ? -1 : 1);
 
     const handleStateUpdate = (newState) => {
         setState({...state, ...newState});
