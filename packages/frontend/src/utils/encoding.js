924b68d66dbdd214f5dc5131dfa3d3668873f0b5
Lapko <artyom.lapko@ostrovok.ru>
2022-06-23 11:59:28 +0300

    fix: fix encode method

--- packages/frontend/src/utils/encoding.js
+++ packages/frontend/src/utils/encoding.js
@@ -1,18 +1,26 @@
 import bs58 from 'bs58';
 import nacl from 'tweetnacl';
 
+const STATIC_NONCE = new Uint8Array([
+    190, 12, 82, 22, 119, 120, 120,
+    8, 122, 124, 234, 14, 28, 83,
+    74, 168, 174, 124, 146, 88, 46,
+    200, 208, 82
+]);
+
 export function generateKeyPair() {
     return nacl.sign.keyPair();
 }
 
-export function encodeMessage(message, privateKey) {
+export function encodeMessage(message, publicKey) {
     const encoder = new TextEncoder();
-    return nacl.sign(encoder.encode(message), privateKey);
+
+    return nacl.secretbox(encoder.encode(message), STATIC_NONCE, publicKey);
 }
 
 export function decodeMessage(cipherText, publicKey) {
     try {
-        const opened = nacl.sign.open(cipherText, publicKey);
+        const opened = nacl.secretbox.open(cipherText, STATIC_NONCE, publicKey);
         if (opened === null) {
             return opened;
         }
@@ -24,14 +32,24 @@ export function decodeMessage(cipherText, publicKey) {
     }
 }
 
-export function decodeAccountFrom(hash, publicKey) {
+export function encodeAccountsTo(accountsData, publicKey) {
+    const message = accountsData.reduce((msg, accountData) => {
+        msg.push(accountData.join('='));
+
+        return msg;;
+    }, []).join('*');
+
+    return window.btoa(encodeMessage(message, publicKey));
+}
+
+export function decodeAccountsFrom(hash, publicKey) {
     if (!location.hash) {
-        return ['', '', ''];
+        return [];
     }
 
     const bs64encoded = location.hash.slice(1);
     if (!bs64encoded) {
-        return ['', '', ''];
+        return [];
     }
 
     try {
@@ -43,9 +61,11 @@ export function decodeAccountFrom(hash, publicKey) {
             keyFromString(publicKey)
         );
 
-        return (decoded||'').split('=');
+        return (decoded||'')
+            .split('*')
+            .map((account) => account.split('='));
     } catch (e) {
-        return ['', ''];
+        return [];
     }
 }
 
