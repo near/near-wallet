d361867520391ce262382a693cfa925956cbb5d8
gutsyphilip <philip.c.obosi@gmail.com>
2022-06-28 14:40:50 +0400

    feat: update logo

--- packages/frontend-near-wallet/src/components/MigrationPrompt.tsx
+++ packages/frontend-near-wallet/src/components/MigrationPrompt.tsx
@@ -1,23 +1,50 @@
 
 import React from 'react';
-import { Translate } from 'react-localize-redux';
 import { WALLET_MIGRATION_VIEWS } from '../utils/constants';
 import { styled } from '../styles';
 import Button from './Button';
 
-// import IconClose from '../../images/IconClose';
-// import FormButton from '../common/FormButton';
-// import Container from '../common/styled/Container.css';
+import MyNearWalletLogo from '../assets/images/ImgMyNearWalletLogo';
+
+const MigrationPrompt = ({ handleSetActiveView, handleRedirectToBatchImport }) => {
+    return (
+        <StyledContainer>
+            <MyNearWalletLogo className="logo" />
+            <h3 className='ttl'>We’ve moved!</h3>
+            <p className='desc'>
+                NEAR Wallet is now My NEAR Wallet, and has moved to a new domain. You can find us at <a href='https://mynearwallet.com'>https://mynearwallet.com</a>.
+                <br /><br />
+                If you haven’t already, quickly and easily transfer your accounts to the new domain:
+            </p>
+            <ButtonsContainer>
+                <StyledButton onClick={handleRedirectToBatchImport}>
+                    Transfer My Accounts
+                </StyledButton>
+                <StyledButton onClick={() => { handleSetActiveView(WALLET_MIGRATION_VIEWS.SELECT_DESTINATION_WALLET); }} variant="text">
+                    Use a different wallet
+                </StyledButton>
+            </ButtonsContainer>
+        </StyledContainer>
+    );
+};
+
+export default MigrationPrompt;
 
 const StyledContainer = styled('section', {
-    maxWidth: '356px',
+    maxWidth: '396px',
     margin: '0 auto',
+    padding: '0 20px',
     height: '100vh',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
     textAlign: 'center',
+
+    '& > .logo': {
+        maxWidth: '200px',
+        width: '100%',
+    },
 
     '& > .ttl': {
         margin: '0 auto 24px',
@@ -28,35 +55,6 @@ const StyledContainer = styled('section', {
         color: '#72727A'
     }
 })
-
-const ContentContainer = styled('div', {})
-
-// `
-//     padding: 15px 0;
-//     text-align: center;
-//     max-width: 362px;
-//     margin: 0 auto;
-//     @media (max-width: 360px) {
-//         padding: 0;
-//     }
-//     .close-icon {
-//         position: absolute;
-//         height: 16px;
-//         width: 16px;
-//         right: 24px;
-//         top: 24px;
-//         cursor: pointer;
-//     }
-//     .logo {
-//         width: 100%;
-//         max-width: 200px;
-//     }
-//     .title{
-//         font-size: 20px;
-//         margin-top: 16px;
-//     }
-// `;
-
 
 const ButtonsContainer = styled('div', {
     marginTop: '72px',
@@ -77,26 +75,3 @@ const StyledButton = styled(Button, {
         marginTop: '10px !important'
     }
 });
-
-const MigrationPrompt = ({ handleSetActiveView, handleRedirectToBatchImport }) => {
-    return (
-        <StyledContainer>
-            <h3 className='ttl'>We’ve moved!</h3>
-            <p className='desc'>
-                NEAR Wallet is now My NEAR Wallet, and has moved to a new domain. You can find us at <a href='https://mynearwallet.com'>https://mynearwallet.com</a>.
-                <br /><br />
-                If you haven’t already, quickly and easily transfer your accounts to the new domain:
-            </p>
-            <ButtonsContainer>
-                <StyledButton onClick={handleRedirectToBatchImport}>
-                    Transfer My Accounts
-                </StyledButton>
-                <StyledButton onClick={() => { handleSetActiveView(WALLET_MIGRATION_VIEWS.SELECT_DESTINATION_WALLET); }} variant="text">
-                    Use a different wallet
-                </StyledButton>
-            </ButtonsContainer>
-        </StyledContainer>
-    );
-};
-
-export default MigrationPrompt;
