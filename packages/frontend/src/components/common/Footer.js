ab66f987f78da99b8151c23ca3c56b4ef89a0b8c
Den Ilin <followdarko@gmail.com>
2022-06-29 14:01:27 +0300

    fix: fix footer

--- packages/frontend/src/components/common/Footer.js
+++ packages/frontend/src/components/common/Footer.js
@@ -133,7 +133,7 @@ const Footer = () => {
             <div className='right'>
                 <Translate id='footer.needHelp' /><br />
                 <a
-                    href='https://discord.com/invite/Vj74PpQYsh'
+                    href={isWhitelabel() ? 'https://discord.com/invite/Vj74PpQYsh' : 'https://near.chat'}
                     rel='noopener noreferrer'
                     target='_blank'
                     onClick={() => Mixpanel.track('Footer Click Join Community')}
