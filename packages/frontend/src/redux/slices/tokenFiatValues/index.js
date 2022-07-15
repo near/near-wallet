b03719516e70f4bf9df90a1e3fd63c6e734dd76e
Judith Lao <judith@near.org>
2022-07-15 13:05:24 -0400

    fix: catch the error so that execution doesn't fail out

--- packages/frontend/src/redux/slices/tokenFiatValues/index.js
+++ packages/frontend/src/redux/slices/tokenFiatValues/index.js
@@ -10,6 +10,9 @@ import handleAsyncThunkStatus from '../../reducerStatus/handleAsyncThunkStatus';
 import initialStatusState from '../../reducerStatus/initialState/initialStatusState';
 import { getCachedContractMetadataOrFetch } from '../tokensMetadata';
 
+// this error would surface if the developer of a token smart contract does not implement the necessary ft_metadata method
+const METHOD_NOT_FOUND_ERROR = 'Querying [object Object] failed: wasm execution failed with error: FunctionCallError(MethodResolveError(MethodNotFound)).';
+
 const SLICE_NAME = 'tokenFiatValues';
 const fiatValueManager = new FiatValueManager();
 
@@ -32,7 +35,13 @@ const fetchTokenFiatValues = createAsyncThunk(
                 try {
                     const metadata = await getCachedContractMetadataOrFetch(contractName, getState());
                     symbol = metadata.symbol;
-                } finally { 
+                }
+                catch(error) {
+                    if (!error.message.includes(METHOD_NOT_FOUND_ERROR)) {
+                        console.log(`Unknown error fetching fiat value for ${contractName}`, error.message)
+                    }
+                }
+                finally { 
                     if (symbol) {
                         ownedTokens.push(symbol);
                     }
