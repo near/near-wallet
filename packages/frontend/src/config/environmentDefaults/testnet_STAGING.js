c431e6d8a57d71e80a89924daf906058c95c453e
Patrick <patrick.tajima@gmail.com>
2022-07-26 17:24:51 -0400

    Revert "[WIP]: added HAPI risc scoring system"

--- packages/frontend/src/config/environmentDefaults/testnet_STAGING.js
+++ packages/frontend/src/config/environmentDefaults/testnet_STAGING.js
@@ -52,6 +52,5 @@ export default {
     FARMING_CLAIM_YOCTO: '1',
     REF_FINANCE_API_ENDPOINT: 'https://testnet-indexer.ref-finance.com/',
     REF_FINANCE_CONTRACT: 'ref-finance-101.testnet',
-    USN_CONTRACT: 'usdn.testnet',
-    HAPI_PROTOCOL_ADDRESS: 'proxy.contracts.sergei24.testnet'
+    USN_CONTRACT: 'usdn.testnet'
 };
