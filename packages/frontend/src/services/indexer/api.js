7660ba81c81e330b3e4e7d150f592df5d5966857
Lapko <artyom.lapko@ostrovok.ru>
2022-08-24 13:11:05 +0300

    fix: fixes after merge

--- packages/frontend/src/services/indexer/api.js
+++ packages/frontend/src/services/indexer/api.js
@@ -1,6 +1,53 @@
-import { INDEXER_SERVICE_URL } from '../config';
-import sendJson from '../tmp_fetch_send_json';
-import { CUSTOM_REQUEST_HEADERS } from '../utils/constants';
+import { INDEXER_SERVICE_URL } from '../../config';
+import sendJson from '../../tmp_fetch_send_json';
+import { CUSTOM_REQUEST_HEADERS } from '../../utils/constants';
+import { buildUrlOptParam } from '../../utils/url';
+
+export default {
+    listAccountsByPublicKey: (publicKey) => {
+        return fetch(`${INDEXER_SERVICE_URL}/publicKey/${publicKey}/accounts`, {
+            headers: {
+                ...CUSTOM_REQUEST_HEADERS,
+            }})
+            .then((res) => res.json());
+    },
+    listLikelyNfts: (accountId, timestamp) => {
+        const url = `${INDEXER_SERVICE_URL}/account/${accountId}/likelyNFTs`;
+
+        return sendJson('GET', buildUrlOptParam(url, {
+            fromBlockTimestamp: timestamp
+        }));
+    },
+    listLikelyTokens: (accountId, timestamp) => {
+        const url = `${INDEXER_SERVICE_URL}/account/${accountId}/likelyTokens`;
+
+        return sendJson('GET', buildUrlOptParam(url, {
+            fromBlockTimestamp: timestamp
+        }));
+    },
+    listRecentTransactions: (accountId) => {
+        return fetch(`${INDEXER_SERVICE_URL}/account/${accountId}/activity`, {
+            headers: {
+                ...CUSTOM_REQUEST_HEADERS,
+            }})
+            .then((res) => res.json());
+    },
+    listStakingDeposits: (accountId) => {
+        return fetch(`${INDEXER_SERVICE_URL}/staking-deposits/${accountId}`, {
+            headers: {
+                ...CUSTOM_REQUEST_HEADERS,
+            }})
+            .then((r) => r.json());
+    },
+    listStakingPools: () => {
+        return fetch(`${INDEXER_SERVICE_URL}/stakingPools`, {
+            headers: {
+                ...CUSTOM_REQUEST_HEADERS,
+            }})
+            .then((r) => r.json());
+    },
+};
+
 
 export function listAccountsByPublicKey(publicKey) {
     return fetch(`${INDEXER_SERVICE_URL}/publicKey/${publicKey}/accounts`, {
