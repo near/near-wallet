a7af5c4cecd3f7cb44d5ce34d9147521064bc24c
NotEternal <cent1pede@protonmail.ch>
2022-08-30 08:26:32 +0200

    feat: clarify test names

--- packages/frontend/src/utils/account.test.js
+++ packages/frontend/src/utils/account.test.js
@@ -1,6 +1,6 @@
 import { shortenAccountId } from './account';
 
-test('ID is shorten with correct formats', () => {
+test('It should shorten account ID with correct format', () => {
     const sourceId = '4ee68bC02dd892aa1ad819101f3adf4cfafb90e3f73b827f4c2352b6efe363ek';
 
     expect(shortenAccountId(sourceId)).toBe('4ee68bC0...efe363ek');
@@ -9,7 +9,7 @@ test('ID is shorten with correct formats', () => {
     expect(shortenAccountId(sourceId, 30, 31)).toBe('4ee68bC02dd892aa1ad819101f3adf...afb90e3f73b827f4c2352b6efe363ek');
 });
 
-test('ID remains the same', () => {
+test('It should not change account ID', () => {
     const sourceId = '4ee68bC02dd892aa1ad819101f3adf4cfafb90e3f73b827f4c2352b6efe363ek';
     const wrongId = 'abcde';
 
