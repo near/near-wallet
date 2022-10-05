733959061cc7d3e61e4afca49e696a4d2276d102
NotEternal <cent1pede@protonmail.ch>
2022-10-05 16:41:18 +0200

    feat(account id): shorten account ids in the middle in the drop-down

--- packages/frontend/src/components/common/DropDown.js
+++ packages/frontend/src/components/common/DropDown.js
@@ -41,8 +41,6 @@ const Container = styled.div`
         }
 
         .dropdown-title {
-            overflow: hidden;
-            text-overflow: ellipsis;
             font-size: 16px;
             font-weight: 500;
         }
