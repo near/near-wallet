61fc35b04fe4a95c2c3d54ec1ae4dd846bd85d85
Danny Cho <hj.danny.cho@gmail.com>
2022-10-25 17:10:20 +1300

    fix: only show finer wallet if device is iOS

--- packages/frontend/src/utils/isMobile.js
+++ packages/frontend/src/utils/isMobile.js
@@ -1,9 +1,13 @@
-const isMobile = () => {
+const isMobile = (target) => {
     let userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
     const windows = /windows phone/i.test(userAgent);
     const android = /android/i.test(userAgent);
     const iOs = /iPad|iPhone|iPod/.test(userAgent) && !window.MSStream;
+
+    if (target === 'iOS') {
+        return iOs;
+    }
 
     return windows || android || iOs;
 };
