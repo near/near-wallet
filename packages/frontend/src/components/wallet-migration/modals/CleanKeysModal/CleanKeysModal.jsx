4a5d6b4ec650b98da747b299be8b4162a75b7707
roshaans <siddiqui.roshaan@gmail.com>
2022-10-26 13:30:19 +0100

    fix: make it commitable X_X

--- packages/frontend/src/components/wallet-migration/modals/CleanKeysModal/CleanKeysModal.jsx
+++ packages/frontend/src/components/wallet-migration/modals/CleanKeysModal/CleanKeysModal.jsx
@@ -7,17 +7,16 @@ import { ButtonsContainer, StyledButton, MigrationModal, Container, IconBackgrou
 
 // Test cases
 // - Ensure that if user leaves the flow and comes back in 
-const YOCTO_NEAR_TO_REMOVE_FAK = 2;
+// const YOCTO_NEAR_TO_REMOVE_FAK = 2;
 
 const CleanKeysModal = ({ onNext, onClose }) => {
-    console.log(YOCTO_NEAR_TO_REMOVE_FAK);
-    // 1. Identify Full Access Keys on all user accounts. 
+    // 1. Identify Full Access Keys on all user accounts.
     // Identify if they are sms, email, or unknown keys
     // 2. Ensure that the newly generated key in the RotateKeysModal is not deleted. 
     // 3. Ensure that the funding account has Threshold * (# of FAKs to be removed) Near present in their account. 
     // 4. Ensure that if an account has a single FAK OR is an Implicit Account with 0 Near, 
     // 5. Create a data structure which stores what keys should be marked for deletion
-    // 6. For each account to be cleaned, show a modal where the user puts their FAK. 
+    // 6. For each account to be cleaned, show a modal where the user puts their FAK.
 
 
     return (
