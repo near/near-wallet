d5be4db020fe472143f36b90884c704eedfc33fc
Danny Cho <hj.danny.cho@gmail.com>
2023-03-27 17:55:33 +1300

    fix: During the route change, close account-export modal with minor ux/error handling

--- packages/frontend/src/components/wallet-migration/modals/WalletSelectorModal/WalletSelectorContent.jsx
+++ packages/frontend/src/components/wallet-migration/modals/WalletSelectorModal/WalletSelectorContent.jsx
@@ -1,15 +1,23 @@
 import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import { useExportAccountSelector } from './WalletSelectorExportContext';
 
 export const WalletSelectorContent = () => {
     const { ExportModal } = useExportAccountSelector();
-    
+    const location = useLocation();
     useEffect(() => {
-        //TODO: this is to mute css class injected from WalletSelectorGetAWallet component
-        const walletWrapperDiv = document.getElementById('near-wallet-selector-modal');
-        walletWrapperDiv?.classList.remove('wallet-selector-get-a-wallet-modal');
-        ExportModal?.show();
-    });
+        if (ExportModal) {
+            //TODO: this is to mute css class injected from WalletSelectorGetAWallet component
+            const walletWrapperDiv = document.getElementById('near-wallet-selector-modal');
+            walletWrapperDiv?.classList.remove('wallet-selector-get-a-wallet-modal');
+            ExportModal?.show();
+        }
+    }, [ExportModal]);
+
+    // On location change, close the modal
+    useEffect(() => {
+        ExportModal?.hide();
+    }, [location]);
     return null;
 };
