11d96e9866747a30453869987e97a55eca997802
DamirSQA <damir.pjetlovic@sqa-consulting.com>
2023-04-13 11:24:23 +0200

    2fa page updates, added input field

--- packages/frontend/src/components/disable-2fa/disable-2fa.js
+++ packages/frontend/src/components/disable-2fa/disable-2fa.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import classNames from '../../utils/classNames';
+import FormButton from '../common/FormButton';
+import { Translate } from 'react-localize-redux';
+
 
 // import Container from '../common/styled/Container.css';
+
+
+const RecoverAccountSeedPhraseForm = ({
+    isLegit,
+    handleChange,
+    seedPhrase,
+    localAlert,
+    recoveringAccount,
+    findMyAccountSending
+}) => (
+        <>
+            <h4><Translate id='recoverSeedPhrase.seedPhraseInput.title' /></h4>
+            <Translate>
+                {({ translate }) => (
+                    <input
+                        value={seedPhrase}
+                        onChange={(e) => handleChange(e.target.value)}
+                        className={classNames([{'success': localAlert && localAlert.success}, {'problem': localAlert && localAlert.success === false}])}
+                        placeholder={translate('recoverSeedPhrase.seedPhraseInput.placeholder')}
+                        disabled={recoveringAccount}
+                        data-test-id="seedPhraseRecoveryInput"
+                        required
+                        tabIndex='2'
+                        autoCapitalize='off'
+                    />
+                )}
+            </Translate>
+            <FormButton
+                type='submit'
+                color='blue'
+                // disabled={!isLegit || recoveringAccount}
+                sending={findMyAccountSending}
+                sendingString='button.recovering'
+                data-test-id="seedPhraseRecoverySubmitButton"
+            >
+                <Translate id='button.findMyAccount' />
+            </FormButton>
+        </>
+);
+
+
 
 const StyledContainer = styled.div`
   &&& {
@@ -12,14 +57,79 @@ const StyledContainer = styled.div`
     justify-content: center;
     align-items: center;
   }
+
+  .input {
+    width: 100%;
+}
+
+.input-sub-label {
+    margin-bottom: 30px;
+}
+
+h4 {
+    :first-of-type {
+        margin: 30px 0 0 0 !important;
+    }
+}
+
+button {
+    width: 100% !important;
+    margin-top: 30px !important;
+}
+
 `;
 
 export function Disable2faPage() {
+
+    const [seedPhrase, setSeedPhrase] = useState('')
+    console.log('%cseedPhrase', 'color: aqua;font-size: 12px;', seedPhrase);
+    
+
+   const handleChange = (value) => {
+   
+            setSeedPhrase(value)
+        
+        // this.props.clearLocalAlert();
+    }
+
+    // handleChange = (value) => {
+    //     this.setState(() => ({
+    //         seedPhrase: value
+    //     }));
+    //     this.props.clearLocalAlert();
+    // }
+
+
+
+    // handleSubmit = async () => {
+    //     if (!this.isLegit) {
+    //         return false;
+    //     }
+    // }
+        
+        // const combinedState = {
+        //     ...this.props,
+        //     ...this.state,
+        //     isLegit: this.isLegit && !(this.props.localAlert && this.props.localAlert.success === false)
+        // }
+
     return (
         <StyledContainer>
-            <h2>
-                Page for disabling 2FA
-            </h2>
+
+
+            {/* <h1><Translate id='recoverSeedPhrase.pageTitle' /></h1> */}
+            <h1>Disable 2 factor autentication</h1>
+                <h2><Translate id='recoverSeedPhrase.pageText' /></h2>
+                <form onSubmit={(e) => {
+                    // handleSubmit();
+                    e.preventDefault();
+                }} autoComplete='off'>
+                    <RecoverAccountSeedPhraseForm
+                        // {...combinedState}
+                        handleChange={handleChange}
+                    />
+                </form>
         </StyledContainer>
     );
 }
+
