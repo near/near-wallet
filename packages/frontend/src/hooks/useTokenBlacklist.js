c2818b5675a3e818f238352e7ad638251656518c
Andy Haynes <andy@near.org>
2023-08-01 16:59:22 -0700

    feat: support wildcards in ft blacklist

--- packages/frontend/src/hooks/useTokenBlacklist.js
+++ packages/frontend/src/hooks/useTokenBlacklist.js
@@ -1,5 +1,26 @@
 import { useMemo } from 'react';
 
+/**
+ * Determine whether a fungible token contract should be included given a token blacklist
+ * @param token {string} token contract name under consideration
+ * @param blacklist {string[]} list of blacklisted tokens; either the full name or a leading wildcard ('*')
+ * @returns {boolean}
+ */
+export function isTokenIncluded(tokenContract, blacklist) {
+    for (let blacklistedToken of blacklist) {
+        if (tokenContract === blacklistedToken) {
+            return false;
+        }
+
+        // e.g. *.mallory.near would exclude a.mallory.near and b.mallory.near
+        if (blacklistedToken.startsWith('*') && tokenContract.endsWith(blacklistedToken.slice(1))) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 export function useTokenBlacklist({ tokens }) {
     // TODO: make list dynamic, fetch from db
     const blacklistedTokens = [
@@ -12,7 +33,7 @@ export function useTokenBlacklist({ tokens }) {
             return tokens;
         }
 
-        return tokens.filter((token) => !blacklistedTokens.includes(token.contractName));
+        return tokens.filter((token) => isTokenIncluded(token.contractName, blacklistedTokens));
     }, [tokens]);
 
     return { blacklistedTokens, allowedTokens };
