262748d67cd992bd06c109ad802aa6bc089eaff0
jackson@near.org <jacksonnear.org@jacksonrorgsMBP.attlocal.net>
2024-04-17 21:22:26 -0700

    chore: clean up console logs and unused styles

--- packages/frontend/src/components/landing/GuestLanding.js
+++ packages/frontend/src/components/landing/GuestLanding.js
@@ -584,41 +584,11 @@ const CustomButton = styled.button`
     }
 `;
 
-const Pattern = styled.div`
-    width: 100%;
-    min-height: 540px;
-    display: flex;
-    align-items: center;
-    background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAJYAAACWCAYAAAA8AXHiAAAACXBIWXMAABYlAAAWJQFJUiTwAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAGeSURBVHgB7doxTisxEAbgeY/mvQro6NiSDo6QkpJbcA2OwjWooKQMJ2DpKENJBV7FEYoBeQSIZr9PGk2cItWvsdfZnSBjKHVf6rnUbdD1N8g4K7VX6jhIEaycofaTIEWwcoam0yFYOYe179WiQ7Byhk8+8wnB6munlHNWgmD1tUGyFSYIVl8bJFcOCYLV106s/aBrJ2hNE+qo1GmpRanz2J5aB6X+x/oQv/l+FWz5E/O1iHU4pom0W/u0/uoZahnrgN2VGuv6Jpidl1+o2T5BznkrfKj9MdZT6l9836r+3k2pq1KXMVNz3gpbU7hOmj49AQ7x/lJ0WWsK5xhv2+AYkHQR29vbddDluqFvbNZPQZdg9S07az4gWH3tHZVgJQhW3xjb4XIZyo+Z3nffHN79CZ1gYuXc1b4KEytFsHLGptMhWDlj7Q9BimDlbJ4Ex4AftggHdwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIpXoUVLSWulnzoAAAAASUVORK5CYII=");
-    background-size: 75px 75px;
-    background-repeat: repeat;
-    background-position: center top;
-
-    @media (max-width: 900px) {
-        min-height: 390px;
-    }
-`;
-
-const PatternContent = styled.div`
-    padding: 1rem;
-    max-width: 496px;
-    margin: 0 auto;
-    background-color: var(--background-color);
-    display: flex;
-    align-items: center;
-    min-height: 260px;
-
-    @media (max-width: 900px) {
-        min-height: 0px;
-    }
-`;
-
 export function GuestLanding({ history, accountFound, onTransfer }) {
     const [walletSelectorModal, setWalletSelectorModal] = useState();
     const [showModal, setShowModal] = useState();
 
     const ref = useRef(null);
-    console.log("accountFound", accountFound);
 
     useEffect(() => {
         recordWalletMigrationEvent("LANDING_PAGE");
